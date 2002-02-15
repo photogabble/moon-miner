3a43c9e27b2b4811a6c048c9684add69fcf7953e
Ron Hardwood <hardwoodr@users.sourceforge.net>
2002-02-15 15:01:00 +0000

    Corrected copyright info

--- blacknova/footer.php
+++ blacknova/footer.php
@@ -2,7 +2,7 @@
 <TABLE WIDTH=100% BORDER=0 CELLSPACING=0 CELLPADDING=0>
 <TR>
 <TD><FONT COLOR=SILVER SIZE=-4><A HREF="http://www.sourceforge.net/projects/blacknova">BlackNova Traders</A></FONT></TD>
-<TD ALIGN=RIGHT><FONT COLOR=SILVER SIZE=-4>© 2000-2002 BNT Dev Team</FONT></TD>
+<TD ALIGN=RIGHT><FONT COLOR=SILVER SIZE=-4>© 2000-2002 <a href=mailto:webmaster@blacknova.net>Ron Harwood</a></FONT></TD>
 </TR>
 <TR><TD><FONT COLOR=SILVER SIZE=-4><A HREF="news.php">Local BlackNova News</A></TD>
 </TR>
