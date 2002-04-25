4c0ab9e1dfc736e05f8f3c7409ba6dd9fc48a793
Dave Rowlands <nospoon@users.sourceforge.net>
2002-04-25 00:19:20 +0000

    merged in dominioons patch to add to to ranking

--- blacknova/ranking.php
+++ blacknova/ranking.php
@@ -17,24 +17,31 @@ $num_players = $row['num_players'];
 
 if($sort=="turns")
 {
-  $res = $db->Execute("SELECT score,character_name,turns_used,last_login,rating FROM $dbtables[ships] WHERE ship_destroyed='N' and email NOT LIKE '%@furangee' ORDER BY turns_used DESC,character_name ASC LIMIT $max_rank");
+  $by="turns_used DESC,character_name ASC";
 }
 elseif($sort=="login")
 {
-  $res = $db->Execute("SELECT score,character_name,turns_used,last_login,rating FROM $dbtables[ships] WHERE ship_destroyed='N' and email NOT LIKE '%@furangee' ORDER BY last_login DESC,character_name ASC LIMIT $max_rank");
+  $by="last_login DESC,character_name ASC";
 }
 elseif($sort=="good")
 {
-  $res = $db->Execute("SELECT score,character_name,turns_used,last_login,rating FROM $dbtables[ships] WHERE ship_destroyed='N' and email NOT LIKE '%@furangee' ORDER BY rating DESC,character_name ASC LIMIT $max_rank");
+  $by="rating DESC,character_name ASC";
 }
 elseif($sort=="bad")
 {
-  $res = $db->Execute("SELECT score,character_name,turns_used,last_login,rating FROM $dbtables[ships] WHERE ship_destroyed='N' and email NOT LIKE '%@furangee' ORDER BY rating ASC,character_name ASC LIMIT $max_rank");
+  $by="rating ASC,character_name ASC";
+}
+elseif($sort=="alliance")
+{
+  $by="$dbtables[teams].team_name DESC, character_name ASC";
 }
 else
 {
-  $res = $db->Execute("SELECT score,character_name,turns_used,last_login,rating FROM $dbtables[ships] WHERE ship_destroyed='N' and email NOT LIKE '%@furangee' ORDER BY score DESC,character_name ASC LIMIT $max_rank");
+  $by="score DESC,character_name ASC";
 }
+
+$res = $db->Execute("SELECT $dbtables[ships].score,$dbtables[ships].character_name,$dbtables[ships].turns_used,$dbtables[ships].last_login,$dbtables[ships].rating, $dbtables[teams].team_name FROM $dbtables[ships] LEFT JOIN $dbtables[teams] ON $dbtables[ships].team = $dbtables[teams].id  WHERE ship_destroyed='N' and email NOT LIKE '%@furangee' ORDER BY $by LIMIT $max_rank");
+
 //-------------------------------------------------------------------------------------------------
 
 if(!$res)
@@ -46,7 +53,7 @@ else
   echo "<BR>$l_ranks_pnum: " . NUMBER($num_players);
   echo "<BR>$l_ranks_dships<BR><BR>";
   echo "<TABLE BORDER=0 CELLSPACING=0 CELLPADDING=2>";
-  echo "<TR BGCOLOR=\"$color_header\"><TD><B>$l_ranks_rank</B></TD><TD><B><A HREF=ranking.php>$l_score</A></B></TD><TD><B>$l_player</B></TD><TD><B><A HREF=ranking.php?sort=turns>$l_turns_used</A></B></TD><TD><B><A HREF=ranking.php?sort=login>$l_ranks_lastlog</A></B></TD><TD><B><A HREF=ranking.php?sort=good>$l_ranks_good</A>/<A HREF=ranking.php?sort=bad>$l_ranks_evil</A></B></TD></TR>";
+  echo "<TR BGCOLOR=\"$color_header\"><TD><B>$l_ranks_rank</B></TD><TD><B><A HREF=ranking.php>$l_score</A></B></TD><TD><B>$l_player</B></TD><TD><B><A HREF=ranking.php?sort=turns>$l_turns_used</A></B></TD><TD><B><A HREF=ranking.php?sort=login>$l_ranks_lastlog</A></B></TD><TD><B><A HREF=ranking.php?sort=good>$l_ranks_good</A>/<A HREF=ranking.php?sort=bad>$l_ranks_evil</A></B></TD><TD><B><A HREF=ranking.php?sort=alliance>$l_team_alliance</A></B></TD></TR>\n";
   $color = $color_line1;
   while(!$res->EOF)
   {
@@ -57,7 +64,7 @@ else
     {
       $rating=-1*$rating;
     }
-    echo "<TR BGCOLOR=\"$color\"><TD>" . NUMBER($i) . "</TD><TD>" . NUMBER($row[score]) . "</TD><TD>$row[character_name]</TD><TD>" . NUMBER($row[turns_used]) . "</TD><TD>$row[last_login]</TD><TD>" . NUMBER($rating) . "</TD></TR>";
+    echo "<TR BGCOLOR=\"$color\"><TD>" . NUMBER($i) . "</TD><TD>" . NUMBER($row[score]) . "</TD><TD>$row[character_name]</TD><TD>" . NUMBER($row[turns_used]) . "</TD><TD>$row[last_login]</TD><TD>&nbsp;&nbsp;" . NUMBER($rating) . "</TD><TD>$row[team_name]&nbsp;</TD></TR>\n";
     if($color == $color_line1)
     {
       $color = $color_line2;
