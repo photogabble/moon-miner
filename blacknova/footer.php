61e47199d942c05ab79dd24f398605ac1c4c80f5
Dave Rowlands <nospoon@users.sourceforge.net>
2002-04-26 23:59:55 +0000

    added online user count

--- blacknova/footer.php
+++ blacknova/footer.php
@@ -1,4 +1,23 @@
-<BR><BR>
+<?
+global $db;
+connectdb();
+$res = $db->Execute("SELECT COUNT(*) as loggedin from $dbtables[ships] WHERE (UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP($dbtables[ships].last_login)) / 60 <= 5");
+$row = $res->fields;
+$online = $row[loggedin];
+?>
+<BR><CENTER>
+<?
+if($online == 1)
+{
+   echo "There is 1 player online.";
+}
+else
+{
+echo "There are $online players online.";
+}
+?>
+</CENTER>
+<BR>
 <TABLE WIDTH=100% BORDER=0 CELLSPACING=0 CELLPADDING=0>
 <TR>
 <TD><FONT COLOR=SILVER SIZE=-4><A HREF="http://www.sourceforge.net/projects/blacknova">BlackNova Traders</A></FONT></TD>
