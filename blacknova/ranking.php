6ac16c091ad81ac897a3d334a0ed49eb02ff7e7f
Dave Rowlands <nospoon@users.sourceforge.net>
2002-04-26 11:05:44 +0000

    added online display

--- blacknova/ranking.php
+++ blacknova/ranking.php
@@ -40,7 +40,7 @@ else
   $by="score DESC,character_name ASC";
 }
 
-$res = $db->Execute("SELECT $dbtables[ships].score,$dbtables[ships].character_name,$dbtables[ships].turns_used,$dbtables[ships].last_login,$dbtables[ships].rating, $dbtables[teams].team_name FROM $dbtables[ships] LEFT JOIN $dbtables[teams] ON $dbtables[ships].team = $dbtables[teams].id  WHERE ship_destroyed='N' and email NOT LIKE '%@furangee' ORDER BY $by LIMIT $max_rank");
+$res = $db->Execute("SELECT $dbtables[ships].score,$dbtables[ships].character_name,$dbtables[ships].turns_used,$dbtables[ships].last_login,UNIX_TIMESTAMP($dbtables[ships].last_login) as online,$dbtables[ships].rating, $dbtables[teams].team_name FROM $dbtables[ships] LEFT JOIN $dbtables[teams] ON $dbtables[ships].team = $dbtables[teams].id  WHERE ship_destroyed='N' and email NOT LIKE '%@furangee' ORDER BY $by LIMIT $max_rank");
 
 //-------------------------------------------------------------------------------------------------
 
@@ -53,7 +53,7 @@ else
   echo "<BR>$l_ranks_pnum: " . NUMBER($num_players);
   echo "<BR>$l_ranks_dships<BR><BR>";
   echo "<TABLE BORDER=0 CELLSPACING=0 CELLPADDING=2>";
-  echo "<TR BGCOLOR=\"$color_header\"><TD><B>$l_ranks_rank</B></TD><TD><B><A HREF=ranking.php>$l_score</A></B></TD><TD><B>$l_player</B></TD><TD><B><A HREF=ranking.php?sort=turns>$l_turns_used</A></B></TD><TD><B><A HREF=ranking.php?sort=login>$l_ranks_lastlog</A></B></TD><TD><B><A HREF=ranking.php?sort=good>$l_ranks_good</A>/<A HREF=ranking.php?sort=bad>$l_ranks_evil</A></B></TD><TD><B><A HREF=ranking.php?sort=alliance>$l_team_alliance</A></B></TD></TR>\n";
+  echo "<TR BGCOLOR=\"$color_header\"><TD><B>$l_ranks_rank</B></TD><TD><B><A HREF=ranking.php>$l_score</A></B></TD><TD><B>$l_player</B></TD><TD><B><A HREF=ranking.php?sort=turns>$l_turns_used</A></B></TD><TD><B><A HREF=ranking.php?sort=login>$l_ranks_lastlog</A></B></TD><TD><B><A HREF=ranking.php?sort=good>$l_ranks_good</A>/<A HREF=ranking.php?sort=bad>$l_ranks_evil</A></B></TD><TD><B><A HREF=ranking.php?sort=alliance>$l_team_alliance</A></B></TD><TD><B>Online</B></TD></TR>\n";
   $color = $color_line1;
   while(!$res->EOF)
   {
@@ -64,7 +64,12 @@ else
     {
       $rating=-1*$rating;
     }
-    echo "<TR BGCOLOR=\"$color\"><TD>" . NUMBER($i) . "</TD><TD>" . NUMBER($row[score]) . "</TD><TD>$row[character_name]</TD><TD>" . NUMBER($row[turns_used]) . "</TD><TD>$row[last_login]</TD><TD>&nbsp;&nbsp;" . NUMBER($rating) . "</TD><TD>$row[team_name]&nbsp;</TD></TR>\n";
+    $curtime = TIME();
+    $time = $row[online];
+    $difftime = ($curtime - $time) / 60;
+    $online = " ";
+    if($difftime <= 5) $online = "Online";
+    echo "<TR BGCOLOR=\"$color\"><TD>" . NUMBER($i) . "</TD><TD>" . NUMBER($row[score]) . "</TD><TD>$row[character_name]</TD><TD>" . NUMBER($row[turns_used]) . "</TD><TD>$row[last_login]</TD><TD>&nbsp;&nbsp;" . NUMBER($rating) . "</TD><TD>$row[team_name]&nbsp;</TD><TD>$online</TD></TR>\n";
     if($color == $color_line1)
     {
       $color = $color_line2;
