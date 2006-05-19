158122412da8f2c6d8c0a96361b97902651b42bd
Paul Kirby <themightydude@users.sourceforge.net>
2006-05-19 23:53:43 +0000

    [1491795] ip detection bug (fixed). For some reason on some servers it was not detecting the players IP, causing problems with the log, sending emails.

--- config.php
+++ config.php
@@ -44,7 +44,7 @@ $local_number_thousands_sep = ",";
 $language = "english";
 
 /* game variables */
-$ip = getenv("REMOTE_ADDR");
+$ip = $_SERVER['REMOTE_ADDR'];
 $mine_hullsize = 8; //Minimum size hull has to be to hit mines
 $ewd_maxhullsize = 15; //Max hull size before EWD degrades
 $sector_max = 5000;
