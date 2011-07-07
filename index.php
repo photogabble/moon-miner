980e51153ea3c93569fb44af717246d53cf73179
L. Patrick Smallwood <iamsure@users.sourceforge.net>
2011-07-07 09:40:42 +0000

    Remove php short tags

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<? 
+<?php
   include("config.php");
 
   if(empty($lang))
@@ -29,17 +29,17 @@
     <td><img src="images/spacer.gif" width="1" height="21" border="0" alt=""></td>
   </tr>
   <tr>
-    <td colspan=3 align=center><a href=<? echo "login.php"; ?> onMouseOut="MM_swapImgRestore()"  onMouseOver="MM_swapImage('login','','images/login_.gif',1);" ><img name="login" src="images/login.gif" width="146" height="58" border="0" alt="Login"></a></td>
+    <td colspan=3 align=center><a href=<?php echo "login.php"; ?> onMouseOut="MM_swapImgRestore()"  onMouseOver="MM_swapImage('login','','images/login_.gif',1);" ><img name="login" src="images/login.gif" width="146" height="58" border="0" alt="Login"></a></td>
     <td><img src="images/spacer.gif" width="1" height="58" border="0" alt=""></td>
   </tr>
   <tr>
-    <td colspan=3 align=center><a href=<? echo "\"mailto:$admin_mail\""; ?> onMouseOut="MM_swapImgRestore()"  onMouseOver="MM_swapImage('mail','','images/mail_.gif',1);" ><img name="mail" src="images/mail.gif" width="146" height="58" border="0" alt="Mail"></a></td>
+    <td colspan=3 align=center><a href=<?php echo "\"mailto:$admin_mail\""; ?> onMouseOut="MM_swapImgRestore()"  onMouseOver="MM_swapImage('mail','','images/mail_.gif',1);" ><img name="mail" src="images/mail.gif" width="146" height="58" border="0" alt="Mail"></a></td>
     <td><img src="images/spacer.gif" width="1" height="58" border="0" alt=""></td>
   </tr>
   <tr>
-  <td colspan=3 align=center><a href="faq.html"><? echo "$l_faq"; ?></a></td>
+  <td colspan=3 align=center><a href="faq.html"><?php echo "$l_faq"; ?></a></td>
   </tr>
   </table></center>
-<?
+<?php
 	include("footer.php");
 ?>
