c932c03a65daf94a5c1f6aaa9aeea7091197b4a5
The Kabal <thekabal@users.sourceforge.net>
2012-05-04 20:54:46 +0000

    Fix from TMD for news not showing list for day.

--- news.php
+++ news.php
@@ -18,53 +18,50 @@ if (!ereg ("([0-9]{4})/([0-9]{1,2})/([0-9]{1,2})", $startdate, $regs) || $startd
 
 $previousday = getpreviousday($startdate);
 $nextday = getnextday($startdate);
-?>
- <table width="73%" border="0" cellspacing="2" cellpadding="2">
-    <tr>
-      <td height="73" width="27%"><img src="images/bnnhead.gif" width="312" height="123" alt="The BlackNova Network"></td>
-      <td height="73" width="73%" bgcolor="#000000" valign="bottom" align="right">
-        <p><font size="-1"><?php echo $l_news_info?></font></p>
-        <p><?php echo $l_news_for ?> <?php echo $startdate?></p>
-      </td>
-    </tr>
-    <tr>
-      <td height="22" width="27%" bgcolor="#00001A">&nbsp;</td>
-      <td height="22" width="73%" bgcolor="#00001A" align="right"><a href="news.php?startdate=<?php echo $previousday ?>"><?php echo $l_news_prev ?></a> - <a href="news.php?startdate=<?php echo $nextday ?>"><?php echo $l_news_next ?></a></td>
-    </tr>
-<?php
+
+echo "<table width=\"73%\" border=\"0\" cellspacing=\"2\" cellpadding=\"2\">\n";
+echo "  <tr>\n";
+echo "    <td height=\"73\" width=\"27%\"><img src=\"images/bnnhead.gif\" width=\"312\" height=\"123\" alt=\"The BlackNova Network\"></td>\n";
+echo "    <td height=\"73\" width=\"73%\" bgcolor=\"#000000\" valign=\"bottom\" align=\"right\">\n";
+echo "      <p><font size=\"-1\">{$l_news_info}</font></p>\n";
+echo "      <p>{$l_news_for} {$startdate}</p>\n";
+echo "    </td>\n";
+echo "  </tr>\n";
+echo "  <tr>\n";
+echo "    <td height=\"22\" width=\"27%\" bgcolor=\"#00001A\">&nbsp;</td>\n";
+echo "    <td height=\"22\" width=\"73%\" bgcolor=\"#00001A\" align=\"right\"><a href=\"news.php?startdate={$previousday}\">{$l_news_prev}</a> - <a href=\"news.php?startdate={$nextday}\">{$l_news_next}</a></td>\n";
+echo "  </tr>\n";
 
 
 //Select news for date range
 $res = $db->Execute("SELECT * from $dbtables[news] WHERE date > '{$startdate} 00:00:00' AND date < '{$startdate} 23:59:59' order by news_id desc");
 
 //Check to see if there was any news to be shown
-if($res->EOF)
+if ($res->RecordCount() >0)
 {
-
-    //No news
-    echo "<tr><td bgcolor=\"#00001A\" align=\"center\">$l_news_flash</td><td bgcolor=\"#00001A\" align=\"right\">$l_news_none</td></tr></table><p align=left>";
-
-    //Display link to the main page
-    TEXT_GOTOMAIN();
-    die();
+    // Yes we do, now cycle through them.
+    while (!$res->EOF)
+    {
+        $row = $res->fields;
+        echo "  <tr>\n";
+        echo "    <td bgcolor=\"#000033\" align=\"center\" style=\"vertical-align:text-top;\">{$row['headline']}</td>\n";
+        echo "    <td bgcolor=\"#000033\" style=\"vertical-align:text-top;\"><p align=\"justify\">{$row['newstext']}</p><br></td>\n";
+        echo "  </tr>\n";
+        $res->MoveNext();
+    }
 }
-
-while (!$res->EOF) {
-  $row = $res->fields;
-?>
-<tr>
-      <td bgcolor="#000033" align="center"><?php echo $row[headline]?></td>
-      <td bgcolor="#000033">
-        <p align="justify"><?php echo $row[newstext]?></p><br>
-      </td>
-</tr>
-
-<?php
-  $res->MoveNext();
+else
+{
+    // Nope none found.
+    echo "  <tr>\n";
+	echo "    <td bgcolor=\"#00001A\" align=\"center\">$l_news_flash</td>\n";
+	echo "    <td bgcolor=\"#00001A\" align=\"right\">$l_news_none</td>\n";
+	echo "  </tr>\n";
 }
-?>
-</table>
-<p align=left>
-<?php
+echo "</table>\n";
+echo "<div style=\"height:16px;\"></div>\n";
+
 TEXT_GOTOMAIN();
+die();
+
 ?>
