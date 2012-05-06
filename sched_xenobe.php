76362d85902de0fbe598ea317c4ed0225cafad8d
The Kabal <thekabal@users.sourceforge.net>
2012-05-06 15:26:59 +0000

    Furangees are now Xenobes

--- sched_xenobe.php
+++ sched_xenobe.php
@@ -1,37 +1,40 @@
 <?php
 
-  if (preg_match("/sched_furangee.php/i", $PHP_SELF)) {
+  if (preg_match("/sched_xenobe.php/i", $PHP_SELF)) {
       echo "You can not access this file directly!";
       die();
   }
 
   // *********************************
-  // ***** FURANGEE TURN UPDATES *****
+  // ***** Xenobe TURN UPDATES *****
   // *********************************
-  echo "<BR><B>FURANGEE TURNS</B><BR><BR>";
+  echo "<BR><B>Xenobe TURNS</B><BR><BR>";
 
   // *********************************
   // ******* INCLUDE FUNCTIONS *******
   // *********************************
-  include_once("furangee_funcs.php");
+  include_once("xenobe_funcs.php");
   include_once("languages/$lang");
   global $targetlink;
-  global $furangeeisdead;
+  global $xenobeisdead;
 
   // *********************************
-  // **** MAKE FURANGEE SELECTION ****
+  // **** MAKE Xenobe SELECTION ****
   // *********************************
   $furcount = $furcount0 = $furcount0a = $furcount1 = $furcount1a = $furcount2 = $furcount2a = $furcount3 = $furcount3a = $furcount3h = 0;
-  $res = $db->Execute("SELECT * FROM $dbtables[players] JOIN $dbtables[furangee] LEFT JOIN $dbtables[ships] ON $dbtables[players].currentship=$dbtables[ships].ship_id WHERE email=furangee_id and active='Y' AND destroyed='N' ORDER BY sector_id");
-  echo $db->ErrorMsg() . "<BR>";
+
+// Lock the tables.
+  $db->Execute("LOCK TABLES $dbtables[xenobe] WRITE, $dbtables[ships] WRITE;");
+
+  $res = $db->Execute("SELECT * FROM {$dbtables['ships']} JOIN {$dbtables['xenobe']} WHERE email=xenobe_id and active='Y' and ship_destroyed='N' ORDER BY ship_id;");
   while(!$res->EOF)
   {
-    $furangeeisdead = 0;
+    $xenobeisdead = 0;
     $playerinfo = $res->fields;
     // *********************************
     // ****** REGENERATE/BUY STATS *****
     // *********************************
-    furangeeregen();
+    xenoberegen();
     // *********************************
     // ****** RUN THROUGH ORDERS *******
     // *********************************
@@ -44,10 +47,11 @@
       if ($playerinfo[orders] == 0)
       {
         $furcount0++;
-        $targetlink = $playerinfo[sector_id];
         // ****** FIND A TARGET ******
         // ****** IN MY SECTOR, NOT MYSELF, NOT ON A PLANET ******
-        $reso0 = $db->Execute("SELECT * FROM $dbtables[ships] LEFT JOIN $dbtables[players] USING(player_id) WHERE sector_id=$targetlink and email!='$playerinfo[email]' AND planet_id=0 AND $dbtables[players].player_id > 1");
+
+        $SQL = "SELECT * FROM $dbtables[ships] WHERE sector=$playerinfo[sector] AND email!='$playerinfo[email]' AND email NOT LIKE '%@xenobe' AND planet_id=0 AND ship_id > 1";
+        $reso0 = $db->Execute($SQL);
         if (!$reso0->EOF)
         {
           $rowo0 = $reso0->fields;
@@ -57,13 +61,13 @@
           }
           elseif ($playerinfo[aggression] == 1)        // ****** O = 0 & AGRESSION = 1 ATTACK SOMETIMES ******
           {
-            // Furangee's only compare number of fighters when determining if they have an attack advantage
-            if ($playerinfo[fighters] > $rowo0[fighters])
+            // Xenobe's only compare number of fighters when determining if they have an attack advantage
+            if ($playerinfo[ship_fighters] > $rowo0[ship_fighters])
             {
               $furcount0a++;
-              playerlog($playerinfo[player_id], LOG_FURANGEE_ATTACK, "$rowo0[character_name]");
-              furangeetoship($rowo0[ship_id]);
-              if ($furangeeisdead>0) {
+              playerlog($playerinfo[ship_id], LOG_Xenobe_ATTACK, "$rowo0[character_name]");
+              xenobetoship($rowo0[ship_id]);
+              if ($xenobeisdead>0) {
                 $res->MoveNext();
                 continue;
               }
@@ -72,9 +76,9 @@
           elseif ($playerinfo[aggression] == 2)        // ****** O = 0 & AGRESSION = 2 ATTACK ALLWAYS ******
           {
             $furcount0a++;
-            playerlog($playerinfo[player_id], LOG_FURANGEE_ATTACK, "$rowo0[character_name]");
-            furangeetoship($rowo0[ship_id]);
-            if ($furangeeisdead>0) {
+            playerlog($playerinfo[ship_id], LOG_Xenobe_ATTACK, "$rowo0[character_name]");
+            xenobetoship($rowo0[ship_id]);
+            if ($xenobeisdead>0) {
               $res->MoveNext();
               continue;
             }
@@ -88,15 +92,15 @@
       {
         $furcount1++;
         // ****** ROAM TO A NEW SECTOR BEFORE DOING ANYTHING ELSE ******
-        $targetlink = $playerinfo[sector_id];
-        furangeemove();
-        if ($furangeeisdead>0) {
+        $targetlink = $playerinfo[sector];
+        xenobemove();
+        if ($xenobeisdead>0) {
           $res->MoveNext();
           continue;
         }
         // ****** FIND A TARGET ******
         // ****** IN MY SECTOR, NOT MYSELF ******
-        $reso1 = $db->Execute("SELECT * FROM $dbtables[ships] LEFT JOIN $dbtables[players] USING(player_id) WHERE sector_id=$targetlink and email!='$playerinfo[email]' and $dbtables[players].player_id > 1");
+        $reso1 = $db->Execute("SELECT * FROM $dbtables[ships] WHERE sector=$targetlink and email!='$playerinfo[email]' and ship_id > 1");
         if (!$reso1->EOF)
         {
           $rowo1= $reso1->fields;
@@ -106,13 +110,13 @@
           }
           elseif ($playerinfo[aggression] == 1)        // ****** O = 1 & AGRESSION = 1 ATTACK SOMETIMES ******
           {
-            // Furangee's only compare number of fighters when determining if they have an attack advantage
-            if ($playerinfo[fighters] > $rowo1[fighters] && $rowo1[planet_id] == 0)
+            // Xenobe's only compare number of fighters when determining if they have an attack advantage
+            if ($playerinfo[ship_fighters] > $rowo1[ship_fighters] && $rowo1[planet_id] == 0)
             {
               $furcount1a++;
-              playerlog($playerinfo[player_id], LOG_FURANGEE_ATTACK, "$rowo1[character_name]");
-              furangeetoship($rowo1[ship_id]);
-              if ($furangeeisdead>0) {
+              playerlog($playerinfo[ship_id], LOG_Xenobe_ATTACK, "$rowo1[character_name]");
+              xenobetoship($rowo1[ship_id]);
+              if ($xenobeisdead>0) {
                 $res->MoveNext();
                 continue;
               }
@@ -121,13 +125,13 @@
           elseif ($playerinfo[aggression] == 2)        // ****** O = 1 & AGRESSION = 2 ATTACK ALLWAYS ******
           {
             $furcount1a++;
-            playerlog($playerinfo[player_id], LOG_FURANGEE_ATTACK, "$rowo1[character_name]");
+            playerlog($playerinfo[ship_id], LOG_Xenobe_ATTACK, "$rowo1[character_name]");
             if (!$rowo1[planet_id] == 0) {              // *** IS ON PLANET ***
-              furangeetoplanet($rowo1[planet_id]);
+              xenobetoplanet($rowo1[planet_id]);
             } else {
-              furangeetoship($rowo1[ship_id]);
+              xenobetoship($rowo1[ship_id]);
             }
-            if ($furangeeisdead>0) {
+            if ($xenobeisdead>0) {
               $res->MoveNext();
               continue;
             }
@@ -141,17 +145,17 @@
       {
         $furcount2++;
         // ****** ROAM TO A NEW SECTOR BEFORE DOING ANYTHING ELSE ******
-        $targetlink = $playerinfo[sector_id];
-        furangeemove();
-        if ($furangeeisdead>0) {
+        $targetlink = $playerinfo[sector];
+        xenobemove();
+        if ($xenobeisdead>0) {
           $res->MoveNext();
           continue;
         }
         // ****** NOW TRADE BEFORE WE DO ANY AGGRESSION CHECKS ******
-        furangeetrade();
+        xenobetrade();
         // ****** FIND A TARGET ******
         // ****** IN MY SECTOR, NOT MYSELF ******
-        $reso2 = $db->Execute("SELECT * FROM $dbtables[ships] LEFT JOIN $dbtables[players] USING(player_id) WHERE sector_id=$targetlink and email!='$playerinfo[email]' and $dbtables[players].player_id > 1");
+        $reso2 = $db->Execute("SELECT * FROM $dbtables[ships] WHERE sector=$targetlink and email!='$playerinfo[email]' and ship_id > 1");
         if (!$reso2->EOF)
         {
           $rowo2=$reso2->fields;
@@ -161,13 +165,13 @@
           }
           elseif ($playerinfo[aggression] == 1)        // ****** O = 2 & AGRESSION = 1 ATTACK SOMETIMES ******
           {
-            // Furangee's only compare number of fighters when determining if they have an attack advantage
-            if ($playerinfo[fighters] > $rowo2[fighters] && $rowo2[planet_id] == 0)
+            // Xenobe's only compare number of fighters when determining if they have an attack advantage
+            if ($playerinfo[ship_fighters] > $rowo2[ship_fighters] && $rowo2[planet_id] == 0)
             {
               $furcount2a++;
-              playerlog($playerinfo[player_id], LOG_FURANGEE_ATTACK, "$rowo2[character_name]");
-              furangeetoship($rowo2[ship_id]);
-              if ($furangeeisdead>0) {
+              playerlog($playerinfo[ship_id], LOG_Xenobe_ATTACK, "$rowo2[character_name]");
+              xenobetoship($rowo2[ship_id]);
+              if ($xenobeisdead>0) {
                 $res->MoveNext();
                 continue;
               }
@@ -176,13 +180,13 @@
           elseif ($playerinfo[aggression] == 2)        // ****** O = 2 & AGRESSION = 2 ATTACK ALLWAYS ******
           {
             $furcount2a++;
-            playerlog($playerinfo[player_id], LOG_FURANGEE_ATTACK, "$rowo2[character_name]");
+            playerlog($playerinfo[ship_id], LOG_Xenobe_ATTACK, "$rowo2[character_name]");
             if (!$rowo2[planet_id] == 0) {              // *** IS ON PLANET ***
-              furangeetoplanet($rowo2[planet_id]);
+              xenobetoplanet($rowo2[planet_id]);
             } else {
-              furangeetoship($rowo2[ship_id]);
+              xenobetoship($rowo2[ship_id]);
             }
-            if ($furangeeisdead>0) {
+            if ($xenobeisdead>0) {
               $res->MoveNext();
               continue;
             }
@@ -202,23 +206,22 @@
         if ($hunt==0)
         {
         $furcount3h++;
-        furangeehunter();
-        if ($furangeeisdead>0) {
+        xenobehunter();
+        if ($xenobeisdead>0) {
           $res->MoveNext();
           continue;
         }
         } else
         {
           // ****** ROAM TO A NEW SECTOR BEFORE DOING ANYTHING ELSE ******
-          $targetlink = $playerinfo[sector_id];
-          furangeemove();
-          if ($furangeeisdead>0) {
+          xenobemove();
+          if ($xenobeisdead>0) {
             $res->MoveNext();
             continue;
           }
           // ****** FIND A TARGET ******
           // ****** IN MY SECTOR, NOT MYSELF ******
-          $reso3 = $db->Execute("SELECT * FROM $dbtables[ships] LEFT JOIN $dbtables[players] USING(player_id) WHERE sector_id=$targetlink and email!='$playerinfo[email]' and $dbtables[players].player_id > 1");
+          $reso3 = $db->Execute("SELECT * FROM $dbtables[ships] WHERE sector=$playerinfo[sector] and email!='$playerinfo[email]' and ship_id > 1");
           if (!$reso3->EOF)
           {
             $rowo3=$reso3->fields;
@@ -228,13 +231,13 @@
             }
             elseif ($playerinfo[aggression] == 1)        // ****** O = 3 & AGRESSION = 1 ATTACK SOMETIMES ******
             {
-              // Furangee's only compare number of fighters when determining if they have an attack advantage
-              if ($playerinfo[fighters] > $rowo3[fighters] && $rowo3[planet_id] == 0)
+              // Xenobe's only compare number of fighters when determining if they have an attack advantage
+              if ($playerinfo[ship_fighters] > $rowo3[ship_fighters] && $rowo3[planet_id] == 0)
               {
                 $furcount3a++;
-                playerlog($playerinfo[player_id], LOG_FURANGEE_ATTACK, "$rowo3[character_name]");
-                furangeetoship($rowo3[ship_id]);
-                if ($furangeeisdead>0) {
+                playerlog($playerinfo[ship_id], LOG_Xenobe_ATTACK, "$rowo3[character_name]");
+                xenobetoship($rowo3[ship_id]);
+                if ($xenobeisdead>0) {
                   $res->MoveNext();
                   continue;
                 }
@@ -243,13 +246,13 @@
             elseif ($playerinfo[aggression] == 2)        // ****** O = 3 & AGRESSION = 2 ATTACK ALLWAYS ******
             {
               $furcount3a++;
-              playerlog($playerinfo[player_id], LOG_FURANGEE_ATTACK, "$rowo3[character_name]");
+              playerlog($playerinfo[ship_id], LOG_Xenobe_ATTACK, "$rowo3[character_name]");
               if (!$rowo3[planet_id] == 0) {              // *** IS ON PLANET ***
-                furangeetoplanet($rowo3[planet_id]);
+                xenobetoplanet($rowo3[planet_id]);
               } else {
-                furangeetoship($rowo3[ship_id]);
+                xenobetoship($rowo3[ship_id]);
               }
-              if ($furangeeisdead>0) {
+              if ($xenobeisdead>0) {
                 $res->MoveNext();
                 continue;
               }
@@ -261,19 +264,21 @@
     $res->MoveNext();
   }
   $res->_close();
+
   $furnonmove = $furcount - ($furcount0 + $furcount1 + $furcount2 + $furcount3);
-  echo "Counted $furcount Furangee players that are ACTIVE with working ships.<BR>";
-  echo "$furnonmove Furangee players did not do anything this round. <BR>";
-  echo "$furcount0 Furangee players had SENTINEL orders of which $furcount0a launched attacks. <BR>";
-  echo "$furcount1 Furangee players had ROAM orders of which $furcount1a launched attacks. <BR>";
-  echo "$furcount2 Furangee players had ROAM AND TRADE orders of which $furcount2a launched attacks. <BR>";
-  echo "$furcount3 Furangee players had ROAM AND HUNT orders of which $furcount3a launched attacks and $furcount3h went hunting. <BR>";
-  echo "FURANGEE TURNS COMPLETE. <BR>";
+  echo "Counted $furcount Xenobe players that are ACTIVE with working ships.<BR>";
+  echo "$furnonmove Xenobe players did not do anything this round. <BR>";
+  echo "$furcount0 Xenobe players had SENTINEL orders of which $furcount0a launched attacks. <BR>";
+  echo "$furcount1 Xenobe players had ROAM orders of which $furcount1a launched attacks. <BR>";
+  echo "$furcount2 Xenobe players had ROAM AND TRADE orders of which $furcount2a launched attacks. <BR>";
+  echo "$furcount3 Xenobe players had ROAM AND HUNT orders of which $furcount3a launched attacks and $furcount3h went hunting. <BR>";
+  echo "Xenobe TURNS COMPLETE. <BR>";
   echo "<BR>";
   // *********************************
-  // ***** END OF FURANGEE TURNS *****
+  // ***** END OF Xenobe TURNS *****
   // *********************************
 
-  $db->Execute("UNLOCK TABLES"); //some unsafe returns in funcs
+// Unlock the tables.
+$db->Execute("UNLOCK TABLES;");
 
 ?>
