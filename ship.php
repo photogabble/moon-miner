501481eba7fab2dd9a1f697f30e637c8775ec027
The Kabal <thekabal@users.sourceforge.net>
2012-05-06 02:18:05 +0000

    Improved SQL selects, and only show attack link depending on team status

--- ship.php
+++ ship.php
@@ -11,28 +11,32 @@ connectdb();
 
 if(checklogin())
 {
-  die();
+    die();
 }
 
-
-$res = $db->Execute("SELECT sector FROM $dbtables[ships] WHERE email='$username'");
+$res = $db->Execute("SELECT team, ship_name, character_name, sector FROM $dbtables[ships] WHERE email='$username'");
 $playerinfo = $res->fields;
-$res2 = $db->Execute("SELECT ship_name, character_name, sector FROM $dbtables[ships] WHERE ship_id=$ship_id");
+$res2 = $db->Execute("SELECT team, ship_name, character_name, sector FROM $dbtables[ships] WHERE ship_id=$ship_id");
 $othership = $res2->fields;
 
 bigtitle();
 
 if($othership[sector] != $playerinfo[sector])
 {
-  echo "$l_ship_the <font color=white>", $othership[ship_name],"</font> $l_ship_nolonger ", $playerinfo[sector], "<BR>";
+    echo "$l_ship_the <font color=white>{$othership[ship_name]}</font> $l_ship_nolonger {$playerinfo[sector]}<BR>";
 }
 else
 {
-	echo "$l_ship_youc <font color=white>", $othership[ship_name], "</font>, $l_ship_owned <font color=white>", $othership[character_name],"</font>.<br><br>";
-	echo "$l_ship_perform<BR><BR>";
-	echo "<a href=scan.php?ship_id=$ship_id>$l_planet_scn_link</a><br>";
-	echo "<a href=attack.php?ship_id=$ship_id>$l_planet_att_link</a><br>";
-	echo "<a href=mailto.php?to=$ship_id>$l_send_msg</a><br>";
+    echo "$l_ship_youc <font color=white>{$othership[ship_name]}</font>, $l_ship_owned <font color=white>{$othership[character_name]}</font>.<br><br>";
+    echo "$l_ship_perform<BR><BR>";
+    echo "<a href=scan.php?ship_id=$ship_id>$l_planet_scn_link</a><br>";
+
+    if( !isSameTeam($playerinfo['team'], $othership['team']) )
+    {
+        echo "<a href=attack.php?ship_id=$ship_id>$l_planet_att_link</a><br>";
+    }
+
+    echo "<a href=mailto.php?to=$ship_id>$l_send_msg</a><br>";
 }
 
 echo "<BR>";
