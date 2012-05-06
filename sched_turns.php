a5ba078e6727db4a9a879319a732a7b4f9a37382
The Kabal <thekabal@users.sourceforge.net>
2012-05-06 15:31:57 +0000

    Improved coding (server var instead of phpself, improved sql, etc)

--- sched_turns.php
+++ sched_turns.php
@@ -1,15 +1,16 @@
 <?php
 
-  if (preg_match("/sched_turns.php/i", $PHP_SELF)) {
+  if (preg_match("/sched_turns.php/i", $_SERVER['PHP_SELF']))
+  {
       echo "You can not access this file directly!";
       die();
   }
 
   echo "<B>TURNS</B><BR><BR>";
   echo "Adding turns...";
-  QUERYOK($db->Execute("UPDATE $dbtables[ships] SET turns=turns+(1*$multiplier) WHERE turns<$max_turns"));
+  QUERYOK($db->Execute("UPDATE $dbtables[ships] SET turns = turns + ($turns_per_tick * $multiplier) WHERE turns < $max_turns;"));
   echo "Ensuring maximum turns are $max_turns...";
-  QUERYOK($db->Execute("UPDATE $dbtables[ships] SET turns=$max_turns WHERE turns>$max_turns"));
+  QUERYOK($db->Execute("UPDATE $dbtables[ships] SET turns = $max_turns WHERE turns > $max_turns;"));
   echo "<BR>";
   $multiplier = 0;
 
