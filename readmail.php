716b2ba6ef8952298789f5ab6a6b2e2e6fe0d2fc
The Kabal <thekabal@users.sourceforge.net>
2012-05-13 23:13:03 +0000

    Mostly cosmetic changes, also adds "validated as admin"

--- readmail.php
+++ readmail.php
@@ -44,8 +44,8 @@ $res = $db->Execute("SELECT * FROM $dbtables[messages] WHERE recp_id='".$playeri
                 <div align="center">
                   <table border="1" cellspacing="1" width="100%" bgcolor="gray" bordercolorlight="black" bordercolordark="silver">
                     <tr>
-                      <td width="75%" align="left"><font color="white" size="2"><b><?php echo $l_readm_center ?></b></font></td>
-                      <td width="21%" align="center" nowrap><font color="white" size="2"><?php echo "$cur_D" ?>&nbsp;<?php echo "$cur_T" ?></font></td>
+                      <td width="75%" align="left"><font color="white" size="2"><b><? echo $l_readm_center ?> (<span style='color:#00C0C0;'>Subspace</span>)</b></font></td>
+                      <td width="21%" align="center" nowrap><font color="white" size="2"><?echo "$cur_D" ?>&nbsp;<?echo "$cur_T" ?></font></td>
                       <td width="4%" align="center" bordercolorlight="black" bordercolordark="gray"><A HREF="main.php"><img alt="Click here to return to the main menu" src="images/c95x.gif" width="16" height="14" border="0"></a></td>
                     </tr>
                   </table>
@@ -63,7 +63,7 @@ $res = $db->Execute("SELECT * FROM $dbtables[messages] WHERE recp_id='".$playeri
                 <div align="center">
                   <table border="1" cellspacing="1" width="100%" bgcolor="white" bordercolorlight="black" bordercolordark="silver">
                     <tr>
-                      <td width="100%" align="center" bgcolor="white"><font color="red"><?php echo $l_readm_nomessage ?></font></td>
+                      <td width="100%" align="center" bgcolor="white"><font color="red"><? echo $l_readm_nomessage ?></font></td>
                     </tr>
                   </table>
                 </div>
@@ -79,6 +79,7 @@ $res = $db->Execute("SELECT * FROM $dbtables[messages] WHERE recp_id='".$playeri
    $msg = $res->fields;
    $result = $db->Execute("SELECT * FROM $dbtables[ships] WHERE ship_id='".$msg[sender_id]."'");
    $sender = $result->fields;
+   $isAdmin = isAdmin($sender);
 ?>
             <tr>
               <td width="100%" align="center" bgcolor="black" height="4"></td>
@@ -88,10 +89,18 @@ $res = $db->Execute("SELECT * FROM $dbtables[messages] WHERE recp_id='".$playeri
                 <div align="center">
                   <table border="0" cellspacing="1" width="100%" bgcolor="gray" cellpadding="0">
                     <tr>
-                      <td width="20%"><font color="white" size="2"><b><?php echo $l_readm_sender; ?></b></td>
-                      <td width="55%"><font color="yellow" size="2"><?php echo $sender[character_name]; ?></font></td>
-                      <td width="21%" align="center"><font color="white" size="2"><?php echo "$msg[sent]" ?></font></td>
-                      <td width="4%" align="center" bordercolorlight="black" bordercolordark="gray"><A class="but" HREF="readmail.php?action=delete&ID=<?php echo $msg[ID]; ?>"><img src="images/c95x.gif" width="16" height="14" border="0"></a></td>
+                      <td width="20%" style="text-align:left;"><font color="white" size="2"><b><? echo $l_readm_sender; ?></b></td>
+                      <td width="55%" style="text-align:left;"><font color="yellow" size="2">
+<?php
+echo "<span style='vertical-align:middle;'>{$sender['character_name']}</span>";
+if ($isAdmin === true)
+{
+    echo "&nbsp;<img style='border:none; padding:0px; vertical-align:text-bottom;' src='images/validated_administrator2.gif' alt='Validated as Admin' />";
+}
+?>
+</font></td>
+                      <td width="21%" align="center"><font color="white" size="2"><? echo "$msg[sent]" ?></font></td>
+                      <td width="4%" align="center" bordercolorlight="black" bordercolordark="gray"><A class="but" HREF="readmail.php?action=delete&ID=<? echo $msg[ID]; ?>"><img src="images/c95x.gif" width="16" height="14" border="0"></a></td>
                     </tr>
                   </table>
                 </div>
@@ -102,8 +111,8 @@ $res = $db->Execute("SELECT * FROM $dbtables[messages] WHERE recp_id='".$playeri
                 <div align="center">
                   <table border="0" cellspacing="1" width="100%" bgcolor="gray" cellpadding="0">
                     <tr>
-                      <td width="20%"><font color="white" size="2"><b><?php echo $l_readm_captn ?></b></font></td>
-                      <td width="80%"><font color="yellow" size="2"><?php echo $sender[ship_name] ?></font></td>
+                      <td width="20%" style="text-align:left;"><font color="white" size="2"><b><? echo $l_readm_captn ?></b></font></td>
+                      <td width="80%" style="text-align:left;"><font color="yellow" size="2"><? echo $sender[ship_name] ?></font></td>
                     </tr>
                   </table>
                 </div>
@@ -114,8 +123,8 @@ $res = $db->Execute("SELECT * FROM $dbtables[messages] WHERE recp_id='".$playeri
                 <div align="center">
                   <table border="0" cellspacing="1" width="100%" bgcolor="gray" cellpadding="0">
                     <tr>
-                      <td width="20%"><font color="white" size="2"><b>Subject</b></font></td>
-                      <td width="80%"><b><font color="yellow" size="2"><?php echo $msg[subject]; ?></font></b></td>
+                      <td width="20%" style="text-align:left;"><font color="white" size="2"><b>Subject</b></font></td>
+                      <td width="80%" style="text-align:left;"><b><font color="yellow" size="2"><? echo $msg[subject]; ?></font></b></td>
                     </tr>
                   </table>
                 </div>
@@ -126,7 +135,7 @@ $res = $db->Execute("SELECT * FROM $dbtables[messages] WHERE recp_id='".$playeri
                 <div align="center">
                   <table border="1" cellspacing="1" width="100%" bgcolor="white" bordercolorlight="black" bordercolordark="silver">
                     <tr>
-                      <td width="100%"><font color="black" size="2"><?php echo nl2br($msg[message]); ?></font></td>
+                      <td width="100%" style="text-align:left; vertical-align:text-top;"><font color="black" size="2"><? echo nl2br($msg[message]); ?></font></td>
                     </tr>
                   </table>
                 </div>
@@ -137,8 +146,8 @@ $res = $db->Execute("SELECT * FROM $dbtables[messages] WHERE recp_id='".$playeri
                 <div align="center">
                   <table border="1" cellspacing="1" width="100%" bgcolor="gray" bordercolorlight="black" bordercolordark="silver" cellpadding="0">
                     <tr>
-                      <td width="100%" align="center" valign="middle"><A class="but" HREF="readmail.php?action=delete&ID=<?php echo $msg[ID]; ?>"><?php echo $l_readm_del ?></A> |
-        <A class="but" HREF="mailto2.php?name=<?php echo $sender[character_name]; ?>&subject=<?php echo $msg[subject] ?>"><?php echo $l_readm_repl ?></A>
+                      <td width="100%" align="center" valign="middle"><A class="but" HREF="readmail.php?action=delete&ID=<? echo $msg[ID]; ?>"><? echo $l_readm_del ?></A> |
+        <A class="but" HREF="mailto2.php?name=<? echo $sender[character_name]; ?>&subject=<? echo $msg[subject] ?>"><? echo $l_readm_repl ?></A>
                       </td>
                     </tr>
                   </table>
@@ -158,14 +167,13 @@ $res = $db->Execute("SELECT * FROM $dbtables[messages] WHERE recp_id='".$playeri
                 <div align="center">
                   <table border="1" cellspacing="1" width="100%" bgcolor="#808080" bordercolorlight="#000000" bordercolordark="#C0C0C0" height="8">
                     <tr>
-                      <td width="50%"><p align="left"><font color="#FFFFFF" size="2">Mail Reader</font></td>
+                      <td width="50%"><p align="left"><font color="#FFFFFF" size="2">Mail Reader </font></td>
                       <td width="50%"><p align="right"><font color="#FFFFFF" size="2"><A class="but" HREF="readmail.php?action=delete_all">Delete All</a></font></td>
                     </tr>
                   </table>
                 </div>
               </td>
             </tr>
-
           </table>
           </center>
         </div>
