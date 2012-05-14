fde0044fd83fbfafcc1afc54da0a15a71fd16b1c
The Kabal <thekabal@users.sourceforge.net>
2012-05-14 00:03:07 +0000

    Mostly cosmetic changes, table layout. One SQL fix for selecting players

--- mailto2.php
+++ mailto2.php
@@ -20,34 +20,54 @@ bigtitle();
 
 if(empty($content))
 {
-  $res = $db->Execute("SELECT character_name FROM $dbtables[ships] WHERE email NOT LIKE '%@Xenobe' ORDER BY character_name ASC");
-  $res2 = $db->Execute("SELECT team_name FROM $dbtables[teams] ORDER BY team_name ASC");
-  echo "<FORM ACTION=mailto2.php METHOD=POST>";
-  echo "<TABLE>";
-  echo "<TR><TD>$l_sendm_to:</TD><TD><SELECT NAME=to>";
+  $res = $db->Execute("SELECT character_name FROM $dbtables[ships] WHERE email NOT LIKE '%@Xenobe' AND ship_destroyed ='N' AND turns_used > 0 AND ship_id <> {$playerinfo['ship_id']} ORDER BY character_name ASC");
+  $res2 = $db->Execute("SELECT team_name FROM $dbtables[teams] WHERE admin ='N' ORDER BY team_name ASC");
+  echo "<FORM ACTION=mailto2.php METHOD=POST>\n";
+  echo "  <TABLE>\n";
+  echo "    <TR>\n";
+  echo "      <TD>$l_sendm_to:</TD>\n";
+  echo "      <TD>\n";
+  echo "        <SELECT NAME=to style='width:200px;'>\n";
+
+  # Add self to list.
+  echo "          <OPTION".(($playerinfo['character_name']==$name)?" selected":"").">{$playerinfo['character_name']}</OPTION>\n";
+
   while(!$res->EOF)
   {
     $row = $res->fields;
-  ?>
-    <OPTION <?php if ($row[character_name]==$name) echo "selected" ?>><?php echo $row[character_name] ?></OPTION>
-  <?php
+    echo "          <OPTION".(($row['character_name']==$name)?" selected":"").">{$row['character_name']}</OPTION>\n";
     $res->MoveNext();
   }
+
   while(!$res2->EOF)
   {
     $row2 = $res2->fields;
-    echo "<OPTION>$l_sendm_ally $row2[team_name]</OPTION>";
+    echo "          <OPTION>$l_sendm_ally $row2[team_name]</OPTION>\n";
     $res2->MoveNext();
   }
 
-  echo "</SELECT></TD></TR>";
-  echo "<TR><TD>$l_sendm_from:</TD><TD><INPUT DISABLED TYPE=TEXT NAME=dummy SIZE=40 MAXLENGTH=40 VALUE=\"$playerinfo[character_name]\"></TD></TR>";
+  echo "        </SELECT>\n";
+  echo "      </TD>\n";
+  echo "    </TR>\n";
+  echo "    <TR>\n";
+  echo "      <TD>$l_sendm_from:</TD>\n";
+  echo "      <TD><INPUT DISABLED TYPE=TEXT NAME=dummy SIZE=40 MAXLENGTH=40 VALUE=\"$playerinfo[character_name]\"></TD>\n";
+  echo "    </TR>\n";
   if (isset($subject)) $subject = "RE: " . $subject;
-  echo "<TR><TD>$l_sendm_subj:</TD><TD><INPUT TYPE=TEXT NAME=subject SIZE=40 MAXLENGTH=40 VALUE=\"$subject\"></TD></TR>";
-  echo "<TR><TD>$l_sendm_mess:</TD><TD><TEXTAREA NAME=content ROWS=5 COLS=40></TEXTAREA></TD></TR>";
-  echo "<TR><TD></TD><TD><INPUT TYPE=SUBMIT VALUE=$l_sendm_send><INPUT TYPE=RESET VALUE=$l_reset></TD>";
-  echo "</TABLE>";
-  echo "</FORM>";
+  echo "    <TR>\n";
+  echo "      <TD>$l_sendm_subj:</TD>\n";
+  echo "      <TD><INPUT TYPE=TEXT NAME=subject SIZE=40 MAXLENGTH=40 VALUE=\"$subject\"></TD>\n";
+  echo "    </TR>\n";
+  echo "    <TR>\n";
+  echo "      <TD>$l_sendm_mess:</TD>\n";
+  echo "      <TD><TEXTAREA NAME=content ROWS=5 COLS=40></TEXTAREA></TD>\n";
+  echo "    </TR>";
+  echo "    <TR>\n";
+  echo "      <TD></TD>\n";
+  echo "      <TD><INPUT TYPE=SUBMIT VALUE=$l_sendm_send><INPUT TYPE=RESET VALUE=$l_reset></TD>\n";
+  echo "    </TR>\n";
+  echo "  </TABLE>\n";
+  echo "</FORM>\n";
 }
 else
 {
@@ -59,8 +79,13 @@ if (strpos($to, $l_sendm_ally)===false)
   $res = $db->Execute("SELECT * FROM $dbtables[ships] WHERE character_name='$to'");
   $target_info = $res->fields;
   $content = htmlspecialchars($content);
+  $content = addslashes($content);
   $subject = htmlspecialchars($subject);
   $db->Execute("INSERT INTO $dbtables[messages] (sender_id, recp_id, sent, subject, message) VALUES ('".$playerinfo[ship_id]."', '".$target_info[ship_id]."', '".$timestamp."', '".$subject."', '".$content."')");
+  if(mysql_errno() != 0)
+  {
+    echo "Message failed to send.<br />\n";
+  }
 }
 else
 {
