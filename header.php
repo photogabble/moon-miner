5fce1fb584d565c834402f5320f191c5261f5e89
The Kabal <thekabal@users.sourceforge.net>
2012-05-14 02:41:27 +0000

    Lots of cosmetic fixes, CSS additions, new meta tags, new javascript

--- header.php
+++ header.php
@@ -12,19 +12,28 @@
 <meta name="Description" content="A free online game - Open source, web game, with multiplayer space exploration">
 <meta name="Keywords" content="Free, online, game, Open source, web game, multiplayer, space, exploration, blacknova, traders">
 <meta name="Rating" content="General">
+<link rel="shortcut icon" href="images/myicon.ico" />
 <title><?php echo $title; ?></title>
  <style type="text/css">
  <!--
 <?php
-if($interface == "")
+if(!isset($interface) || $interface == "")
 {
   $interface = "main.php";
 }
 
-if($interface == "main.php" || $interface == "index.php")
-{
+if(isset($interface) && $interface == "main.php")
+{ 
+
   echo "  a.mnu {text-decoration:none; font-size: 8Pt; font-family: Verdana, Arial, sans-serif; color:white; font-weight:bold;}
   a.mnu:hover {text-decoration:none; font-size: 8Pt; font-family: Verdana, Arial, sans-serif; color:#3366ff; font-weight:bold;}
+
+  img.mnu {padding:4px; border:transparent 2px dashed;}
+  img.mnu:hover {padding:4px; border:#FF0000 2px dashed;}
+  
+  a.new_link {text-decoration:none; font-size: 8Pt; font-family: Verdana, Arial, sans-serif; color:#00FF00; font-weight:bold;}
+  a.new_link:hover {text-decoration:none; font-size: 8Pt; font-family: Verdana, Arial, sans-serif; color:#3366ff; font-weight:bold;}
+  
   div.mnu {text-decoration:none; font-size: 8Pt; font-family: Verdana, Arial, sans-serif; color:white; font-weight:bold;}
   span.mnu {text-decoration:none; font-size: 8Pt; font-family: Verdana, Arial, sans-serif; color:white; font-weight:bold;}
   a.dis {text-decoration:none; font-size: 8Pt; font-family: Verdana, Arial, sans-serif; color:silver; font-weight:bold;}
@@ -35,57 +44,144 @@ if($interface == "main.php" || $interface == "index.php")
   .headlines {text-decoration:none; font-size:8Pt; font-family: Verdana, Arial, sans-serif; font-weight:bold; color:white;}
   .portcosts1 {width:7em; border-style:none; font-family: Verdana, Arial, sans-serif; font-size:12pt; background-color:$color_line1; color:#c0c0c0;}
   .portcosts2 {width:7em; border-style:none; font-family: Verdana, Arial, sans-serif; font-size:12pt; background-color:$color_line2; color:#c0c0c0;}
-  .faderlines {background-color:$color_line2;}";
+  .faderlines {background-color:$color_line2;}\n";
+
+// Added for dev small text forthe rankings page
+  echo "  .rank_dev_text {text-decoration:none; color:#FF0000; vertical-align:middle; font-family:Verdana, Arial, Helvetica, sans-serif; font-size:10px;}\n";
+
 }
-echo "\n  body {font-family: Verdana, Arial, sans-serif; font-size: x-small;}\n";
+#echo "a:link {color: #00FF00;}\n";
+#echo "a:visited {color: #00FF00;}\n";
+#echo "a:hover {color: #0066FF;}\n";
+#echo "a:active {color: #FF0000;}\n";
+
+echo "\n  body {font-family: Verdana, Arial, sans-serif; font-size: 12px; color: #CCCCCC;}\n";
 ?>
  -->
  </style>
+
 <?php
-if ($interface == "index.php")
+
+/* Java functions for index.php used for button images */
+if (isset($interface) && $interface == "index.php")
 {
+
 ?>
-<SCRIPT LANGUAGE="JavaScript" type="text/javascript">
-<!--
-function MM_swapImgRestore() { //v3.0
-  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
-}
+  <SCRIPT LANGUAGE="JavaScript" type="text/javascript">
+  <!--
+  function MM_swapImgRestore()
+  { //v3.0
+      var i,x,a=document.MM_sr;
+      for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++)
+      {
+          x.src=x.oSrc;
+      }
+  }
 
-function MM_preloadImages() { //v3.0
-  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
-    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
-    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
-}
+  function MM_preloadImages()
+  { //v3.0
+      var d=document;
+      if(d.images)
+      {
+          if(!d.MM_p)
+          {
+              d.MM_p=new Array();
+          }
 
-function MM_findObj(n, d) { //v4.0
-  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
-    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
-  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
-  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
-  if(!x && document.getElementById) x=document.getElementById(n); return x;
-}
+          var i,j=d.MM_p.length,a=MM_preloadImages.arguments;
+          for(i=0; i<a.length; i++)
+          {
+              if (a[i].indexOf("#")!=0)
+              {
+                  d.MM_p[j]=new Image;
+                  d.MM_p[j++].src=a[i];
+              }
+          }
+      }
+  }
 
-function MM_swapImage() { //v3.0
-  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
-   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
-}
+  function MM_findObj(n, d)
+  { //v4.0
+      var p,i,x;
+      if(!d)
+      {
+          d=document;
+      }
+
+      if((p=n.indexOf("?"))>0&&parent.frames.length)
+      {
+          d=parent.frames[n.substring(p+1)].document;
+          n=n.substring(0,p);
+      }
+
+      if(!(x=d[n])&&d.all)
+      {
+          x=d.all[n];
+      }
+
+      for (i=0;!x&&i<d.forms.length;i++)
+      {
+          x=d.forms[i][n];
+      }
+
+      for(i=0;!x&&d.layers&&i<d.layers.length;i++)
+      {
+          x=MM_findObj(n,d.layers[i].document);
+      }
+
+      if(!x && document.getElementById)
+      {
+          x=document.getElementById(n);
+      }
+      return x;
+  }
+
+  function MM_swapImage()
+  { //v3.0
+      var i,j=0,x,a=MM_swapImage.arguments;
+      document.MM_sr=new Array;
+      for(i=0;i<(a.length-2);i+=3)
+      {
+          if ((x=MM_findObj(a[i]))!=null)
+          {
+              document.MM_sr[j++]=x;
+              if(!x.oSrc)
+              {
+                  x.oSrc=x.src;
+              }
+              x.src=a[i+2];
+          }
+      }
+  }
+
 //-->
 </script>
 <?php
 }
-echo "</head>";
+/* End of Java functions */
+?>
+
+</head>
+
+<?php
 
 if(empty($no_body))
 {
-  if($interface=="main.php")
-  {
-    echo "<body background=\"images/bgoutspace1.gif\" bgcolor=\"#000000\" text=\"#c0c0c0\" link=\"#00ff00\" vlink=\"#00ff00\" alink=\"#ff0000\" id=\"bntbody\">";
-  }
-  else
-  {
-    echo "<body background=\"\" bgcolor=\"#000000\" text=\"#c0c0c0\" link=\"#00ff00\" vlink=\"#808080\" alink=\"#ff0000\" id=\"bntbody\">";
-  }
+    if(isset($interface) && $interface=="main.php")
+    {
+        echo "<body style=\"background-image: url('images/bgoutspace1.gif'); background-color:#000000; color:#C0C0C0;\" link=\"#00ff00\" vlink=\"#00ff00\" alink=\"#ff0000\">";
+    }
+    else
+    {
+        echo "<body background=\"\" bgcolor=\"#000000\" text=\"#c0c0c0\" link=\"#00ff00\" vlink=\"#808080\" alink=\"#ff0000\">";
+    }
 }
+else
+{
+    echo "<body bgcolor=\"#666666\" text=\"#F0F0F0\" link=\"#00ff00\" vlink=\"#00ff00\" alink=\"#ff0000\">";
+}
+
 echo "\n";
 
+//include("server_ticker.php");
 ?>
