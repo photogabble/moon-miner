0189f3948b5f50c6b9051cd8f28591347cecded3
The Kabal <thekabal@users.sourceforge.net>
2012-05-15 02:22:06 +0000

    Small typo in SQL

--- sched_turns.php
+++ sched_turns.php
@@ -8,7 +8,7 @@
 
   echo "<B>TURNS</B><BR><BR>";
   echo "Adding turns...";
-  QUERYOK($db->Execute("UPDATE $dbtables[ships] SET turns = turns + ($turns_per_tick * $multiplier) WHERE turns < $max_turns;"));
+  QUERYOK($db->Execute("UPDATE $dbtables[ships] SET turns = turns + ($turns_per_tick * $multiplier) WHERE turns < $max_turns"));
   echo "Ensuring maximum turns are $max_turns...";
   QUERYOK($db->Execute("UPDATE $dbtables[ships] SET turns = $max_turns WHERE turns > $max_turns;"));
   echo "<BR>";
