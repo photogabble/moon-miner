1896bf905b73edaf9274228afa6e8ef3aceffb18
The Kabal <thekabal@users.sourceforge.net>
2012-05-16 04:54:09 +0000

    Remove benchmark code (it has been moved to its own file under includes, and called in global_funcs.php)

--- create_universe.php
+++ create_universe.php
@@ -5,7 +5,6 @@
 include("config.php");
 include("languages/$lang");
 
-
 /*
 ##############################################################################
 # Create Universe Script                                                     #
@@ -123,34 +122,6 @@ if (!function_exists('Table_Footer'))
 
 ### Description: Create Benchmark Class
 
-class c_Timer
-{
-    var $t_start = 0;
-    var $t_stop = 0;
-    var $t_elapsed = 0;
-
-    function start()
-    {
-        $this->t_start = microtime();
-    }
-
-    function stop()
-    {
-        $this->t_stop = microtime();
-    }
-
-    function elapsed()
-    {
-        $start_u = substr($this->t_start,0,10);
-        $start_s = substr($this->t_start,11,10);
-        $stop_u  = substr($this->t_stop,0,10);
-        $stop_s  = substr($this->t_stop,11,10);
-        $start_total = doubleval($start_u) + $start_s;
-        $stop_total  = doubleval($stop_u) + $stop_s;
-        $this->t_elapsed = $stop_total - $start_total;
-        return $this->t_elapsed;
-    }
-}
 /*
 //function PrintFlush($Text="")
 //{
@@ -159,10 +130,6 @@ class c_Timer
 //}
  */
 ### End defining functions.
-
-### Start Timer
-$BenchmarkTimer = new c_Timer;
-$BenchmarkTimer->start();
 
 ### Set timelimit and randomize timer.
 
