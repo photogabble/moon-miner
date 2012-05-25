c99c43ce3b67d618759d29a57a7e85f7be78d9f3
The Kabal <thekabal@users.sourceforge.net>
2012-05-25 17:57:43 +0000

    Switch licensing to AGPLv3.

--- config.php
+++ config.php
@@ -1,4 +1,22 @@
 <?php
+// Blacknova Traders - A web-based massively multiplayer space combat and trading game
+// Copyright (C) 2001-2012 Ron Harwood and the BNT development team
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// File: config.php
+
 // ini_set('error_reporting', E_ALL); // During development
 ini_set('error_reporting', 0);
 // ini_set('display_errors', '1'); // During development
