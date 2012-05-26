4f34828d9978008944bf563b9c0ec40b7f7e2d4a
The Kabal <thekabal@users.sourceforge.net>
2012-05-26 16:27:30 +0000

    Whitespace/indent fixes, and remove extra connectdb() call

--- self_destruct.php
+++ self_destruct.php
@@ -19,17 +19,13 @@
 
 include("config.php");
 updatecookie();
-
 include("languages/$lang");
-
 $title=$l_die_title;
 include("header.php");
 
-connectdb();
-
 if(checklogin())
 {
-  die();
+    die();
 }
 
 bigtitle();
@@ -39,40 +35,38 @@ $playerinfo = $result->fields;
 
 if(!isset($sure))
 {
-  echo "<FONT COLOR=RED><B>$l_die_rusure</B></FONT><BR><BR>";
-  echo "Please Note: You will loose all your Planets if you Self-Destruct!.<br />\n";
-  echo "<A HREF=$interface>$l_die_nonono</A> $l_die_what<BR><BR>";
-  echo "<A HREF=self_destruct.php?sure=1>$l_yes!</A> $l_die_goodbye<BR><BR>";
+    echo "<FONT COLOR=RED><B>$l_die_rusure</B></FONT><BR><BR>";
+    echo "Please Note: You will loose all your Planets if you Self-Destruct!.<br />\n";
+    echo "<A HREF=$interface>$l_die_nonono</A> $l_die_what<BR><BR>";
+    echo "<A HREF=self_destruct.php?sure=1>$l_yes!</A> $l_die_goodbye<BR><BR>";
 }
 elseif($sure == 1)
 {
-  echo "<FONT COLOR=RED><B>$l_die_check</B></FONT><BR><BR>";
-  echo "Please Note: You will loose all your Planets if you Self-Destruct!.<br />\n";
-  echo "<A HREF=$interface>$l_die_nonono</A> $l_die_what<BR><BR>";
-  echo "<A HREF=self_destruct.php?sure=2>$l_yes!</A> $l_die_goodbye<BR><BR>";
+    echo "<FONT COLOR=RED><B>$l_die_check</B></FONT><BR><BR>";
+    echo "Please Note: You will loose all your Planets if you Self-Destruct!.<br />\n";
+    echo "<A HREF=$interface>$l_die_nonono</A> $l_die_what<BR><BR>";
+    echo "<A HREF=self_destruct.php?sure=2>$l_yes!</A> $l_die_goodbye<BR><BR>";
 }
 elseif($sure == 2)
 {
-  echo "$l_die_count<BR>";
-  echo "$l_die_vapor<BR><BR>";
-  echo "$l_die_please.<BR>";
-  db_kill_player($playerinfo['ship_id'], true);
-  cancel_bounty($playerinfo['ship_id']);
-  adminlog(LOG_ADMIN_HARAKIRI, "$playerinfo[character_name]|$ip");
-  playerlog($playerinfo[ship_id], LOG_HARAKIRI, "$ip");
-  echo "Due to nobody looking after your Planets, all your Planets have reduced into dust and ruble. Your Planets are no more.<br />\n";
-
+    echo "$l_die_count<BR>";
+    echo "$l_die_vapor<BR><BR>";
+    echo "$l_die_please.<BR>";
+    db_kill_player($playerinfo['ship_id'], true);
+    cancel_bounty($playerinfo['ship_id']);
+    adminlog(LOG_ADMIN_HARAKIRI, "$playerinfo[character_name]|$ip");
+    playerlog($playerinfo[ship_id], LOG_HARAKIRI, "$ip");
+    echo "Due to nobody looking after your Planets, all your Planets have reduced into dust and ruble. Your Planets are no more.<br />\n";
 }
 else
 {
-  echo "$l_die_exploit<BR><BR>";
+    echo "$l_die_exploit<BR><BR>";
 }
 
 if($sure != 2)
 {
-  TEXT_GOTOMAIN();
+    TEXT_GOTOMAIN();
 }
 
 include("footer.php");
-
 ?>
