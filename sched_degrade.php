0b25ed4408007417384e26134dfb5d979040e06e
Paul Kirby <themightydude@users.sourceforge.net>
2012-07-20 11:42:55 +0000

    Fixed Notice:  Use of undefined constant quantity - assumed 'quantity' and energy_available - assumed 'energy_available' in sched_degrade.php

--- sched_degrade.php
+++ sched_degrade.php
@@ -30,46 +30,46 @@ db_op_result ($db, $res, __LINE__, __FILE__, $db_logging);
 while (!$res->EOF)
 {
     $row = $res->fields;
-    $res3 = $db->Execute ("SELECT * from {$db->prefix}ships where ship_id = $row[ship_id]");
+    $res3 = $db->Execute ("SELECT * FROM {$db->prefix}ships WHERE ship_id = ?;", array($row[ship_id]));
     db_op_result ($db, $res3, __LINE__, __FILE__, $db_logging);
     $sched_playerinfo = $res3->fields;
-    $res2 = $db->Execute ("SELECT * from {$db->prefix}planets where (owner = $row[ship_id] or (corp = $sched_playerinfo[team] AND $sched_playerinfo[team] <> 0)) and sector_id = $row[sector_id] and energy > 0");
+    $res2 = $db->Execute ("SELECT * FROM {$db->prefix}planets WHERE (owner = ? OR (corp = ? AND ? <> 0)) AND sector_id = ? AND energy > 0;", array($row['ship_id'], $sched_playerinfo['team'], $sched_playerinfo['team'], $row['sector_id']));
     db_op_result ($db, $res2, __LINE__, __FILE__, $db_logging);
     if ($res2->EOF)
     {
-        $resa = $db->Execute ("UPDATE {$db->prefix}sector_defence set quantity = quantity - GREATEST(ROUND(quantity * $defence_degrade_rate),1) where defence_id = $row[defence_id] and quantity > 0");
+        $resa = $db->Execute ("UPDATE {$db->prefix}sector_defence SET quantity = quantity - GREATEST(ROUND(quantity * ?),1) WHERE defence_id = ? AND quantity > 0;", array($defence_degrade_rate, $row['defence_id']));
         db_op_result ($db, $resa, __LINE__, __FILE__, $db_logging);
         $degrade_rate = $defence_degrade_rate * 100;
-        playerlog ($db, $row['ship_id'], LOG_DEFENCE_DEGRADE, "$row[sector_id]|$degrade_rate");
+        playerlog ($db, $row['ship_id'], LOG_DEFENCE_DEGRADE, $row['sector_id'] ."|". $degrade_rate);
     }
     else
     {
-        $energy_required = ROUND($row[quantity] * $energy_per_fighter);
-        $res4 = $db->Execute ("SELECT IFNULL(SUM(energy),0) as energy_available from {$db->prefix}planets where (owner = $row[ship_id] or (corp = $sched_playerinfo[team] AND $sched_playerinfo[team] <> 0)) and sector_id = $row[sector_id]");
+        $energy_required = ROUND($row['quantity'] * $energy_per_fighter);
+        $res4 = $db->Execute ("SELECT IFNULL(SUM(energy),0) AS energy_available FROM {$db->prefix}planets WHERE (owner = ? OR (corp = ? AND ? <> 0)) AND sector_id = ?;", array($row['ship_id'], $sched_playerinfo['team'], $sched_playerinfo['team'], $row['sector_id']));
         db_op_result ($db, $res4, __LINE__, __FILE__, $db_logging);
         $planet_energy = $res4->fields;
-        $energy_available = $planet_energy[energy_available];
+        $energy_available = $planet_energy['energy_available'];
         echo "available $energy_available, required $energy_required.";
         if ($energy_available > $energy_required)
         {
             while (!$res2->EOF)
             {
                 $degrade_row = $res2->fields;
-                $resb = $db->Execute ("UPDATE {$db->prefix}planets set energy = energy - GREATEST(ROUND($energy_required * (energy / $energy_available)),1)  where planet_id = $degrade_row[planet_id] ");
+                $resb = $db->Execute ("UPDATE {$db->prefix}planets SET energy = energy - GREATEST(ROUND(? * (energy / ?)),1)  WHERE planet_id = ?;", array($energy_required, $energy_available, $degrade_row['planet_id']));
                 db_op_result ($db, $resb, __LINE__, __FILE__, $db_logging);
                 $res2->MoveNext();
             }
         }
         else
         {
-            $resc = $db->Execute ("UPDATE {$db->prefix}sector_defence set quantity = quantity - GREATEST(ROUND(quantity * $defence_degrade_rate),1) where defence_id = $row[defence_id] ");
+            $resc = $db->Execute ("UPDATE {$db->prefix}sector_defence SET quantity = quantity - GREATEST(ROUND(quantity * ?),1) WHERE defence_id = ?;", array($defence_degrade_rate, $row['defence_id']));
             db_op_result ($db, $resc, __LINE__, __FILE__, $db_logging);
             $degrade_rate = $defence_degrade_rate * 100;
-            playerlog ($db, $row['ship_id'], LOG_DEFENCE_DEGRADE, "$row[sector_id]|$degrade_rate");
+            playerlog ($db, $row['ship_id'], LOG_DEFENCE_DEGRADE, $row['sector_id'] ."|". $degrade_rate);
         }
     }
     $res->MoveNext();
 }
-$resx = $db->Execute("DELETE from {$db->prefix}sector_defence where quantity <= 0");
+$resx = $db->Execute("DELETE FROM {$db->prefix}sector_defence WHERE quantity <= 0");
 db_op_result ($db, $resx, __LINE__, __FILE__, $db_logging);
 ?>
