fdb1f4a8f3a5d82886e37a3b9ae76795e9665b36
The Kabal <thekabal@users.sourceforge.net>
2012-08-06 23:57:34 +0000

    Oops, forgot to change the reference to the new file name.

--- global_includes.php
+++ global_includes.php
@@ -62,5 +62,5 @@ include_once $ADOdbpath . "/adodb-perf.inc.php";
 include_once $ADOdbpath . "/session/adodb-session.php";
 
 spl_autoload_register('bnt_autoload');
-require_once "global_cleanups.php";
+require_once "common.php";
 ?>
