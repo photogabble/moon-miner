67e1659ff803ff83a95b661fb945169a9541d66c
The Kabal <thekabal@users.sourceforge.net>
2012-08-26 02:45:08 +0000

    HTML5 email input type, gives browser-based validation if the browser supports it.

--- new.php
+++ new.php
@@ -42,7 +42,7 @@ bigtitle ();
 echo "<form action='new2.php" . $link . "' method='post'>\n";
 echo "    <dl class='twocolumn-form'>\n";
 echo "        <dt style='padding:3px'><label for='username'>{$l_login_email}:</label></dt>\n";
-echo "        <dd style='padding:3px'><input type='text' id='username' name='username' size='20' maxlength='40' value='' style='width:200px'></dd>\n";
+echo "        <dd style='padding:3px'><input type='email' id='username' name='username' size='20' maxlength='40' value='' style='width:200px'></dd>\n";
 echo "        <dt style='padding:3px'><label for='shipname'>{$l_new_shipname}:</label></dt>\n";
 echo "        <dd style='padding:3px'><input type='text' id='shipname' name='shipname' size='20' maxlength='20' value='' style='width:200px'></dd>\n";
 echo "        <dt style='padding:3px'><label for='character'>{$l_new_pname}:</label></dt>\n";
