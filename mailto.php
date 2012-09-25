7d77299d7258322aee4017bef5949227980c2e61
The Kabal <thekabal@users.sourceforge.net>
2012-09-25 00:53:13 +0000

    Merge mailto2 and mailto

--- mailto.php
+++ mailto.php
@@ -15,7 +15,7 @@
 //  You should have received a copy of the GNU Affero General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
-// File: mailto.php
+// File: mailto2.php
 
 include './global_includes.php';
 if (check_login ($db, $lang, $langvars)) // Checks player login, sets playerinfo
@@ -24,55 +24,171 @@ if (check_login ($db, $lang, $langvars)) // Checks player login, sets playerinfo
 }
 
 // New database driven language entries
-load_languages($db, $lang, array('mailto', 'common', 'global_includes', 'global_funcs', 'footer', 'news'), $langvars);
+load_languages($db, $lang, array ('mailto2', 'common', 'global_includes', 'global_funcs', 'footer', 'planet_report'), $langvars);
 
-$title = $l_mt_title;
+$title = $l_sendm_title;
 include './header.php';
-echo "<h1>" . $title . "</h1>\n";
 
-$res = $db->Execute("SELECT * FROM {$db->prefix}ships WHERE email=?", array($_SESSION['username']));
+// Filter to the FILTER_SANITIZE_STRING ruleset, because we need to allow spaces for names & subject (FILTER_SANITIZE_URL doesn't allow spaces)
+// $name, $to, and $subject are all sent both via post and get, so we have to do a filter input for each
+// filter_input doesn't support INPUT_REQUEST, and also doesn't support the format INPUT_POST | INPUT_GET - I tried.
+
+$name = null;
+if (isset ($_POST['name']))
+{
+    $name  = filter_input (INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+}
+elseif (isset ($_GET['name']))
+{
+    $name  = filter_input (INPUT_GET, 'name', FILTER_SANITIZE_STRING);
+}
+
+$to = null;
+if (isset ($_POST['to']))
+{
+    $to  = filter_input (INPUT_POST, 'to', FILTER_SANITIZE_STRING);
+}
+elseif (isset ($_GET['to']))
+{
+    $to  = filter_input (INPUT_GET, 'to', FILTER_SANITIZE_STRING);
+}
+
+$subject = null;
+if (!empty ($_POST['subject']))
+{
+    $subject  = filter_input (INPUT_POST, 'subject', FILTER_SANITIZE_STRING);
+}
+elseif (!empty ($_GET['subject']))
+{
+    $subject  = filter_input (INPUT_GET, 'subject', FILTER_SANITIZE_STRING);
+}
+
+// Allow rich-text codes (dirty) in, we will filter them using html purifier
+$dirtycontent = filter_input (INPUT_POST, 'content', FILTER_UNSAFE_RAW);
+
+// Include HTML purifier, set its config, use the 4.01 doctype (since they don't do HTML5 yet)
+require_once 'backends/htmlpurifier/library/HTMLPurifier.auto.php';
+$html_purifier_config = HTMLPurifier_Config::createDefault();
+$html_purifier_config->set('HTML.Doctype', 'HTML 4.01 Transitional');
+$purifier = new HTMLPurifier($html_purifier_config);
+
+// Filter the submitted content to ensure that it doesn't have exploits
+$content = $purifier->purify($dirtycontent);
+
+// Filter the submitted subject to ensure that it doesn't have exploits either
+if (!empty ($subject))
+{
+    $subject = $purifier->purify($subject);
+}
+
+$res = $db->Execute("SELECT ship_id, character_name FROM {$db->prefix}ships WHERE email=?;", array ($_SESSION['username']));
 db_op_result ($db, $res, __LINE__, __FILE__);
 $playerinfo = $res->fields;
 
+echo "<h1>" . $title . "</h1>\n";
+
 if (empty($content))
 {
-    $res = $db->Execute("SELECT * FROM {$db->prefix}ships WHERE ship_destroyed = 'N' AND turns_used > 0 ORDER BY character_name ASC");
+    $res = $db->Execute("SELECT character_name FROM {$db->prefix}ships WHERE email NOT LIKE '%@Xenobe' AND ship_id <> {$playerinfo['ship_id']} ORDER BY character_name ASC");
     db_op_result ($db, $res, __LINE__, __FILE__);
-    echo "<form action=mailto2.php method=post>";
-    echo "<table>";
-    echo "<tr><td>To:</td><td><select name=to style='width:200px;'>";
+    $res2 = $db->Execute("SELECT team_name FROM {$db->prefix}teams WHERE admin ='N' ORDER BY team_name ASC");
+    db_op_result ($db, $res2, __LINE__, __FILE__);
+    echo "<form action=mailto2.php method=post>\n";
+    echo "  <table>\n";
+    echo "    <tr>\n";
+    echo "      <td>$l_sendm_from:</td>\n";
+    echo "      <td><input disabled type='text' name='dummy' size='40' maxlength='40' value=\"$playerinfo[character_name]\"></td>\n";
+    echo "    </tr>\n";
+    echo "    <tr>\n";
+    echo "      <td>$l_sendm_to:</td>\n";
+    echo "      <td>\n";
+    echo "        <select name='to' style='width:200px;'>\n";
+
+    // Add self to list.
+    echo "          <option".(($playerinfo['character_name']==$name)?" selected":"").">{$playerinfo['character_name']}</option>\n";
+
     while (!$res->EOF)
     {
         $row = $res->fields;
-        if ($row['ship_id'] == $to)
-        {
-            echo "\n<option selectED>$row[character_name]</option>";
-        }
-        else
-        {
-            echo "\n<option>$row[character_name]</option>";
-        }
+        echo "          <option".(($row['character_name']==$to)?" selected":"").">{$row['character_name']}</option>\n";
         $res->MoveNext();
     }
-    echo "</select></td></tr>";
-    echo "<tr><td>$l_mt_from</td><td><input disabled type=text name=dummy size=50 maxlength=40 value=\"$playerinfo[character_name]\"></td></tr>";
-    echo "<tr><td>$l_mt_subject</td><td><input type=text name=subject size=40 maxlength=40></td></tr>";
-    echo "<tr><td>$l_mt_message:</td><td><textarea name=content rows=5 cols=40></textarea></td></tr>";
-    echo "<tr><td></td><td><input type=submit value=$l_mt_send><input type=reset value=Clear></td>";
-    echo "</table>";
-    echo "</form>";
+
+    while (!$res2->EOF && $res2->fields != null)
+    {
+        $row2 = $res2->fields;
+        echo "          <option>$l_sendm_ally $row2[team_name]</option>\n";
+        $res2->MoveNext();
+    }
+
+    echo "        </select>\n";
+    echo "      </td>\n";
+    echo "    </tr>\n";
+    if (isset ($subject))
+    {
+        $subject = "RE: " . $subject;
+    }
+    else
+    {
+        $subject = '';
+    }
+
+    echo "    <tr>\n";
+    echo "      <td>$l_sendm_subj:</td>\n";
+    echo "      <td><input type=text name=subject size=40 maxlength=40 value=\"$subject\"></td>\n";
+    echo "    </tr>\n";
+    echo "    <tr>\n";
+    echo "      <td>$l_sendm_mess:</td>\n";
+    echo "      <td><textarea id=richeditor name=content rows=6 cols=80></textarea></td>\n";
+    echo "    </tr>";
+    echo "    <tr>\n";
+    echo "      <td></td>\n";
+    echo "      <td><input type=submit value=$l_sendm_send><input type=reset value=$l_reset></td>\n";
+    echo "    </tr>\n";
+    echo "  </table>\n";
+    echo "<script>CKEDITOR.replace('richeditor');</script>";
+    echo "</form>\n";
 }
 else
 {
-    echo "$l_mt_sent<br><br>";
-    $content = htmlspecialchars ($content);
-    $subject = htmlspecialchars ($subject);
+    if (strpos ($to, $l_sendm_ally) === false)
+    {
+        $timestamp = date("Y\-m\-d H\:i\:s");
+        $res = $db->Execute("SELECT ship_id FROM {$db->prefix}ships WHERE character_name=?;", array ($to));
+        db_op_result ($db, $res, __LINE__, __FILE__);
+        $target_info = $res->fields;
+        $resx = $db->Execute("INSERT INTO {$db->prefix}messages (sender_id, recp_id, sent, subject, message) VALUES (?, ?, ?, ?, ?);", array ($playerinfo['ship_id'], $target_info['ship_id'], $timestamp, $subject, $content));
+        db_op_result ($db, $resx, __LINE__, __FILE__);
+        if ($db->ErrorNo() != 0)
+        {
+            echo "Message failed to send: " . $db->ErrorMsg() . "<br>\n";
+        }
+        else
+        {
+            echo $l_sendm_sent . "<br><br>";
+        }
+    }
+    else
+    {
+        $timestamp = date("Y\-m\-d H\:i\:s");
+        $to = str_replace ($l_sendm_ally, "", $to);
+        $to = trim($to);
+        $to = addslashes($to);
+        $res = $db->Execute("SELECT id FROM {$db->prefix}teams WHERE team_name=?;", array ($to));
+        db_op_result ($db, $res, __LINE__, __FILE__);
+        $row = $res->fields;
 
-    $res = $db->Execute("SELECT * FROM {$db->prefix}ships WHERE character_name='$to'");
-    db_op_result ($db, $res, __LINE__, __FILE__);
-    $target_info = $res->fields;
-    $db->Execute("INSERT INTO messages (sender_id, recp_id, subject, message) VALUES ('".$playerinfo[ship_id]."', '".$target_info[ship_id]."', '".$subject."', '".$content."')");
-    // Using this three lines to get recipients ship_id and sending the message -- blindcoder
+        $res2 = $db->Execute("SELECT * FROM {$db->prefix}ships WHERE team=?;", array ($row['id']));
+        db_op_result ($db, $res2, __LINE__, __FILE__);
+
+        while (!$res2->EOF)
+        {
+            $row2 = $res2->fields;
+            $resx = $db->Execute("INSERT INTO {$db->prefix}messages (sender_id, recp_id, sent, subject, message) VALUES (?, ?, ?, ?, ?);", array ($playerinfo['ship_id'], $row2['ship_id'], $timestamp, $subject, $content));
+            db_op_result ($db, $resx, __LINE__, __FILE__);
+            $res2->MoveNext();
+        }
+   }
 }
 
 TEXT_GOTOMAIN();
