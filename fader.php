5db484e502b1b51f866055be8615fefd050c4bdf
TheMightyDude <themightydude@users.sourceforge.net>
2012-10-09 19:20:34 +0000

    Fixed ERROR: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '2012/10/09' 00:00:00' AND date < ''2012/10/09' 23:59:59' ORDER BY news_id' at line 1 -> We cannot use ? within a quote like the following "date > '? 00:00:00'" due to it auto adds a quote wrapper around the string and we end up with "date > ''2012/10/08' 00:00:00'".

--- fader.php
+++ fader.php
@@ -41,7 +41,7 @@ if (result == true)
     }
     else
     {
-        $res = $db->Execute("SELECT headline, news_type FROM {$db->prefix}news WHERE date > '? 00:00:00' AND date < '? 23:59:59' ORDER BY news_id", array ($startdate, $startdate));
+        $res = $db->Execute("SELECT headline, news_type FROM {$db->prefix}news WHERE date > ? AND date < ? ORDER BY news_id", array ($startdate ." 00:00:00", $startdate ." 23:59:59"));
         db_op_result ($db, $res, __LINE__, __FILE__);
         if (!$res instanceof ADORecordSet || $res->RecordCount() == 0)
         {
