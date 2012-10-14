a68028a0fb8424627f57265f45569e56a9d5598a
The Kabal <thekabal@users.sourceforge.net>
2012-10-14 19:39:56 +0000

    Test ranking becomes official ranking page

--- ranking.php
+++ ranking.php
@@ -20,168 +20,157 @@
 include './global_includes.php';
 include './includes/player_insignia_name.php';
 
-if (!isset($_GET['lang']))
+// Always make sure we are using empty vars before use.
+$langvars = null;
+$variables = null;
+
+$variables['lang'] = $lang;
+$variables['link'] = "test_ranking.php";
+
+// These should be set within the template config.
+$variables['color_header'] = $color_header;
+$variables['color_line1'] = $color_line1;
+$variables['color_line2'] = $color_line2;
+
+// Load required language variables for the ranking page.
+load_languages ($db, $lang, array ('main', 'ranking', 'common', 'global_includes', 'global_funcs', 'footer', 'teams'), $langvars);
+
+// Modify the requires language variables here.
+$langvars['l_ranks_title'] = str_replace ("[max_ranks]", $max_ranks, $langvars['l_ranks_title']);
+
+// Get requested ranking order.
+$sort = '';
+if (isset ($_GET['sort']))
 {
-    $_GET['lang'] = null;
-    $lang = $default_lang;
-    $link = 'ranking.php';
-    $link_back = null;
-}
-else
-{
-    $lang = $_GET['lang'];
-    $link = "ranking.php?lang=" . $lang;
-    $link_back = '?lang=' . $lang;
+    $sort = filter_input (INPUT_GET, 'sort', FILTER_SANITIZE_STRING);
 }
 
-// New database driven language entries
-load_languages($db, $lang, array('main', 'ranking', 'common', 'global_includes', 'global_funcs', 'footer', 'teams'), $langvars);
-
-$l_ranks_title = str_replace("[max_ranks]", $max_ranks, $l_ranks_title);
-$title = $l_ranks_title;
-include './header.php';
-echo "<h1>" . $title . "</h1>\n";
-
-$res = $db->Execute("SELECT COUNT(*) AS num_players FROM {$db->prefix}ships WHERE ship_destroyed='N' and email NOT LIKE '%@xenobe' AND turns_used >0");
-db_op_result ($db, $res, __LINE__, __FILE__);
-$row = $res->fields;
-$num_players = $row['num_players'];
-
-if (!isset($_GET['sort']))
+switch ($sort)
 {
-    $_GET['sort'] = '';
-}
-$sort = $_GET['sort'];
-
-if ($sort=="turns")
-{
-    $by="turns_used DESC,character_name ASC";
-}
-elseif ($sort=="login")
-{
-    $by="last_login DESC,character_name ASC";
-}
-elseif ($sort=="good")
-{
-    $by="rating DESC,character_name ASC";
-}
-elseif ($sort=="bad")
-{
-    $by="rating ASC,character_name ASC";
-}
-elseif ($sort=="team")
-{
-    $by="{$db->prefix}teams.team_name DESC, character_name ASC";
-}
-elseif ($sort=="efficiency")
-{
-    $by="efficiency DESC";
-}
-else
-{
-    $by="score DESC,character_name ASC";
-}
-
-$res = $db->Execute("SELECT {$db->prefix}ships.email,{$db->prefix}ships.score,{$db->prefix}ships.character_name,{$db->prefix}ships.turns_used,{$db->prefix}ships.last_login,UNIX_TIMESTAMP({$db->prefix}ships.last_login) as online,{$db->prefix}ships.rating, {$db->prefix}teams.team_name, if ({$db->prefix}ships.turns_used<150,0,ROUND({$db->prefix}ships.score/{$db->prefix}ships.turns_used)) AS efficiency FROM {$db->prefix}ships LEFT JOIN {$db->prefix}teams ON {$db->prefix}ships.team = {$db->prefix}teams.id  WHERE ship_destroyed='N' and email NOT LIKE '%@xenobe' AND turns_used >0 ORDER BY ? LIMIT ?;", array ($by, $max_ranks));
-db_op_result ($db, $res, __LINE__, __FILE__);
-
-if (!$res)
-{
-    echo "$l_ranks_none<br>";
-}
-else
-{
-    echo "<br>$l_ranks_pnum: " . NUMBER($num_players);
-    echo "<br>$l_ranks_dships<br><br>";
-    echo "<table border=0 cellspacing=0 cellpadding=2>";
-    echo "<tr bgcolor=\"$color_header\">";
-    echo "<td><strong>$l_ranks_rank</strong></td>";
-    echo "<td><strong><a href=\"" . $link . "\">$l_score</a></strong></td>";
-    echo "<td><strong>$l_player</strong></td>";
-    if (is_null($link_back))
+    case "turns":
     {
-        $link .= "?sort="; 
+        $by = "turns_used DESC, character_name ASC";
+        break;
     }
-    else
+    case "login":
     {
-        $link .= "&amp;sort=";
+        $by = "last_login DESC, character_name ASC";
+        break;
     }
-    echo "<td><strong><a href=\"" . $link . "turns\">$l_turns_used</a></strong></td>";
-    echo "<td><strong><a href=\"" . $link . "login\">$l_ranks_lastlog</a></strong></td>";
-    echo "<td><strong><a href=\"" . $link . "good\">$l_ranks_good</a>/<a href=\"" . $link . "?sort=bad\">$l_ranks_evil</a></strong></td>";
-    echo "<td><strong><a href=\"" . $link . "team\">$l_team_team</a></strong></td>";
-    echo "<td><strong><a href=\"" . $link . "online\">$l_status</a></strong></td>";
-    echo "<td><strong><a href=\"" . $link . "efficiency\">Eff. Rating.</a></strong></td></tr>\n";
-    $color = $color_line1;
-    $i = '';
-    while (!$res->EOF)
+    case "good":
     {
-        $row = $res->fields;
-        $i++;
-        $rating=round(sqrt( abs($row['rating']) ));
-        if (abs($row['rating'])!=$row['rating'])
-        {
-            $rating=-1*$rating;
-        }
-
-        $curtime = TIME();
-        $time = $row['online'];
-        $difftime = ($curtime - $time) / 60;
-        $temp_turns = $row['turns_used'];
-        if ($temp_turns <= 0)
-        {
-            $temp_turns = 1;
-        }
-
-        $online = " ";
-        if ($difftime <= 5)
-        {
-            $online = "<span style='color:#FFFF00;'>{$l_online}</span>";
-        }
-        else
-        {
-            $online = $l_offline;
-        }
-
-        echo "<tr bgcolor=\"$color\"><td>" . NUMBER($i) . "</td><td>" . NUMBER($row['score']) . "</td><td>";
-        echo "&nbsp;";
-        echo player_insignia_name ($db, $row['email'], $langvars);
-        echo "&nbsp;";
-        echo "<strong>$row[character_name]</strong></td><td>" . NUMBER($row['turns_used']) . "</td><td>$row[last_login]</td><td>&nbsp;&nbsp;" . NUMBER($rating) . "</td><td>$row[team_name]&nbsp;</td><td>$online</td><td>$row[efficiency]</td></tr>\n";
-        if ($color == $color_line1)
-        {
-            $color = $color_line2;
-        }
-        else
-        {
-            $color = $color_line1;
-        }
-
-        $res->MoveNext();
+        $by = "rating DESC, character_name ASC";
+        break;
     }
-    echo "</table>";
+    case "bad":
+    {
+        $by = "rating ASC, character_name ASC";
+        break;
+    }
+    case "team":
+    {
+        $by = "{$db->prefix}teams.team_name DESC, character_name ASC";
+        break;
+    }
+    case "efficiency":
+    {
+        $by = "efficiency DESC";
+        break;
+    }
+    default:
+    {
+        $by = "score DESC, character_name ASC";
+        break;
+    }
 }
 
-echo "<br>";
+$variables['num_players'] = (integer) 0;
 
-if (!isset($_GET['lang']))
+$rs = $db->Execute ("SELECT {$db->prefix}ships.email,{$db->prefix}ships.score,{$db->prefix}ships.character_name,{$db->prefix}ships.turns_used,{$db->prefix}ships.last_login,UNIX_TIMESTAMP({$db->prefix}ships.last_login) as online,{$db->prefix}ships.rating, {$db->prefix}teams.team_name, if ({$db->prefix}ships.turns_used<150,0,ROUND({$db->prefix}ships.score/{$db->prefix}ships.turns_used)) AS efficiency FROM {$db->prefix}ships LEFT JOIN {$db->prefix}teams ON {$db->prefix}ships.team = {$db->prefix}teams.id  WHERE ship_destroyed='N' and email NOT LIKE '%@xenobe' AND turns_used >0 ORDER BY $by LIMIT $max_ranks");
+db_op_result ($db, $rs, __LINE__, __FILE__);
+if ($rs instanceof ADORecordSet)
 {
-    $_GET['lang'] = null;
-    $lang = $default_lang;
+    $variables['num_players'] = (integer) $rs->RecordCount();
+
+    if ($variables['num_players'] > 0)
+    {
+        $player_list = array ();
+
+        while (!$rs->EOF)
+        {
+            $row = $rs->fields;
+
+            // Set the players rank number.
+            $row['rank'] = count ($player_list) + 1;
+
+            // Calculate the players rating.
+            $rating = round (sqrt (abs ($row['rating']) ));
+            if (abs ($row['rating']) != $row['rating'])
+            {
+                $rating = -1 * $rating;
+            }
+            $row['rating']  = $rating;
+
+            // Calculate the players online status.
+            $curtime = TIME ();
+            $time = $row['online'];
+            $difftime = ($curtime - $time) / 60;
+            $temp_turns = $row['turns_used'];
+            if ($temp_turns <= 0)
+            {
+                $temp_turns = 1;
+            }
+
+            $row['online'] = "Offline";
+            if ($difftime <= 5)
+            {
+                $row['online'] = "Online";
+            }
+
+            // Set the players Insignia.
+            $row['insignia'] = player_insignia_name ($db, $row['email'], $langvars);
+
+            // This is just to show that we can set the type of player.
+            // like: banned, admin, player, npc etc.
+            if ($row['email'] == $admin_mail)
+            {
+                $row['type'] = "admin";
+            }
+            else
+            {
+                $row['type'] = "player";
+            }
+
+            array_push ($player_list, $row);
+
+            $rs->MoveNext ();
+        }
+        $player_list['container']    = "player";
+        $template->AddVariables ("players", $player_list);
+    }
+}
+
+if (empty ($_SESSION['username']))
+{
+    $variables['loggedin'] = (boolean) true;
+    $variables['linkback'] = array ("caption"=>$langvars['l_global_mlogin'], "link"=>"index.php");
 }
 else
 {
-    $lang = $_GET['lang'];
+    $variables['loggedin'] = (boolean) false;
+    $variables['linkback'] = array ("caption"=>$langvars['l_global_mmenu'], "link"=>"main.php");
 }
 
-if (empty($_SESSION['username']))
-{
-    echo str_replace("[here]", "<a href='index.php" . $link_back . "'>" . $langvars['l_here'] . "</a>", $langvars['l_global_mlogin']);
-}
-else
-{
-    echo str_replace("[here]", "<a href='main.php" . $link_back . "'>" . $langvars['l_here'] . "</a>", $langvars['l_global_mmenu']);
-}
+// Now we include the Footer Logic.
+include_once './footer_t.php';
 
-include './footer.php';
+$variables['container'] = "variable";
+$template->AddVariables ('variables', $variables);
+
+// Now add the loaded language variables into the Template API.
+$langvars['container'] = "langvar";
+$template->AddVariables ('langvars', $langvars);
+
+// Now we tell the Template API to output the page
+$template->Display("ranking.tpl");
 ?>
