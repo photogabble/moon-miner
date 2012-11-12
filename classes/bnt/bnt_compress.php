685ce960c6852d8d1cef7db4769f88c33e87f863
The Kabal <thekabal@users.sourceforge.net>
2012-11-12 01:37:16 +0000

    Fix file reference

--- classes/bnt/bnt_compress.php
+++ classes/bnt/bnt_compress.php
@@ -15,7 +15,7 @@
 //  You should have received a copy of the GNU Affero General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
-// File: inclues/bnt_compress.php
+// File: classes/bnt/bnt_compress.php
 //
 namespace bnt;
 
