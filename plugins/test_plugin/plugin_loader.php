21aa45b23e51f78f7f2e296e48daaddaaf4c3d4d
TheMightyDude <themightydude@users.sourceforge.net>
2013-01-01 14:36:58 +0000

    Second part of the Plugin and Event System, Clean-up and [test plugin] (THIS WILL BREAK)

---
+++ plugins/test_plugin/plugin_loader.php
@@ -0,0 +1,80 @@
+<?php
+// Blacknova Traders - A web-based massively multiplayer space combat and trading game
+// Copyright (C) 2001-2012 Ron Harwood and the BNT development team
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// File: plugins/test_plugin/plugin_loader.php
+
+if (strpos ($_SERVER['PHP_SELF'], 'plugin_loader.php')) // Prevent direct access to this file
+{
+    $error_file = $_SERVER['SCRIPT_NAME'];
+    include 'error.php';
+}
+
+if (isset($plugin_config[$pluginname]) && $plugin_config[$pluginname]['enabled'] == true)
+{
+    class PluginTest extends Plugin
+    {
+        static $AppName                         = "Plugin Test";
+        static $Version                         = "0.0.0 (0000) Alpha";
+        static $Author                          = "TheMightyDude";
+        static $isDisabled                      = true;
+        static $usesEvents                      = true;
+        static $moduleSupport                   = true;
+        static $pluginVer                       = "3a";
+
+        private $switches                       = NULL;
+
+        function __construct()
+        {
+            global $plugin_config;
+
+            $this->initialised                  = (boolean) false;
+            $this->switches                     = $plugin_config['PluginTest'];
+            $this->modules                      = array();
+        }
+
+        function __destruct()
+        {
+            // Need to put all unloading here.
+        }
+
+        public function Initialize($db = null)
+        {
+            // Need to put all Initialization stuff here along with all the Event Hooks.
+            // PluginSystem::AddEventHook(EVENT_PLAYER_JOIN, $this);
+        }
+
+        function getPluginInfo($needModuleList = false)
+        {
+            $info = NULL;
+            $info = get_class_vars("FriendsList");
+
+            $info['switches'] = $this->switches;
+
+            if(isset(self::$isDisabled)) $info['isDisabled'] = self::$isDisabled;
+            $info['modules'] = NULL;
+            return $info;
+        }
+
+        function OnEvent()
+        {
+            // echo "Hello World ('". implode("', '", func_get_args()) ."')<br />\n";
+            // This is called along with arguments.
+        }
+    }
+}
+
+?>
