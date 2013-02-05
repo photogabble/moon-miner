858d7301f85a49ee992b5729974ff81a9cf119e6
TheMightyDude <themightydude@users.sourceforge.net>
2013-02-05 01:01:26 +0000

    oops, forgot to update the autoloader file.

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit61550695118d303a823048fb12ef76a9::getLoader();
+return ComposerAutoloaderInit6fb0ce3b886fbe0070f8661312fefac4::getLoader();
