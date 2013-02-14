de6d0acd69476d695a9c5fd76bf418249d6d70f3
TheMightyDude <themightydude@users.sourceforge.net>
2013-02-14 18:56:49 +0000

    Fixed Exploit in IGB Loan System.

--- includes/ibank_repay.php
+++ includes/ibank_repay.php
@@ -31,13 +31,13 @@ function ibank_repay ($db)
     global $l_ibank_invalidamount;
     global $l_ibank_back, $l_ibank_logout;
 
-    $amound = preg_replace("/[^0-9]/", "", $amount);
+    $amount = preg_replace("/[^0-9]/", "", $amount);
     if (($amount * 1) != $amount)
     {
         ibank_error ($l_ibank_invalidamount, "igb.php?command=loans");
     }
 
-    if ($amount == 0)
+    if ($amount <= 0)
     {
         ibank_error ($l_ibank_invalidamount, "igb.php?command=loans");
     }
