0bec12acdfd24aeb5be17dd38570b3bcf8a22457
The Kabal <thekabal@users.sourceforge.net>
2013-03-03 06:05:21 +0000

    Replace old functions with static methods instead

---
+++ vendor/bnt/bnttext.php
@@ -0,0 +1,34 @@
+<?php
+// Blacknova Traders - A web-based massively multiplayer space combat and trading game
+// Copyright (C) 2001-2012 Ron Harwood and the BNT development team
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// File: vendor/bnt/bnttext.php
+namespace bnt;
+
+if (strpos ($_SERVER['PHP_SELF'], 'bnttext.php')) // Prevent direct access to this file
+{
+    $error_file = $_SERVER['SCRIPT_NAME'];
+    include './error.php';
+}
+
+class bnttext
+{
+    static function gotomain ($langvars)
+    {
+        echo str_replace("[here]", "<a href='main.php'>" . $langvars['l_here'] . "</a>", $langvars['l_global_mmenu']);
+    }
+}
+?>
