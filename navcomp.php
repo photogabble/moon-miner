03c5fdb205270e11f75e762bda663b69fbf7e80c
The Kabal <thekabal@users.sourceforge.net>
2013-04-19 03:40:28 +0000

    Simplify filtering input

--- navcomp.php
+++ navcomp.php
@@ -41,17 +41,8 @@ if (!$allow_navcomp)
     die ();
 }
 
-$state = null;
-if (isset($_POST['state']))
-{
-    $state = filter_input (INPUT_POST, 'state', FILTER_SANITIZE_NUMBER_INT);
-}
-
-$stop_sector = null;
-if (isset($_POST['stop_sector']))
-{
-    $stop_sector = filter_input (INPUT_POST, 'stop_sector', FILTER_SANITIZE_NUMBER_INT);
-}
+$state = filter_input (INPUT_POST, 'state', FILTER_SANITIZE_NUMBER_INT);
+$stop_sector = filter_input (INPUT_POST, 'stop_sector', FILTER_SANITIZE_NUMBER_INT);
 
 $result = $db->Execute ("SELECT * FROM {$db->prefix}ships WHERE email = ?;", array ($_SESSION['username']));
 DbOp::dbResult ($db, $result, __LINE__, __FILE__);
