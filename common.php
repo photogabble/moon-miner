61dcd94f028cceaf4902686b86e3a11b0a2a2c78
The Kabal <thekabal@users.sourceforge.net>
2013-06-26 04:33:38 +0000

    Change the order so that the mb calls are before most of the output, just in case

--- common.php
+++ common.php
@@ -25,6 +25,17 @@ if (strpos ($_SERVER['PHP_SELF'], 'common.php')) // Prevent direct access to thi
     include_once './error.php';
 }
 
+ini_set ("include_path", ".");                    // This seems to be a problem on a few platforms, so we manually set it to avoid those problems.
+ini_set ('session.use_only_cookies', 1);          // Ensure that sessions will only be stored in a cookie
+ini_set ('session.cookie_httponly', 1);           // Make the session cookie HTTP only, a flag that helps ensure that javascript cannot tamper with the session cookie
+ini_set ('session.entropy_file', '/dev/urandom'); // Use urandom as entropy source, to help the random number generator
+ini_set ('session.entropy_length', '32');         // Increase the length of entropy gathered
+ini_set ('session.hash_function', 'sha1');        // We are going to switch this to sha512 for release, it brings far improved reduction for session collision
+ini_set ('url_rewriter.tags', '');                // Ensure that the session id is *not* passed on the url - this is a possible security hole for logins - including admin.
+date_default_timezone_set ('UTC');                // Set to your server's local time zone - PHP throws a notice if this is not set.
+mb_http_output ("UTF-8");                         // Specify that our output should be served in UTF-8, even if the PHP file served from isn't correctly saved in UTF-8.
+mb_internal_encoding ("UTF-8");                   // On many systems, this defaults to ISO-8859-1. We are explicitly a UTF-8 code base, with Unicode language variables. So set it manually.
+
 $bntreg = new BntRegistry ();
 $BenchmarkTimer = new Timer;
 $BenchmarkTimer->start(); // Start benchmarking immediately
@@ -71,17 +82,6 @@ catch (exception $e)
 }
 
 $db->prefix = $db_prefix;
-
-ini_set ("include_path", ".");                    // This seems to be a problem on a few platforms, so we manually set it to avoid those problems.
-ini_set ('session.use_only_cookies', 1);          // Ensure that sessions will only be stored in a cookie
-ini_set ('session.cookie_httponly', 1);           // Make the session cookie HTTP only, a flag that helps ensure that javascript cannot tamper with the session cookie
-ini_set ('session.entropy_file', '/dev/urandom'); // Use urandom as entropy source, to help the random number generator
-ini_set ('session.entropy_length', '32');         // Increase the length of entropy gathered
-ini_set ('session.hash_function', 'sha1');        // We are going to switch this to sha512 for release, it brings far improved reduction for session collision
-ini_set ('url_rewriter.tags', '');                // Ensure that the session id is *not* passed on the url - this is a possible security hole for logins - including admin.
-date_default_timezone_set ('UTC');                // Set to your server's local time zone - PHP throws a notice if this is not set.
-mb_http_output ("UTF-8");                         // Specify that our output should be served in UTF-8, even if the PHP file served from isn't correctly saved in UTF-8.
-mb_internal_encoding ("UTF-8");                   // On many systems, this defaults to ISO-8859-1. We are explicitly a UTF-8 code base, with Unicode language variables. So set it manually.
 
 // Get the config_values from the DB
 $debug_query = $db->Execute ("SELECT name,value FROM {$db->prefix}gameconfig");
