fb7adf016b2210cae06ec1cae1ca4081466c0397
The Kabal <thekabal@users.sourceforge.net>
2013-10-05 17:04:54 +0000

    Assorted minor cleanups

--- rsmove.php
+++ rsmove.php
@@ -16,8 +16,8 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 // File: rsmove.php
-// External variables: $destination (from get or post), int, range 1-$sector_max)
-// $engage (from get), int, range 0-2)
+// External variables: $destination (from get or post), int, range 1 - $sector_max)
+// $engage (from get), int, range 0 - 2)
 
 include './global_includes.php';
 
@@ -31,34 +31,14 @@ include './header.php';
 // Database driven language entries
 $langvars = BntTranslate::load ($db, $lang, array ('rsmove', 'common', 'global_funcs', 'global_includes', 'combat', 'footer', 'news'));
 
-// Get the Players Information.
+// Get the players information.
 $res = $db->Execute ("SELECT * FROM {$db->prefix}ships WHERE email = ?;", array ($_SESSION['username']));
 DbOp::dbResult ($db, $res, __LINE__, __FILE__);
 $playerinfo = $res->fields;
 
 echo "<h1>" . $title . "</h1>\n";
 
-/* TK version
-$destination = null;
-if (isset ($_GET['destination']))
-{
-    $destination  = (int) filter_input (INPUT_GET, 'destination', FILTER_SANITIZE_NUMBER_INT);
-}
-
-if (isset ($_POST['destination']))
-{
-    $destination  = (int) filter_input (INPUT_POST, 'destination', FILTER_SANITIZE_NUMBER_INT);
-}
-
-if ($destination < 1 || $destination > $sector_max)
-{
-    $destination = null;
-}
-*/
-
-/* TMD version 
- * Returns null if it doesn't have it set, boolean false if its set but fails to validate and the actual value if it all passes.
- */
+// Returns null if it doesn't have it set, boolean false if its set but fails to validate and the actual value if it all passes.
 $destination  = filter_input (INPUT_GET, 'destination', FILTER_VALIDATE_INT, array('options'=>array('min_range'=>1, 'max_range'=>$sector_max)));
 if ( is_null($destination) )
 {
@@ -69,10 +49,8 @@ $engage  = filter_input (INPUT_GET, 'engage', FILTER_VALIDATE_INT, array('option
 
 if ($destination === false || $engage === false)
 {
-    // Either the destination or the engage variables have failed validation.
-    /* Output:
-     * Invalid Destination
-     */
+    // Either the destination or the engage variables have failed validation. Output:
+    // Invalid destination
 
     echo $langvars['l_rs_invalid'] . ".<br><br>";
     $resx = $db->Execute ("UPDATE {$db->prefix}ships SET cleared_defences=' ' WHERE ship_id = ?;", array ($playerinfo['ship_id']));
@@ -83,14 +61,14 @@ else
     // Check if we haven't been sent the destination.
     if (is_null($destination) === true)
     {
-        // Nope, destination not sent, so show Web Form for destination.
-        /* Output:
-         * <form action='rsmove.php' method='post'>
-         * You are presently in sector X - and there are sectors available from 1 to Y.
-         * Which sector would you like to reach through Realspace? :  <input type='text' name='destination' size='10' maxlength='10'>
-         * <input type='submit' value='Compute'>
-         * </form>
-         */
+        // Nope, the destination was not sent, so show a web form asking for a destination.
+        // Output:
+        // <form action='rsmove.php' method='post'>
+        // You are presently in sector X - and there are sectors available from 1 to Y.
+        // Which sector would you like to reach through real space? :  <input type='text' name='destination' size='10' maxlength='10'>
+        // <input type='submit' value='Compute'>
+        // </form>
+
         echo "<form action='rsmove.php' method='post'>\n";
         $langvars['l_rs_insector'] = str_replace ("[sector]", $playerinfo['sector'], $langvars['l_rs_insector']);
         $langvars['l_rs_insector'] = str_replace ("[sector_max]", $sector_max - 1, $langvars['l_rs_insector']);
@@ -101,7 +79,7 @@ else
     }
     else
     {
-        // Ok, we have been given the destination value...
+        // Ok, we have been given the destination value.
         // Get the players current sector information.
         $result2 = $db->Execute ("SELECT angle1, angle2, distance FROM {$db->prefix}universe WHERE sector_id = ?;", array ($playerinfo['sector']));
         DbOp::dbResult ($db, $result2, __LINE__, __FILE__);
@@ -112,7 +90,7 @@ else
         DbOp::dbResult ($db, $result3, __LINE__, __FILE__);
         $finish = $result3->fields;
 
-        // Calculate the Math for the Distance.
+        // Calculate the distance.
         $deg = pi () / 180;
 
         $sa1 = $start['angle1'] * $deg;
@@ -126,10 +104,10 @@ else
 
         $distance = round (sqrt (pow ($x, 2) + pow ($y, 2) + pow ($z, 2)));
 
-        // Calculate the Speed of the Ship.
+        // Calculate the speed of the ship.
         $shipspeed = pow ($level_factor, $playerinfo['engines']);
 
-        // Calculate the Trip time.
+        // Calculate the trip time.
         $triptime = round ($distance / $shipspeed);
 
         if ($destination == $playerinfo['sector'])
@@ -145,31 +123,29 @@ else
         // Check to see if engage isn't set or if triptime is larger than 100 and engage is set to 1
         if ( is_null ($engage) || ($triptime > 100 && $engage == 1))
         {
-            // Handle the Fuel Scoop.
+            // Handle the fuel scoop.
             $energyscooped = HandleFuelScoop($playerinfo, $distance, $triptime, $bntreg);
 
-            /* Output:
-             * With your engines, it will take X turns to complete the journey.
-             * You would gather Y units of energy
-             */
+            // Output:
+            // With your engines, it will take X turns to complete the journey.
+            // You would gather Y units of energy.
+
             $langvars['l_rs_movetime'] = str_replace ("[triptime]", number_format ($triptime, 0, $local_number_dec_point, $local_number_thousands_sep), $langvars['l_rs_movetime']);
             $langvars['l_rs_energy'] = str_replace ("[energy]", number_format ($energyscooped, 0, $local_number_dec_point, $local_number_thousands_sep), $langvars['l_rs_energy']);
             echo $langvars['l_rs_movetime'] . "<br>" . $langvars['l_rs_energy'] . "<br><br>";
 
             if ($triptime > $playerinfo['turns'])
             {
-                // Player doesn't have enough turns.
-                /* Output:
-                 * You do not have enough turns left, and cannot embark on this journey.
-                 */
+                // Player doesn't have enough turns. Output:
+                // You do not have enough turns left, and cannot embark on this journey.
                 echo $langvars['l_rs_noturns'];
             }
             else
             {
                 // Player has enough turns.
-                /* Output:
-                 * You have X turns. <a href=rsmove.php?engage=2&destination=$destination>Engage</a> engines?
-                 */
+                // Output:
+                // You have X turns. <a href=rsmove.php?engage=2&destination=$destination>Engage</a> engines?
+
                 $langvars['l_rs_engage_link'] = "<a href=rsmove.php?engage=2&destination=$destination>" . $langvars['l_rs_engage_link'] . "</a>";
                 $langvars['l_rs_engage'] = str_replace ("[turns]", number_format ($playerinfo['turns'], 0, $local_number_dec_point, $local_number_thousands_sep), $langvars['l_rs_engage']);
                 $langvars['l_rs_engage'] = str_replace ("[engage]", $langvars['l_rs_engage_link'], $langvars['l_rs_engage']);
@@ -179,15 +155,14 @@ else
         }
         elseif ($engage > 0)
         {
-            // Handle the Fuel Scoop.
+            // Handle the fuel scoop.
             $energyscooped = HandleFuelScoop($playerinfo, $distance, $triptime, $bntreg);
 
             if ($triptime > $playerinfo['turns'])
             {
-                /* Output:
-                 * With your engines, it will take X turns to complete the journey.
-                 * You do not have enough turns left, and cannot embark on this journey.
-                 */
+                // Output:
+                // With your engines, it will take X turns to complete the journey.
+                // You do not have enough turns left, and cannot embark on this journey.
 
                 $langvars['l_rs_movetime'] = str_replace ("[triptime]", number_format ($triptime, 0, $local_number_dec_point, $local_number_thousands_sep), $langvars['l_rs_movetime']);
                 echo $langvars['l_rs_movetime'] . "<br><br>";
@@ -203,9 +178,8 @@ else
                 include_once './check_fighters.php';
                 if ($ok > 0)
                 {
-                    /* Output:
-                     * You are now in sector X. You used Y turns, and gained Z energy units.
-                     */
+                    // Output:
+                    // You are now in sector X. You used Y turns, and gained Z energy units.
 
                     $langvars = BntTranslate::load ($db, $lang, array ('rsmove', 'common', 'global_funcs', 'global_includes', 'combat', 'footer', 'news'));
                     $stamp = date ("Y-m-d H:i:s");
@@ -224,40 +198,41 @@ else
     }
 
 }
+
 BntText::gotoMain ($db, $lang, $langvars);
 include './footer.php';
 
 // Now for our functions.
-function HandleFuelScoop($playerinfo, $distance, $triptime, $bntreg)
+function HandleFuelScoop ($playerinfo, $distance, $triptime, $bntreg)
 {
-    // Check if we have a FuelScoop
+    // Check if we have a fuel scoop
     if ($playerinfo['dev_fuelscoop'] == "Y")
     {
-        // WE have a FuelScoop, no calculate the amount of Energy Scooped.
+        // We have a fuel scoop, now calculate the amount of energy scooped.
         $energyscooped = $distance * 100;
     }
     else
     {
-        // Nope, the FuelScoop won't be installed until next Tuesday (StarTrek Quote) :P
+        // Nope, the FuelScoop won't be installed until next Tuesday (Star Trek quote) :P
         $energyscooped = 0;
     }
 
-// Seems this will never happen ?
+    // Seems this will never happen ?
     if ($playerinfo['dev_fuelscoop'] == "Y" && $energyscooped == 0 && $triptime == 1)
     {
         $energyscooped = 100;
     }
 
-    // Calculate the Ship Free Power.
+    // Calculate the free power for the ship.
     $free_power = BntCalcLevels::Energy ($playerinfo['power'], $bntreg->get('level_factor') ) - $playerinfo['ship_energy'];
     if ($free_power < $energyscooped)
     {
-        // Clip the Energy Scooped to the maximum Free Power available.
+        // Limit the energy scooped to the maximum free power available.
         $energyscooped = $free_power;
     }
 
-// Not too sure what this line is doing, may need to add debugging code.
-// Could be checking for a negitive scoop value.
+    // Not too sure what this line is doing, may need to add debugging code.
+    // Could be checking for a negitive scoop value.
     if ($energyscooped < 1)
     {
         $energyscooped = 0;
@@ -265,5 +240,4 @@ function HandleFuelScoop($playerinfo, $distance, $triptime, $bntreg)
 
     return $energyscooped;
 }
-
 ?>
