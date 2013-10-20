97a5df3f9f4264d14cb55bd97b226e8d10ca7254
The Kabal <thekabal@users.sourceforge.net>
2013-10-20 20:44:06 +0000

    BntDb::logDbErrors replaces DbOp::dbResult

--- classes/BntDb.php
+++ classes/BntDb.php
@@ -27,7 +27,8 @@ if (strpos ($_SERVER['PHP_SELF'], 'BntDb.php')) // Prevent direct access to this
 
 class BntDb
 {
-    static function initDb ($ADODB_SESSION_CONNECT, $ADODB_SESSION_USER, $ADODB_SESSION_PWD, $ADODB_SESSION_DB, $ADODB_SESSION_DRIVER, $db_prefix, $dbport)
+    public static function initDb ($ADODB_SESSION_CONNECT, $ADODB_SESSION_USER, $ADODB_SESSION_PWD,
+                                   $ADODB_SESSION_DB, $ADODB_SESSION_DRIVER, $db_prefix, $dbport)
     {
         // The data field name "data" violates SQL reserved words - switch it to SESSDATA
         ADODB_Session::dataFieldName ('SESSDATA');
@@ -46,7 +47,9 @@ class BntDb
         try
         {
             $db = NewADOConnection($ADODB_SESSION_DRIVER);
-            $db_init_result = @$db->Connect ("$ADODB_SESSION_CONNECT", "$ADODB_SESSION_USER", "$ADODB_SESSION_PWD", "$ADODB_SESSION_DB");
+            // Adodb should not throw a warning here if the DB is unavailable, but it does, so we @.
+            $db_init_result = @$db->Connect ("$ADODB_SESSION_CONNECT", "$ADODB_SESSION_USER",
+                                             "$ADODB_SESSION_PWD", "$ADODB_SESSION_DB");
             // Returns Boolean True or False.
             // However ADOdb's postgres driver returns null if postgres insn't installed.
             if ($db_init_result === false || $db_init_result === 0)
@@ -65,13 +68,46 @@ class BntDb
         catch (exception $e)
         {
             // We need to display the error message onto the screen.
-            $err_msg = "Unable to connect to the " . $ADODB_SESSION_DRIVER . " Database.<br>\n Database Error: ". $db->ErrorNo () .": ". $db->ErrorMsg () ."<br>\n";
+            $err_msg = "Unable to connect to the " . $ADODB_SESSION_DRIVER .
+                       " Database.<br>\n Database Error: ". $db->ErrorNo () .
+                       ": ". $db->ErrorMsg () ."<br>\n";
             die ($err_msg);
         }
 
         $db->prefix = $db_prefix;
         // End of database work
         return $db;
+    }
+
+    public static function logDbErrors ($db, $query, $served_line, $served_page)
+    {
+        if ($db->ErrorMsg() == '')
+        {
+            return true;
+        }
+        else
+        {
+            // Convert the content of PHP_SELF (in case it has been tainted) to the correct html entities
+            $safe_script_name = htmlentities ($_SERVER['PHP_SELF']);
+            $dberror = "A Database error occurred in " . $served_page .
+                       " on line " . ($served_line-1) .
+                       " (called from: " . $safe_script_name . " the error message was: " . $db->ErrorMsg() .
+                       "and the query was " . $query;
+            $dberror = str_replace ("'", "&#39;", $dberror); // Allows the use of apostrophes.
+            if (!$db->inactive)
+            {
+                // Adodb doesn't support LogSQL on Postgresql
+                if (property_exists ($db, 'LogSQL'))
+                {
+                    if ($db->LogSQL)
+                    {
+                        BntAdminLog::writeLog ($db, LOG_RAW, $dberror);
+                    }
+                }
+            }
+
+            return $db->ErrorMsg();
+        }
     }
 }
 ?>
