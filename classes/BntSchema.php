67e478120298e66075801d9f14eaa30ead4aa9a8
The Kabal <thekabal@users.sourceforge.net>
2014-04-27 23:54:44 +0000

    Goodbye adodb's xml schema, hello SQL schema, add support for persistent tables

--- classes/BntSchema.php
+++ classes/BntSchema.php
@@ -31,7 +31,7 @@ class BntSchema
         $db->inactive = true;
 
         $i = 0;
-        $schema_files = new DirectoryIterator ("schema/");
+        $schema_files = new DirectoryIterator ("schema/mysql"); // TODO: This is hardcoded for mysql right now, but needs to be extended to handle pgsql also
         $destroy_table_results = Array ();
 
         foreach ($schema_files as $schema_filename)
@@ -39,22 +39,36 @@ class BntSchema
             $table_timer = new BntTimer;
             $table_timer->start (); // Start benchmarking
 
-            // This is to get around the issue of not having DirectoryIterator::getExtension.
-            $file_ext = pathinfo ($schema_filename->getFilename (), PATHINFO_EXTENSION);
-
-            if ($schema_filename->isFile () && $file_ext == 'xml')
+            if ($schema_filename->isFile () && $schema_filename->getExtension() == 'sql')
             {
-                $tablename = substr ($schema_filename, 0, -4);
-                $drop_res = $db->exec ('DROP TABLE ' . $db_prefix . $tablename);
-                BntDb::logDbErrors ($db, $drop_res, __LINE__, __FILE__);
-
-                if ($drop_res !== false)
+                // Routine to handle persistent database tables. If a SQL schema file starts with persist-, then it is a persistent table. Fix the name.
+                $persist_file = (substr ($schema_filename, 0, 8) === 'persist-');
+                if ($persist_file)
                 {
-                    $destroy_table_results[$i]['result'] = true;
+                    $tablename = substr ($schema_filename, 8, -4);
                 }
                 else
                 {
-                    $destroy_table_results[$i]['result'] = $db->errorInfo()[1] . ": " . $db->errorInfo()[2];
+                    $tablename = substr ($schema_filename, 0, -4);
+                }
+
+                if (!$persist_file)
+                {
+                    $drop_res = $db->exec ('DROP TABLE ' . $db_prefix . $tablename);
+                    BntDb::logDbErrors ($db, $drop_res, __LINE__, __FILE__);
+
+                    if ($drop_res !== false)
+                    {
+                        $destroy_table_results[$i]['result'] = true;
+                    }
+                    else
+                    {
+                        $destroy_table_results[$i]['result'] = $db->errorInfo()[1] . ": " . $db->errorInfo()[2];
+                    }
+                }
+                else
+                {
+                    $destroy_table_results[$i]['result'] = 'Skipped - Persistent table';
                 }
 
                 $destroy_table_results[$i]['name'] = $db_prefix . $tablename;
@@ -69,63 +83,56 @@ class BntSchema
     static function create ($db, $db_prefix)
     {
         $i = 0;
-        $schema_files = new DirectoryIterator("schema/");
+        define ("PDO_SUCCESS", (string) "00000"); // PDO gives an error code of string 00000 if successful. Not extremely helpful.
+        $schema_files = new DirectoryIterator ("schema/mysql/"); // TODO: This is hardcoded for mysql right now, but needs to be extended to handle pgsql also
 
-        // New XML Schema table creation
-        $schema = new adoSchema3 ($db);
-        $schema->setPrefix ($db_prefix);
+        // New SQL Schema table creation
         $create_table_results = Array ();
 
         foreach ($schema_files as $schema_filename)
         {
-            $schema->clearSQL ();
             $table_timer = new BntTimer;
             $table_timer->start (); // Start benchmarking
 
-            // This is to get around the issue of not having DirectoryIterator::getExtension.
-            $file_ext = pathinfo ($schema_filename->getFilename (), PATHINFO_EXTENSION);
-
-            if ($schema_filename->isFile () && $file_ext == 'xml')
+            if ($schema_filename->isFile () && $schema_filename->getExtension() == 'sql')
             {
-                $tablename = substr ($schema_filename, 0, -4);
-                $message = BntTestXml::parse ("schema/" . $schema_filename); // Test the xml file for validity before processing
-
-                if ($message !== true)
+                // Routine to handle persistent database tables. If a SQL schema file starts with persist-, then it is a persistent table. Fix the name.
+                $persist_file = (substr ($schema_filename, 0, 8) === 'persist-');
+                if ($persist_file)
                 {
-                    $table_timer->stop ();
-
-                    // TODO: This needs to be translated text
-                    $create_table_results[$i]['result'] = "XML Schema " . $schema_filename . " could not be parsed because of error: " . $message;
-                    $create_table_results[$i]['name'] = $db_prefix . $tablename;
-                    $create_table_results[$i]['time'] = $table_timer->elapsed ();
-                    $i++;
+                    $tablename = substr ($schema_filename, 8, -4);
                 }
                 else
                 {
-                    // Call ParseSchema () to build SQL from the XML schema file. Then call ExecuteSchema () to apply the resulting SQL to the database.
-                    $parsed_xml = null;
-                    $parsed_xml = $schema->ParseSchema ("schema/" . $schema_filename);
-
-                    foreach ($parsed_xml as $execute_sql)
-                    {
-                        $execute_res = $db->Execute ($execute_sql);
-                        if ($db->ErrorMsg() === 0 || $db->ErrorMsg() == null) // Adodb gives either a 0 OR a null string for success. Thanks, that is helpful (not)!
-                        {
-                            // TODO: This needs to be translated text
-                            $create_table_results[$i]['result'] = true;
-                        }
-                        else
-                        {
-                            $create_table_results[$i]['result'] = $db->ErrorNo () . ": " . $db->ErrorMsg ();
-                        }
-
-                        BntDb::logDbErrors ($db, $execute_res, __LINE__, __FILE__);
-                        $create_table_results[$i]['name'] = $db_prefix . $tablename;
-                        $table_timer->stop ();
-                        $create_table_results[$i]['time'] = $table_timer->elapsed ();
-                    }
-                    $i++;
+                    $tablename = substr ($schema_filename, 0, -4);
                 }
+
+                // Slurp the SQL call from schema, and turn it into an SQL string
+                $sql_query = file_get_contents ("schema/mysql/" . $schema_filename);
+
+                // Replace the default prefix (bnt_) with the chosen table prefix from the game.
+                $sql_query = preg_replace ('/bnt_/', $db_prefix, $sql_query);
+
+                // TODO: Remove all comments from SQL
+
+                // TODO: Test handling invalid SQL to ensure it hits the error logger below AND the visible output during running
+                $sth = $db->prepare ($sql_query);
+                $execute_res = $sth->execute ();
+
+                if ($db->errorCode() !== PDO_SUCCESS)
+                {
+                    $create_table_results[$i]['result'] = $db->errorInfo()[1] . ": " . $db->errorInfo()[2];
+                }
+                else
+                {
+                    $create_table_results[$i]['result'] = true;
+                }
+
+                BntDb::logDbErrors ($db, $execute_res, __LINE__, __FILE__);
+                $create_table_results[$i]['name'] = $db_prefix . $tablename;
+                $table_timer->stop ();
+                $create_table_results[$i]['time'] = $table_timer->elapsed ();
+                $i++;
             }
         }
         return $create_table_results;
