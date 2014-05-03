63c0c26b6a427d400e076244b0776098d63e51ae
The Kabal <thekabal@users.sourceforge.net>
2014-05-03 23:21:48 +0000

    Update copyright notices

--- config/classic_config.ini.php
+++ config/classic_config.ini.php
@@ -1,7 +1,7 @@
 ; <?php die ( 'Please do not access this page directly.' ); ?>
 ; This is the set config page, do not modify the above line.
 // Blacknova Traders - A web-based massively multiplayer space combat and trading game
-// Copyright 2001-2012 Ron Harwood and the BNT development team
+// Copyright 2001-2014 Ron Harwood and the BNT development team
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Affero General Public License as
