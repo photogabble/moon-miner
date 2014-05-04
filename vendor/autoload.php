f4007747e4cd078f986e3c5746c51fb1b2557920
The Kabal <thekabal@users.sourceforge.net>
2014-05-04 16:35:04 +0000

    Cleanup handling of adodb, so that it isn't double loaded (automap)

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInited76b02e1c4726a03b7d0f1d4ca63639::getLoader();
+return ComposerAutoloaderInitea90f5332251c4e5021123ad448b8986::getLoader();
