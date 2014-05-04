57411894144e1f7e4133f15e281ff84547bfda64
The Kabal <thekabal@users.sourceforge.net>
2014-05-04 00:20:06 +0000

    Correct indention, remove comment stars

--- classes/BadXenobe.php
+++ classes/BadXenobe.php
@@ -1634,79 +1634,76 @@ class BadXenobe
             BntPlayerLog::writeLog ($db, $playerinfo['ship_id'], LOG_RAW, "Xenobe hunt failed, target $targetinfo[character_name] was in a no attack zone (sector $targetinfo[sector]).");
         }
     }
-******
-public static function xenobeRegen($db, $playerinfo)
-{
-    global $xen_unemployment, $xenobeisdead;
 
-    // Xenobe Unempoyment Check
-    $playerinfo['credits'] = $playerinfo['credits'] + $xen_unemployment;
-    $maxenergy = BntCalcLevels::Energy ($playerinfo['power'], $level_factor); // Regenerate energy
-    if ($playerinfo['ship_energy'] <= ($maxenergy - 50))  // Stop regen when within 50 of max
+    public static function xenobeRegen($db, $playerinfo)
     {
-        $playerinfo['ship_energy'] = $playerinfo['ship_energy'] + round (($maxenergy - $playerinfo['ship_energy']) / 2); // Regen half of remaining energy
-        $gene = "regenerated Energy to $playerinfo[ship_energy] units,";
-    }
+        global $xen_unemployment, $xenobeisdead;
 
-    $maxarmor = BntCalcLevels::Armor ($playerinfo['armor'], $level_factor); // Regenerate armor
-    if ($playerinfo['armor_pts'] <= ($maxarmor - 50))  // Stop regen when within 50 of max
-    {
-        $playerinfo['armor_pts'] = $playerinfo['armor_pts'] + round (($maxarmor - $playerinfo['armor_pts']) / 2); // Regen half of remaining armor
-        $gena = "regenerated Armor to $playerinfo[armor_pts] points,";
-    }
-
-    // Buy fighters & torpedos at 6 credits per fighter
-    $available_fighters = BntCalcLevels::Fighters ($playerinfo['computer'], $level_factor) - $playerinfo['ship_fighters'];
-    if (($playerinfo['credits'] > 5) && ($available_fighters > 0))
-    {
-        if (round ($playerinfo['credits'] / 6) > $available_fighters)
+        // Xenobe Unempoyment Check
+        $playerinfo['credits'] = $playerinfo['credits'] + $xen_unemployment;
+        $maxenergy = BntCalcLevels::Energy ($playerinfo['power'], $level_factor); // Regenerate energy
+        if ($playerinfo['ship_energy'] <= ($maxenergy - 50))  // Stop regen when within 50 of max
         {
-            $purchase = ($available_fighters * 6);
-            $playerinfo['credits'] = $playerinfo['credits'] - $purchase;
-            $playerinfo['ship_fighters'] = $playerinfo['ship_fighters'] + $available_fighters;
-            $genf = "purchased $available_fighters fighters for $purchase credits,";
+            $playerinfo['ship_energy'] = $playerinfo['ship_energy'] + round (($maxenergy - $playerinfo['ship_energy']) / 2); // Regen half of remaining energy
+            $gene = "regenerated Energy to $playerinfo[ship_energy] units,";
         }
 
-        if (round ($playerinfo['credits'] / 6) <= $available_fighters)
+        $maxarmor = BntCalcLevels::Armor ($playerinfo['armor'], $level_factor); // Regenerate armor
+        if ($playerinfo['armor_pts'] <= ($maxarmor - 50))  // Stop regen when within 50 of max
         {
-            $purchase = (round ($playerinfo['credits'] / 6));
-            $playerinfo['ship_fighters'] = $playerinfo['ship_fighters'] + $purchase;
-            $genf = "purchased $purchase fighters for $playerinfo[credits] credits,";
-            $playerinfo['credits'] = 0;
-        }
-    }
-
-    // Xenobe pay 3 credits per torpedo
-    $available_torpedoes = BntCalcLevels::Torpedoes ($playerinfo['torp_launchers'], $level_factor) - $playerinfo['torps'];
-    if (($playerinfo['credits'] > 2) && ($available_torpedoes > 0))
-    {
-        if (round ($playerinfo['credits'] / 3) > $available_torpedoes)
-        {
-            $purchase = ($available_torpedoes * 3);
-            $playerinfo['credits'] = $playerinfo['credits'] - $purchase;
-            $playerinfo['torps'] = $playerinfo['torps'] + $available_torpedoes;
-            $gent = "purchased $available_torpedoes torpedoes for $purchase credits,";
+            $playerinfo['armor_pts'] = $playerinfo['armor_pts'] + round (($maxarmor - $playerinfo['armor_pts']) / 2); // Regen half of remaining armor
+            $gena = "regenerated Armor to $playerinfo[armor_pts] points,";
         }
 
-        if (round ($playerinfo['credits'] / 3) <= $available_torpedoes)
+        // Buy fighters & torpedos at 6 credits per fighter
+        $available_fighters = BntCalcLevels::Fighters ($playerinfo['computer'], $level_factor) - $playerinfo['ship_fighters'];
+        if (($playerinfo['credits'] > 5) && ($available_fighters > 0))
         {
-            $purchase = (round ($playerinfo['credits'] / 3));
-            $playerinfo['torps'] = $playerinfo['torps'] + $purchase;
-            $gent = "purchased $purchase torpedoes for $playerinfo[credits] credits,";
-            $playerinfo['credits'] = 0;
+            if (round ($playerinfo['credits'] / 6) > $available_fighters)
+            {
+                $purchase = ($available_fighters * 6);
+                $playerinfo['credits'] = $playerinfo['credits'] - $purchase;
+                $playerinfo['ship_fighters'] = $playerinfo['ship_fighters'] + $available_fighters;
+                $genf = "purchased $available_fighters fighters for $purchase credits,";
+            }
+
+            if (round ($playerinfo['credits'] / 6) <= $available_fighters)
+            {
+                $purchase = (round ($playerinfo['credits'] / 6));
+                $playerinfo['ship_fighters'] = $playerinfo['ship_fighters'] + $purchase;
+                $genf = "purchased $purchase fighters for $playerinfo[credits] credits,";
+                $playerinfo['credits'] = 0;
+            }
+        }
+
+        // Xenobe pay 3 credits per torpedo
+        $available_torpedoes = BntCalcLevels::Torpedoes ($playerinfo['torp_launchers'], $level_factor) - $playerinfo['torps'];
+        if (($playerinfo['credits'] > 2) && ($available_torpedoes > 0))
+        {
+            if (round ($playerinfo['credits'] / 3) > $available_torpedoes)
+            {
+                $purchase = ($available_torpedoes * 3);
+                $playerinfo['credits'] = $playerinfo['credits'] - $purchase;
+                $playerinfo['torps'] = $playerinfo['torps'] + $available_torpedoes;
+                $gent = "purchased $available_torpedoes torpedoes for $purchase credits,";
+            }
+
+            if (round ($playerinfo['credits'] / 3) <= $available_torpedoes)
+            {
+                $purchase = (round ($playerinfo['credits'] / 3));
+                $playerinfo['torps'] = $playerinfo['torps'] + $purchase;
+                $gent = "purchased $purchase torpedoes for $playerinfo[credits] credits,";
+                $playerinfo['credits'] = 0;
+            }
+        }
+
+        // Update Xenobe record
+        $resg = $db->Execute ("UPDATE {$db->prefix}ships SET ship_energy=?, armor_pts=?, ship_fighters=?, torps=?, credits=? WHERE ship_id=?", array ($playerinfo['ship_energy'], $playerinfo['armor_pts'], $playerinfo['ship_fighters'], $playerinfo['torps'], $playerinfo['credits'], $playerinfo['ship_id']));
+        BntDb::logDbErrors ($db, $resg, __LINE__, __FILE__);
+        if (!$gene=='' || !$gena=='' || !$genf=='' || !$gent=='')
+        {
+            BntPlayerLog::writeLog ($db, $playerinfo['ship_id'], LOG_RAW, "Xenobe $gene $gena $genf $gent and has been updated.");
         }
     }
-
-    // Update Xenobe record
-    $resg = $db->Execute ("UPDATE {$db->prefix}ships SET ship_energy=?, armor_pts=?, ship_fighters=?, torps=?, credits=? WHERE ship_id=?", array ($playerinfo['ship_energy'], $playerinfo['armor_pts'], $playerinfo['ship_fighters'], $playerinfo['torps'], $playerinfo['credits'], $playerinfo['ship_id']));
-    BntDb::logDbErrors ($db, $resg, __LINE__, __FILE__);
-    if (!$gene=='' || !$gena=='' || !$genf=='' || !$gent=='')
-    {
-        BntPlayerLog::writeLog ($db, $playerinfo['ship_id'], LOG_RAW, "Xenobe $gene $gena $genf $gent and has been updated.");
-    }
-}
-
-******
-
 }
 ?>
