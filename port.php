9b6c89fcf49fbcd1120f750cc254c602d879a72f
The Kabal <thekabal@users.sourceforge.net>
2014-05-12 04:40:09 +0000

    Eliminate global variables

--- port.php
+++ port.php
@@ -641,10 +641,8 @@ elseif ($sectorinfo['port_type'] == "special")
     $onclick =  "ONCLICK=\"count_total()\"";
 
     // Create dropdowns when called
-    function dropdown($element_name, $current_value)
+    function dropdown($element_name, $current_value, $onchange, $max_upgrades_devices)
     {
-        global $onchange;
-        global $max_upgrades_devices;
         $i = $current_value;
         $dropdownvar = "<select size='1' name='$element_name'";
         $dropdownvar = "$dropdownvar $onchange>\n";
@@ -719,7 +717,7 @@ elseif ($sectorinfo['port_type'] == "special")
     echo "    <td><input type=text readonly class='portcosts1' name=hull_costper value='0' tabindex='0' $onblur></td>\n";
     echo "    <td>" . number_format ($playerinfo['hull'], 0, $langvars['local_number_dec_point'], $langvars['local_number_thousands_sep']) . "</td>\n";
     echo "    <td>\n       ";
-    echo dropdown ("hull_upgrade", $playerinfo['hull']);
+    echo dropdown ("hull_upgrade", $playerinfo['hull'], $onchange, $max_upgrades_devices);
     echo "    </td>\n";
     echo "   </tr>\n";
     echo "   <tr>\n";
@@ -749,7 +747,7 @@ elseif ($sectorinfo['port_type'] == "special")
     echo "    <td><input type=text readonly class='portcosts2' size=10 name=engine_costper value='0' tabindex='0' $onblur></td>\n";
     echo "    <td>" . number_format ($playerinfo['engines'], 0, $langvars['local_number_dec_point'], $langvars['local_number_thousands_sep']) . "</td>\n";
     echo "    <td>\n       ";
-    echo dropdown ("engine_upgrade", $playerinfo['engines']);
+    echo dropdown ("engine_upgrade", $playerinfo['engines'], $onchange, $max_upgrades_devices);
     echo "    </td>\n";
     echo "   </tr>\n";
     echo "   <tr>\n";
@@ -774,7 +772,7 @@ elseif ($sectorinfo['port_type'] == "special")
     echo "    <td><input type=text readonly class='portcosts1' name=power_costper value='0' tabindex='0' $onblur></td>\n";
     echo "    <td>" . number_format ($playerinfo['power'], 0, $langvars['local_number_dec_point'], $langvars['local_number_thousands_sep']) . "</td>\n";
     echo "    <td>\n       ";
-    echo dropdown ("power_upgrade", $playerinfo['power']);
+    echo dropdown ("power_upgrade", $playerinfo['power'], $onchange, $max_upgrades_devices);
     echo "    </td>\n";
     echo "  </tr>\n";
     echo "  <tr>\n";
@@ -804,7 +802,7 @@ elseif ($sectorinfo['port_type'] == "special")
     echo "    <td><input type=text readonly class='portcosts2' name=computer_costper value='0' tabindex='0' $onblur></td>\n";
     echo "    <td>" . number_format ($playerinfo['computer'], 0, $langvars['local_number_dec_point'], $langvars['local_number_thousands_sep']) . "</td>\n";
     echo "    <td>\n       ";
-    echo dropdown ("computer_upgrade", $playerinfo['computer']);
+    echo dropdown ("computer_upgrade", $playerinfo['computer'], $onchange, $max_upgrades_devices);
     echo "    </td>\n";
     echo "  </tr>\n";
     echo "  <tr>\n";
@@ -817,7 +815,7 @@ elseif ($sectorinfo['port_type'] == "special")
     echo "    <td><input type=text readonly class='portcosts1' name=sensors_costper value='0' tabindex='0' $onblur></td>\n";
     echo "    <td>" . number_format ($playerinfo['sensors'], 0, $langvars['local_number_dec_point'], $langvars['local_number_thousands_sep']) . "</td>\n";
     echo "    <td>\n       ";
-    echo dropdown ("sensors_upgrade", $playerinfo['sensors']);
+    echo dropdown ("sensors_upgrade", $playerinfo['sensors'], $onchange, $max_upgrades_devices);
     echo "    </td>\n";
     echo "  </tr>";
     echo "  <tr>\n";
@@ -830,7 +828,7 @@ elseif ($sectorinfo['port_type'] == "special")
     echo "    <td><input type=text readonly class='portcosts2' name=beams_costper value='0' tabindex='0' $onblur></td>";
     echo "    <td>" . number_format ($playerinfo['beams'], 0, $langvars['local_number_dec_point'], $langvars['local_number_thousands_sep']) . "</td>\n";
     echo "    <td>\n       ";
-    echo dropdown ("beams_upgrade", $playerinfo['beams']);
+    echo dropdown ("beams_upgrade", $playerinfo['beams'], $onchange, $max_upgrades_devices);
     echo "    </td>\n";
     echo "  </tr>\n";
     echo "  <tr>\n";
@@ -853,7 +851,7 @@ elseif ($sectorinfo['port_type'] == "special")
     echo "    <td><input type=text readonly class='portcosts1' name=armor_costper value='0' tabindex='0' $onblur></td>\n";
     echo "    <td>" . number_format ($playerinfo['armor'], 0, $langvars['local_number_dec_point'], $langvars['local_number_thousands_sep']) . "</td>\n";
     echo "    <td>\n       ";
-    echo dropdown ("armor_upgrade", $playerinfo['armor']);
+    echo dropdown ("armor_upgrade", $playerinfo['armor'], $onchange, $max_upgrades_devices);
     echo "    </td>\n";
     echo "  </tr>\n";
     echo "  <tr>\n";
@@ -876,7 +874,7 @@ elseif ($sectorinfo['port_type'] == "special")
     echo "    <td><input type=text readonly class='portcosts2' name=cloak_costper value='0' tabindex='0' $onblur $onfocus></td>\n";
     echo "    <td>" . number_format ($playerinfo['cloak'], 0, $langvars['local_number_dec_point'], $langvars['local_number_thousands_sep']) . "</td>\n";
     echo "    <td>\n       ";
-    echo dropdown ("cloak_upgrade", $playerinfo['cloak']);
+    echo dropdown ("cloak_upgrade", $playerinfo['cloak'], $onchange, $max_upgrades_devices);
     echo "    </td>\n";
     echo "  </tr>\n";
     echo "  <tr>\n";
@@ -899,7 +897,7 @@ elseif ($sectorinfo['port_type'] == "special")
     echo "    <td><input type=text readonly class='portcosts1' name=torp_launchers_costper value='0' tabindex='0' $onblur></td>\n";
     echo "    <td>" . number_format ($playerinfo['torp_launchers'], 0, $langvars['local_number_dec_point'], $langvars['local_number_thousands_sep']) . "</td>\n";
     echo "    <td>\n       ";
-    echo dropdown ("torp_launchers_upgrade", $playerinfo['torp_launchers']);
+    echo dropdown ("torp_launchers_upgrade", $playerinfo['torp_launchers'], $onchange, $max_upgrades_devices);
     echo "    </td>\n";
     echo "  </tr>\n";
     echo "  <tr>\n";
@@ -912,7 +910,7 @@ elseif ($sectorinfo['port_type'] == "special")
     echo "    <td><input type=text readonly class='portcosts2' name=shields_costper value='0' tabindex='0' $onblur></td>\n";
     echo "    <td>" . number_format ($playerinfo['shields'], 0, $langvars['local_number_dec_point'], $langvars['local_number_thousands_sep']) . "</td>\n";
     echo "    <td>\n       ";
-    echo dropdown ("shields_upgrade", $playerinfo['shields']);
+    echo dropdown ("shields_upgrade", $playerinfo['shields'], $onchange, $max_upgrades_devices);
     echo "    </td>\n";
     echo "  </tr>\n";
     echo " </table>\n";
