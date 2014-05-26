ca1599c015886c35e037a068204c2ccd4bce2eeb
The Kabal <thekabal@users.sourceforge.net>
2014-05-26 05:19:31 +0000

    Cleanup, formatting

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 $index_page = true;
 include './global_includes.php';
 
-if (!isset ($_GET['lang']))
+if (!isset($_GET['lang']))
 {
     $_GET['lang'] = null;
     $lang = $bntreg->default_lang;
@@ -33,7 +33,7 @@ else
 }
 
 // Check to see if the language database has been installed yet.
-$result = $db->Execute ("SELECT name, value FROM {$db->prefix}languages WHERE category = ? AND section = ?", array ('common', $lang));
+$result = $db->Execute("SELECT name, value FROM {$db->prefix}languages WHERE category = ? AND section = ?", array ('common', $lang));
 
 if (($result instanceof ADORecordSet) && ($result != false)) // Before DB is installed, result will give false.
 {
@@ -41,12 +41,12 @@ if (($result instanceof ADORecordSet) && ($result != false)) // Before DB is ins
 else
 {
     // If not, redirect to create_universe.
-    header ("Location: create_universe.php");
-    die ();
+    header("Location: create_universe.php");
+    die();
 }
 
 // Database driven language entries
-$langvars = Bnt\Translate::load ($db, $lang, array ('main', 'login', 'logout', 'index', 'common','regional', 'footer','global_includes'));
+$langvars = Bnt\Translate::load($db, $lang, array ('main', 'login', 'logout', 'index', 'common','regional', 'footer','global_includes'));
 
 $variables = null;
 $variables['lang'] = $lang;
@@ -56,46 +56,42 @@ $variables['admin_mail'] = $bntreg->admin_mail;
 $variables['body_class'] = 'index';
 
 // Now Get a list of supported languages etc.
-$lang_rs = $db->GetAll ("SELECT section, name, value FROM {$db->prefix}languages WHERE category = 'regional' AND (name = 'local_lang_name' OR name = 'local_lang_flag') ORDER BY section, name;");
+$lang_rs = $db->GetAll("SELECT section, name, value FROM {$db->prefix}languages WHERE category = 'regional' AND (name = 'local_lang_name' OR name = 'local_lang_flag') ORDER BY section, name;");
 $list_of_langs = array();
 
-if (is_array ($lang_rs) === true && count ($lang_rs) >= 2)
+if (is_array($lang_rs) === true && count($lang_rs) >= 2)
 {
     foreach ($lang_rs as $id => $langinfo)
     {
-        if (array_key_exists ($langinfo['section'], $list_of_langs) === false)
+        if (array_key_exists($langinfo['section'], $list_of_langs) === false)
         {
             $list_of_langs[$langinfo['section']] = array();
         }
 
-        switch ($langinfo['name'])
+        switch($langinfo['name'])
         {
             case 'local_lang_flag':
-            {
-                $list_of_langs[$langinfo['section']] = array_merge ($list_of_langs[$langinfo['section']], array('flag' => $langinfo['value']));
+                $list_of_langs[$langinfo['section']] = array_merge($list_of_langs[$langinfo['section']], array('flag' => $langinfo['value']));
                 break;
-            }
 
             case 'local_lang_name':
-            {
-                $list_of_langs[$langinfo['section']] = array_merge ($list_of_langs[$langinfo['section']], array('lang_name' => $langinfo['value']));
+                $list_of_langs[$langinfo['section']] = array_merge($list_of_langs[$langinfo['section']], array('lang_name' => $langinfo['value']));
                 break;
-            }
         }
     }
 
     // Extract our default language, and remove it from the list of supported languages.
     $our_lang = $list_of_langs[$lang];
-    unset ($list_of_langs[$lang]);
+    unset($list_of_langs[$lang]);
 
     // Add our default language back in, this should be put at the end of the list.
     $list_of_langs[$lang] = $our_lang;
-    unset ($our_lang);
+    unset($our_lang);
 }
 
 // Add the games supported languages and unset no longer required variables.
 $variables['list_of_langs'] = $list_of_langs;
-unset ($list_of_langs, $lang_rs);
+unset($list_of_langs, $lang_rs);
 
 $variables['template'] = $bntreg->default_template; // Temporarily set the template to the default template until we have a user option
 // Now set a container for the variables and langvars and send them off to the template system
@@ -104,9 +100,7 @@ $langvars['container'] = "langvars";
 
 // Pull in footer variables from footer_t.php
 include './footer_t.php';
-$template->addVariables ('langvars', $langvars);
-$template->addVariables ('variables', $variables);
-$template->display ("index.tpl");
-// Bnt\Class::method($blah);
-// BntNew\BntError::test("testing test");
+$template->addVariables('langvars', $langvars);
+$template->addVariables('variables', $variables);
+$template->display('index.tpl');
 ?>
