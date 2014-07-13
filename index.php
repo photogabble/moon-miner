ab9d2fc247e18d428c77a84f5ac6081b56ac656a
The Kabal <thekabal@users.sourceforge.net>
2014-07-13 00:16:54 +0000

    Move list of installed languages to a class

--- index.php
+++ index.php
@@ -20,13 +20,14 @@
 $index_page = true;
 require_once './common.php';
 
+// If language has not been selected, give default
 if (!isset($_GET['lang']))
 {
     $_GET['lang'] = null;
     $lang = $bntreg->default_lang;
     $link = '';
 }
-else
+else // Else set the language as requested by player
 {
     $lang = $_GET['lang'];
     $link = '?lang=' . $lang;
@@ -34,7 +35,7 @@ else
 
 if (!Bnt\Db::isActive($pdo_db))
 {
-    // If DB is not active, redirect to CU to run install
+    // If DB is not active, redirect to create universe to run install
     header('Location: create_universe.php');
     die();
 }
@@ -50,50 +51,12 @@ $variables['link_forums'] = $bntreg->link_forums;
 $variables['admin_mail'] = $bntreg->admin_mail;
 $variables['body_class'] = 'index';
 
-// Now Get a list of supported languages etc.
-$sql = "SELECT section, name, value FROM {$pdo_db->prefix}languages WHERE category = :category AND (name = :name1 OR name = :name2) ORDER BY section, name;";
-$stmt = $pdo_db->prepare($sql);
-$stmt->bindValue(':category', 'regional');
-$stmt->bindValue(':name1', 'local_lang_name');
-$stmt->bindValue(':name2', 'local_lang_flag');
-$stmt->execute();
-$lang_rs = $stmt->fetchAll();
-
-$list_of_langs = array();
-
-if (is_array($lang_rs) === true && count($lang_rs) >= 2)
-{
-    foreach ($lang_rs as $id => $langinfo)
-    {
-        if (array_key_exists($langinfo['section'], $list_of_langs) === false)
-        {
-            $list_of_langs[$langinfo['section']] = array();
-        }
-
-        switch($langinfo['name'])
-        {
-            case 'local_lang_flag':
-                $list_of_langs[$langinfo['section']] = array_merge($list_of_langs[$langinfo['section']], array('flag' => $langinfo['value']));
-                break;
-
-            case 'local_lang_name':
-                $list_of_langs[$langinfo['section']] = array_merge($list_of_langs[$langinfo['section']], array('lang_name' => $langinfo['value']));
-                break;
-        }
-    }
-
-    // Extract our default language, and remove it from the list of supported languages.
-    $our_lang = $list_of_langs[$lang];
-    unset($list_of_langs[$lang]);
-
-    // Add our default language back in, this should be put at the end of the list.
-    $list_of_langs[$lang] = $our_lang;
-    unset($our_lang);
-}
+// Get a list of available languages
+$list_of_langs = Bnt\Languages::listAvailable($pdo_db, $lang);
 
 // Add the games supported languages and unset no longer required variables.
 $variables['list_of_langs'] = $list_of_langs;
-unset($list_of_langs, $lang_rs);
+unset($list_of_langs);
 
 $variables['template'] = $bntreg->default_template; // Temporarily set the template to the default template until we have a user option
 // Now set a container for the variables and langvars and send them off to the template system
