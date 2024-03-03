078bb599b18aea496a6ce1af4f79f6fe0af24e4e
Simon <carbontwelve@users.noreply.github.com>
2024-03-03 10:18:56 +0000

    Refactor homepage template into blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
-{*
+{{--
     Blacknova Traders - A web-based massively multiplayer space combat and trading game
-    Copyright (C) 2001-2014 Ron Harwood and the BNT development team.
+    Copyright (C) 2001-2024 Ron Harwood and the BNT development team.
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
@@ -15,51 +15,53 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    File: index.tpl
-*}
+    File: index.blade.php
+--}}
 
-{extends file="layout.tpl"}
+@extends('layouts.layout', ['body_class' => 'index', 'include_ckeditor' => false, 'news' => null, 'suppress_logo' => false, 'footer_show_debug' => true, 'update_ticker' => null, 'players_online' => 1])
 
-{block name=body}
-<div class="index-header"><img height="150" width="994" style="width:100%" class="index" src="templates/{$variables['template']}/images/header1.png" alt="{$langvars['l_bnt']}"></div>
+@section('title', __('index.l_welcome_bnt'))
 
-<div class="index-flags">
-{foreach from=$variables['list_of_langs'] key=lang_file item=language}
-<a href="index.php?lang={$lang_file}"><img width="24" height="16" src="templates/{$variables['template']}/images/flags/{$language['flag']}.png" alt="{$language['lang_name']}"></a>
-{/foreach}
-</div>
+@section('content')
+    <div class="index-header"><img height="150" width="994" style="width:100%" class="index" src="/images/header1.png" alt="{$langvars['l_bnt']}"></div>
 
-<div class="index-header-text">{$langvars['l_bnt']}</div>
-<br>
-<h2 style="display:none">{$langvars['l_navigation']}</h2>
-<div class="navigation" role="navigation">
-<ul class="navigation">
-<li class="navigation"><a href="new.php{$variables['link']}"><span class="button blue"><span class="shine"></span>{$langvars['l_new_player']}</span></a></li>
-<li class="navigation"><a href="mailto:{$variables['admin_mail']}"><span class="button gray"><span class="shine"></span>{$langvars['l_login_emailus']}</span></a></li>
-<li class="navigation"><a href="ranking.php{$variables['link']}"><span class="button purple"><span class="shine"></span>{$langvars['l_rankings']}</span></a></li>
-<li class="navigation"><a href="faq.php{$variables['link']}"><span class="button brown"><span class="shine"></span>{$langvars['l_faq']}</span></a></li>
-<li class="navigation"><a href="settings.php{$variables['link']}"><span class="button red"><span class="shine"></span>{$langvars['l_settings']}</span></a></li>
-<li class="navigation"><a href="//{$variables['link_forums']}"><span class="button orange"><span class="shine"></span>{$langvars['l_forums']}</span></a></li>
-</ul></div><br style="clear:both">
-<div><p></p></div>
-<div class="index-welcome">
-<h1 class="index-h1">{$langvars['l_welcome_bnt']}</h1>
-<p>{$langvars['l_bnt_description']}<br></p>
-<form accept-charset="utf-8" action="login2.php{$variables['link']}" method="post">
-<dl class="twocolumn-form">
-<dt><label for="email">{$langvars['l_login_email']}</label></dt>
-<dd><input type="email" id="email" name="email" size="20" maxlength="40" placeholder="someone@example.com"></dd>
-<dt><label for="pass">{$langvars['l_login_pw']}:</label></dt>
-<dd><input type="password" id="pass" name="pass" size="20" maxlength="20"></dd>
-</dl>
-<br style="clear:both">
-<div style="text-align:center">{$langvars['l_login_forgotpw']}</div><br>
-<div style="text-align:center">
-<span class="button green"><a class="nocolor" href="#" onclick="document.forms[0].submit();return false;"><span class="shine"></span>{$langvars['l_login_title']}</a></span>
-<div style="width: 0; height: 0; overflow: hidden;"><input type="submit" value="{$langvars['l_login_title']}"></div>
-</div>
-</form>
-<br>
-<p class="cookie-warning">{$langvars['l_cookie_warning']}</p></div>
-<br>
-{/block}
+    <div class="index-flags">
+        @foreach(\App\Helpers\Languages::listAvailable() as $id => $lang)
+            <a href="index.php?lang={{ $id }}"><img width="24" height="16" src="/images/flags/{{ $lang['flag'] }}.png" alt="{{ $lang['name'] }}"></a>
+        @endforeach
+    </div>
+
+    <div class="index-header-text">{{ __('index.l_bnt') }}</div>
+    <br>
+    <h2 style="display:none">{{ __('index.l_navigation') }}</h2>
+    <div class="navigation" role="navigation">
+        <ul class="navigation">
+            <li class="navigation"><a href="new.php"><span class="button blue"><span class="shine"></span>{{ __('index.l_new_player') }}</span></a></li>
+            <li class="navigation"><a href="#"><span class="button gray"><span class="shine"></span>{{ __('login.l_login_emailus') }}</span></a></li>
+            <li class="navigation"><a href="ranking.php"><span class="button purple"><span class="shine"></span>{{ __('main.l_rankings') }}</span></a></li>
+            <li class="navigation"><a href="faq.php"><span class="button brown"><span class="shine"></span>{{ __('main.l_faq') }}</span></a></li>
+            <li class="navigation"><a href="settings.php"><span class="button red"><span class="shine"></span>{{ __('main.l_settings') }}</span></a></li>
+            <li class="navigation"><a href="{{ setting('game.link_forums') }}"><span class="button orange"><span class="shine"></span>{{ __('main.l_forums') }}</span></a></li>
+        </ul></div><br style="clear:both">
+    <div><p></p></div>
+    <div class="index-welcome">
+        <h1 class="index-h1">{{ __('index.l_welcome_bnt') }}</h1>
+        <p>{{ __('index.l_bnt_description') }}<br></p>
+        <form accept-charset="utf-8" action="login2.php" method="post">
+            <dl class="twocolumn-form">
+                <dt><label for="email">{{ __('login.l_login_email') }}</label></dt>
+                <dd><input type="email" id="email" name="email" size="20" maxlength="40" placeholder="someone@example.com"></dd>
+                <dt><label for="pass">{{ __('login.l_login_pw') }}:</label></dt>
+                <dd><input type="password" id="pass" name="pass" size="20" maxlength="20"></dd>
+            </dl>
+            <br style="clear:both">
+            <div style="text-align:center">{{ __('login.l_login_forgotpw') }}</div><br>
+            <div style="text-align:center">
+                <span class="button green"><a class="nocolor" href="#" onclick="document.forms[0].submit();return false;"><span class="shine"></span>{{ __('login.l_login_title') }}</a></span>
+                <div style="width: 0; height: 0; overflow: hidden;"><input type="submit" value="{{ __('login.l_login_title') }}"></div>
+            </div>
+        </form>
+        <br>
+        <p class="cookie-warning">{{ __('index.l_cookie_warning') }}</p></div>
+    <br>
+@endsection
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,6 +1,6 @@
-{*
+{{--
     Blacknova Traders - A web-based massively multiplayer space combat and trading game
-    Copyright (C) 2001-2014 Ron Harwood and the BNT development team.
+    Copyright (C) 2001-2024 Ron Harwood and the BNT development team.
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
@@ -15,127 +15,120 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    File: layout.tpl
-*}
-<!DOCTYPE html>
-<html lang="{$langvars['l_lang_attribute']}">
+    File: layout.blade.php
+--}}
+    <!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <!-- START OF HEADER -->
-  <head>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="Description" content="A free online game - Open source, web game, with multiplayer space exploration">
     <meta name="Keywords" content="Free, online, game, Open source, web game, multiplayer, space, exploration, blacknova, traders">
     <meta name="Rating" content="General">
-    <link rel="shortcut icon" href="{$template_dir}/images/bntfavicon.ico">
-    <link rel="stylesheet" type="text/css" href="{$template_dir}/styles/bnt-prime.css.php">
-{if $variables['body_class'] != 'bnt'}
-    <link rel="stylesheet" type="text/css" href="{$template_dir}/styles/{$variables['body_class']}.css.php">
-{else}
-    <link rel="stylesheet" type="text/css" href="{$template_dir}/styles/main.css.php">
-{/if}
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <!-- Styles -->
+    @vite("resources/css/app.css")
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Ubuntu">
-{if isset($variables['title'])}
-    <title>{block name=title}{$variables['title']}{/block}</title>
-{/if}
-{if isset($variables['include_ckeditor'])}
-    <script src="{$template_dir}/javascript/ckeditor/ckeditor.js"></script>
-{/if}
-    <script async src="{$template_dir}/javascript/framebuster.js.php"></script>
-  </head>
+
+    <title>@yield('title')</title>
+    @if($include_ckeditor)
+        <script src="/javascript/ckeditor/ckeditor.js"></script>
+    @endif
+    <script async src="/javascript/framebuster.js.php"></script>
+</head>
 <!-- END OF HEADER -->
 
-{if !isset($variables['body_class'])}
-{$variables['body_class'] = "bnt"}
-{/if}
-  <body class="{$variables['body_class']}">
+<body class="{{ $body_class ?? 'bnt' }}">
 <div class="wrapper">
 
-<!-- START OF BODY -->
-{block name=body_title}{/block}
-{block name=body}{/block}
-<!-- END OF BODY -->
+    <!-- START OF BODY -->
+    @yield('content')
+    <!-- END OF BODY -->
 
-<!-- START OF FOOTER -->
-<div class="footer">
-{if isset($news)}
-<br>
-<script type="text/javascript" src="{$template_dir}/javascript/newsticker.js.php"></script>
-<p id="news_ticker" class="faderlines" style="width:602px; margin:auto; text-align:center;">{$langvars['l_news_broken']}</p>
-<script>
-// News Ticker Constructor.
-news = new newsTicker();
+    <!-- START OF FOOTER -->
+    <div class="footer">
+        @if($news)
+            <br>
+            <script type="text/javascript" src="/javascript/newsticker.js.php"></script>
+            <p id="news_ticker" class="faderlines" style="width:602px; margin:auto; text-align:center;">{{ __('news.l_news_broken') }}</p>
+            <script>
+                // News Ticker Constructor.
+                news = new newsTicker();
 
-// I have put in some safaty precautions, but just in case always check the return value from initTicker().
-if (news.initTicker("news_ticker") == true)
-{
-    // Set the width of the Ticker (in pixles)
-    news.Width(500);
+                // I have put in some safaty precautions, but just in case always check the return value from initTicker().
+                if (news.initTicker("news_ticker") == true) {
+                    // Set the width of the Ticker (in pixles)
+                    news.Width(500);
 
-    // Sets the Interval/Update Time in seconds.
-    news.Interval(5);
+                    // Sets the Interval/Update Time in seconds.
+                    news.Interval(5);
 
-    // I have decided on adding single news articles at a time due to it makes it more easier to add when using PHP or XSL.
-    // We can supply the information by either of the following ways:
-    // 1: Supply the information from a Database and inserting it with PHP.
-    // 2: Supply the information from a Database and convert it into XML (for formatting) and have the XSLT Stylesheet extract the information and insert it.
-{* Cycle through the player list *}
-{foreach $news as $article}
-    news.addArticle('{$article['url']}', '{$article['text']}', '{$article['type']}', {$article['delay']});
-{/foreach}
+                    // I have decided on adding single news articles at a time due to it makes it more easier to add when using PHP or XSL.
+                    // We can supply the information by either of the following ways:
+                    // 1: Supply the information from a Database and inserting it with PHP.
+                    // 2: Supply the information from a Database and convert it into XML (for formatting) and have the XSLT Stylesheet extract the information and insert it.
 
-    // Starts the Ticker.
-    news.startTicker();
+                    {{-- Cycle through the player list --}}
+                    @foreach($news as $article)
+                    news.addArticle('{{ $article['url'] }}', '{{ $article['text'] }}', '{{ $article['type'] }}', '{{ $article['delay'] }}');
+                    @endforeach
 
-    // If for some reason you need to stop the Ticker use the following line.
-    // news.stopTicker();
-}
-</script>
+                    // Starts the Ticker.
+                    news.startTicker();
 
-{/if}
-<br>
+                    // If for some reason you need to stop the Ticker use the following line.
+                    // news.stopTicker();
+                }
+            </script>
 
-{* Items to the left (SF logo) and to the right (mem, copyright, news) *}
-{if $variables['suppress_logo'] == false}
-    <p style='float:left; text-align:left'><a href='http://www.sourceforge.net/projects/blacknova'><img style="border:none;" width="{$variables['sf_logo_width']}" height="{$variables['sf_logo_height']}" src="http://sflogo.sourceforge.net/sflogo.php?group_id=14248&amp;type={$variables['sf_logo_type']}" alt="Blacknova Traders at SourceForge.net"></a></p>
-{/if}
-    <p style="font-size:smaller; float:right; text-align:right"><a class="new_link" href="news.php{$variables['sf_logo_link']}">{$langvars['l_local_news']}</a>
-    <br>&copy; 2000-{$variables['cur_year']} Ron Harwood &amp; the BNT Dev team
+        @endif
+        <br>
 
-{if isset($variables['footer_show_debug']) && $variables['footer_show_debug'] == true}
-    <br>{$variables['elapsed']} {$langvars['l_seconds']} {$langvars['l_time_gen_page']} / {$variables['mem_peak_usage']}{$langvars['l_peak_mem']}
-{/if}
-</p>
+        {{-- Items to the left (SF logo) and to the right (mem, copyright, news) --}}
+        @if(!$suppress_logo)
+            <p style='float:left; text-align:left'><a href='http://www.sourceforge.net/projects/blacknova'><img style="border:none;" width="{$variables['sf_logo_width']}" height="{$variables['sf_logo_height']}" src="http://sflogo.sourceforge.net/sflogo.php?group_id=14248&amp;type={$variables['sf_logo_type']}" alt="Blacknova Traders at SourceForge.net"></a></p>
+        @endif
+        <p style="font-size:smaller; float:right; text-align:right"><a class="new_link" href="news.php">{{ __('global_includes.l_local_news') }}</a>
+            <br>&copy; 2000-{{ date('Y') }} Ron Harwood &amp; the BNT Dev team
 
-<p style="text-align:center;">
+            @if($footer_show_debug)
+                <br>{{ render_time_seconds() }} {{ __('footer.l_seconds') }} {{ __('footer.l_time_gen_page') }} / {{ mem_peak_usage() }}{{ __('footer.l_peak_mem') }}
+            @endif
+        </p>
 
-{* Handle the Servers Update Ticker here *}
-{if isset($variables['update_ticker']['display']) && $variables['update_ticker']['display'] == true}
-    <script type='text/javascript' src='{$template_dir}/javascript/updateticker.js.php'></script>
-    <script>
-        var seconds = {$variables['update_ticker']['seconds_left']};
-        var nextInterval = new Date().getTime();
-        var maxTicks = ({$variables['update_ticker']['sched_ticks']} * 60);
-        var l_running_update = '{$langvars['l_running_update']}';
-        var l_footer_until_update = '{$langvars['l_footer_until_update']}';
+        <p style="text-align:center;">
+            {{-- Handle the Servers Update Ticker here  --}}
+            @if($update_ticker && $update_ticker['display'] === true)
+                <script type='text/javascript' src='/javascript/updateticker.js'></script>
+                <script>
+                    const seconds = {{ $update_ticker['seconds_left'] }};
+                    const nextInterval = new Date().getTime();
+                    const maxTicks = ({{ $update_ticker['sched_ticks'] }} * 60);
+                    const l_running_update = '{{ __('footer.l_running_update') }}';
+                    const l_footer_until_update = '{{ __('footer.l_footer_until_update') }}';
 
-        setTimeout("NextUpdate();", 100);
-    </script>
-    <span id=update_ticker>{$langvars['l_please_wait']}</span>
-{/if}
+                    setTimeout("NextUpdate();", 100);
+                </script>
+                <span id=update_ticker>{{ __('footer.l_please_wait') }}</span>
+            @endif
 
-{* End of Servers Update Ticker *}
+            {{-- End of Servers Update Ticker --}}
 
-<br>
-{* Handle the Online Players Counter *}
-{if isset($variables['players_online']) && $variables['players_online'] == 1}
-{$langvars['l_footer_one_player_on']}
-{else}
-{$langvars['l_footer_players_on_1']} {$variables['players_online']} {$langvars['l_footer_players_on_2']}
-{/if}
-</p>
-{* End of Online Players Counter *}
+            <br>
+            {{-- Handle the Online Players Counter --}}
+            @if($players_online === 1)
+                {{ __('footer.l_footer_one_player_on') }}
+            @else
+                {{ __('footer.l_footer_players_on_1') }} {{ $players_online }} {{ __('footer.l_footer_players_on_2') }}
+            @endif
+        </p>
+        {{-- End of Online Players Counter --}}
 
-</div></div>
+    </div></div>
 <!-- END OF FOOTER -->
 
-  </body>
+</body>
 </html>
