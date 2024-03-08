49ed4187715e282a872cc8a71cb1ca6f5f394e58
Simon <carbontwelve@users.noreply.github.com>
2024-03-08 10:02:12 +0000

    Move news ticker js into resources path

--- resources/js/news_ticker.js
+++ resources/js/news_ticker.js
@@ -1,16 +1,3 @@
-<?php
-require_once '../../../vendor/autoload.php';           // Load the auto-loader
-ob_start (array('Bnt\Compress', 'compress'));
-
-$etag = md5_file (__FILE__); // Generate an md5sum and use it as the etag for the file, ensuring that caches will revalidate if the code itself changes
-//header('Expires: '.gmdate('D, d M Y H:i:s \G\M\T', time() + 604800));
-header ("Vary: Accept-Encoding");
-header ("Content-type: text/javascript");
-header ("Connection: Keep-Alive");
-header ("Cache-Control: public");
-header ('ETag: "' . $etag . '"');
-?>
-<!--
 function newsTicker(inst)
 {
 // Private / Protected Variables
@@ -247,4 +234,3 @@ function newsTicker(inst)
 //Private / Protected Functions
 
 }
--->
