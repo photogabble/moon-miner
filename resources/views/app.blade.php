0638f608e256b0aac4acea5894ede51f5a2ab459
Simon <carbontwelve@users.noreply.github.com>
2024-09-22 10:58:47 +0100

    refactor(inertia.js): add app view

---
+++ resources/views/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="Description" content="A free online game - Open source, web game, with multiplayer space exploration">
+        <meta name="Keywords" content="Free, online, game, Open source, web game, multiplayer, space, exploration, 4x">
+        <meta name="Rating" content="General">
+
+        <title inertia>{{ config('app.name', 'Moon Miner') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @localisation
+        @routes
+        @vite(['resources/js/app.ts', "resources/js/Pages/{$page['component']}.vue"])
+        @inertiaHead
+    </head>
+    <body class="font-sans antialiased">
+        @inertia
+    </body>
+</html>
