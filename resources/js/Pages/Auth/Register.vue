f9fa97144496007fb3d9ba7fb74144d9f46edfb4
Simon <carbontwelve@users.noreply.github.com>
2024-09-22 10:15:13 +0100

    chore: add license banner

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,4 +1,28 @@
 <script setup lang="ts">
+    /**
+     * Moon Miner, a Free & Opensource (FOSS), web-based 4X space/strategy game forked
+     * and based upon Black Nova Traders.
+     *
+     * @copyright 2024 Simon Dann
+     * @copyright 2001-2014 Ron Harwood and the BNT development team
+     *
+     * @license GNU AGPL version 3.0 or (at your option) any later version.
+     *
+     * This program is free software: you can redistribute it and/or modify
+     * it under the terms of the GNU Affero General Public License as
+     * published by the Free Software Foundation, either version 3 of the
+     * License, or (at your option) any later version.
+     *
+     * This program is distributed in the hope that it will be useful,
+     * but WITHOUT ANY WARRANTY without even the implied warranty of
+     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     * GNU Affero General Public License for more details.
+     *
+     * You should have received a copy of the GNU Affero General Public License
+     * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+     *
+     */
+
     import GuestLayout from '@/Layouts/GuestLayout.vue';
     import InputError from '@/Components/Atoms/Form/InputError.vue';
     import InputLabel from '@/Components/Atoms/Form/InputLabel.vue';
@@ -126,8 +150,6 @@
                         <input-error class="mt-2" :message="form.errors.password_confirmation" />
                     </div>
                 </fieldset>
-
-
 
                 <div class="flex items-center justify-end mt-4">
                     <Link :href="route('login')" class="underline text-sm hover:text-white">
