15f74cb6c34f9c986b90e496447ef405e57e5b9e
Simon <carbontwelve@users.noreply.github.com>
2024-09-22 10:21:15 +0100

    chore: indentation

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,57 +1,57 @@
 <script setup lang="ts">
-    /**
-     * Moon Miner, a Free & Opensource (FOSS), web-based 4X space/strategy game forked
-     * and based upon Black Nova Traders.
-     *
-     * @copyright 2024 Simon Dann
-     * @copyright 2001-2014 Ron Harwood and the BNT development team
-     *
-     * @license GNU AGPL version 3.0 or (at your option) any later version.
-     *
-     * This program is free software: you can redistribute it and/or modify
-     * it under the terms of the GNU Affero General Public License as
-     * published by the Free Software Foundation, either version 3 of the
-     * License, or (at your option) any later version.
-     *
-     * This program is distributed in the hope that it will be useful,
-     * but WITHOUT ANY WARRANTY without even the implied warranty of
-     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-     * GNU Affero General Public License for more details.
-     *
-     * You should have received a copy of the GNU Affero General Public License
-     * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-     *
-     */
+/**
+ * Moon Miner, a Free & Opensource (FOSS), web-based 4X space/strategy game forked
+ * and based upon Black Nova Traders.
+ *
+ * @copyright 2024 Simon Dann
+ * @copyright 2001-2014 Ron Harwood and the BNT development team
+ *
+ * @license GNU AGPL version 3.0 or (at your option) any later version.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
-    import GuestLayout from '@/Layouts/GuestLayout.vue';
-    import InputError from '@/Components/Atoms/Form/InputError.vue';
-    import InputLabel from '@/Components/Atoms/Form/InputLabel.vue';
-    import PrimaryButton from '@/Components/Atoms/Button/PrimaryButton.vue';
-    import TextInput from '@/Components/Atoms/Form/TextInput.vue';
-    import { Head, Link, useForm } from '@inertiajs/vue3';
-    import MainPanel from "@/Components/Atoms/MainPanel.vue";
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+import InputError from '@/Components/Atoms/Form/InputError.vue';
+import InputLabel from '@/Components/Atoms/Form/InputLabel.vue';
+import PrimaryButton from '@/Components/Atoms/Button/PrimaryButton.vue';
+import TextInput from '@/Components/Atoms/Form/TextInput.vue';
+import {Head, Link, useForm} from '@inertiajs/vue3';
+import MainPanel from "@/Components/Atoms/MainPanel.vue";
 
-    const props = defineProps<{
-        locale: string,
-        locales: {[name: string]: {name: string, flag: string}},
-    }>();
+const props = defineProps<{
+    locale: string,
+    locales: { [name: string]: { name: string, flag: string } },
+}>();
 
-    const form = useForm({
-        ship_name: '',
-        character_name: '',
-        email: '',
-        password: '',
-        password_confirmation: '',
-        locale: props.locale,
+const form = useForm({
+    ship_name: '',
+    character_name: '',
+    email: '',
+    password: '',
+    password_confirmation: '',
+    locale: props.locale,
+});
+
+const submit = () => {
+    form.post(route('register'), {
+        onFinish: () => {
+            form.reset('password', 'password_confirmation');
+        },
     });
-
-    const submit = () => {
-        form.post(route('register'), {
-            onFinish: () => {
-                form.reset('password', 'password_confirmation');
-            },
-        });
-    };
+};
 </script>
 
 <template>
