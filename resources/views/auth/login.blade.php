18bf9efddc859a76e0405d4f13282b6a2d04873e
Simon <carbontwelve@users.noreply.github.com>
2024-09-22 10:19:19 +0100

    refactor(inertia.js): php -> vue component

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,24 +1,115 @@
-<x-app-layout>
-    <x-slot:title>Login | Moon Miner</x-slot:title>
+<script setup lang="ts">
+/**
+ * Moon Miner, a Free & Opensource (FOSS), web-based 4X space/strategy game forked
+ * and based upon Black Nova Traders.
+ *
+ * @copyright 2024 Simon Dann
+ * @copyright 2001-2014 Ron Harwood and the BNT development team
+ *
+ * @license GNU AGPL version 3.0 or (at your option) any later version.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
-    <x-main-panel centered>
-        <form method="post" class="border border-ui-orange-500 py-2 px-3 border-x-8 w-1/3 space-y-3">
-            @csrf
-            <x-auth-session-status class="mb-4" :status="session('status')" />
+import Checkbox from '@/Components/Checkbox.vue';
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+import InputError from '@/Components/Atoms/Form/InputError.vue';
+import InputLabel from '@/Components/Atoms/Form/InputLabel.vue';
+import PrimaryButton from '@/Components/Atoms/Button/PrimaryButton.vue';
+import TextInput from '@/Components/Atoms/Form/TextInput.vue';
+import { Head, Link, useForm } from '@inertiajs/vue3';
+import MainPanel from "@/Components/Atoms/MainPanel.vue";
 
-            <div>
-                <x-input-label for="email" :value="__('login.l_login_email')" />
-                <x-text-input id="email" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-                <x-input-error :messages="$errors->get('email')" />
+defineProps<{
+    canResetPassword?: boolean;
+    status?: string;
+}>();
+
+const form = useForm({
+    email: '',
+    password: '',
+    remember: false,
+});
+
+const submit = () => {
+    form.post(route('login'), {
+        onFinish: () => {
+            form.reset('password');
+        },
+    });
+};
+</script>
+
+<template>
+    <Head title="Log in" />
+
+    <guest-layout>
+        <main-panel centered>
+            <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+                {{ status }}
             </div>
 
-            <div>
-                <x-input-label for="password" :value="__('login.l_login_pw')" />
-                <x-text-input id="password" type="password" name="password" required autocomplete="current-password" />
-                <x-input-error :messages="$errors->get('password')" />
-            </div>
+            <form @submit.prevent="submit" class="border border-ui-orange-500 py-2 px-3 border-x-8 w-1/3 space-y-3">
+                <div>
+                    <input-label for="email" :value="__('login.l_login_email')" />
 
-            <button>{{ __('login.l_login_title') }}</button>
-        </form>
-    </x-main-panel>
-</x-app-layout>
+                    <text-input
+                        id="email"
+                        type="email"
+                        class="mt-1 block w-full"
+                        v-model="form.email"
+                        required
+                        autofocus
+                        autocomplete="username"
+                    />
+
+                    <input-error class="mt-2" :message="form.errors.email" />
+                </div>
+
+                <div class="mt-4">
+                    <input-label for="password" :value="__('login.l_login_pw')" />
+
+                    <text-input
+                        id="password"
+                        type="password"
+                        class="mt-1 block w-full"
+                        v-model="form.password"
+                        required
+                        autocomplete="current-password"
+                    />
+
+                    <input-error class="mt-2" :message="form.errors.password" />
+                </div>
+
+                <div class="block mt-4">
+                    <label class="flex items-center">
+                        <Checkbox name="remember" v-model:checked="form.remember" />
+                        <span class="ms-2 text-sm text-gray-600">Remember me</span>
+                    </label>
+                </div>
+
+                <div class="flex items-center justify-end mt-4">
+                    <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm hover:text-white">
+                    Forgot your password?
+                    </Link>
+
+                    <primary-button class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                        {{ __('login.l_login_title') }}
+                    </primary-button>
+                </div>
+            </form>
+        </main-panel>
+    </guest-layout>
+</template>
