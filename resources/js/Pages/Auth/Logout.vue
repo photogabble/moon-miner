c6445672f6d70785edaf0c660b2a6f1a036dcdb5
Simon <carbontwelve@users.noreply.github.com>
2024-09-22 10:41:26 +0100

    refactor(inertia.js): logout page not needed, will become a modal

--- resources/js/Pages/Auth/Logout.vue
+++
@@ -1,7 +0,0 @@
-<x-app-layout>
-    <x-slot:title>Ranking | Moon Miner</x-slot:title>
-
-    <x-main-panel centered>
-        You have been successfully logged out
-    </x-main-panel>
-</x-app-layout>
