4d61aae1f4c66913d71aa986a2813684ff39e0aa
Simon <carbontwelve@users.noreply.github.com>
2024-09-24 22:38:18 +0100

    refactor: Timer

--- classes/Timer.php
+++ classes/Timer.php
@@ -1,46 +1,51 @@
-<?php
-// Blacknova Traders - A web-based massively multiplayer space combat and trading game
-// Copyright (C) 2001-2014 Ron Harwood and the BNT development team
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Affero General Public License as
-//  published by the Free Software Foundation, either version 3 of the
-//  License, or (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Affero General Public License for more details.
-//
-//  You should have received a copy of the GNU Affero General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-// File: classes/Timer.php
+<?php declare(strict_types=1);
+/**
+ * Moon Miner, a Free & Opensource (FOSS), web-based 4X space/strategy game forked
+ * and based upon Black Nova Traders.
+ *
+ * @copyright 2024 Simon Dann
+ * @copyright 2001-2014 Ron Harwood and the BNT development team
+ *
+ * @license GNU AGPL version 3.0 or (at your option) any later version.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
-namespace Bnt;
+namespace App\Helpers;
 
 class Timer
 {
-    public $t_start = 0;
-    public $t_stop = 0;
-    public $t_elapsed = 0;
+    private ?float $initTime = null;
+    private float $startTime;
 
-    public function start()
+    public function start(): void
     {
-        $this->t_start = microtime(true);
+        $this->startTime = microtime(true);
+        if (!$this->initTime) $this->initTime = microtime(true);
     }
 
-    public function stop()
+    public function sample(): float
     {
-        $this->t_stop  = microtime(true);
+        return round(microtime(true) - $this->startTime, 4);
     }
 
-    public function elapsed()
+    public function stop(): float
     {
+        $elapsedSeconds = microtime(true) - $this->initTime;
+        $this->initTime = null;
 
-        $this->t_elapsed = $this->t_stop - $this->t_start;
-
-        return round($this->t_elapsed, 2); // Round it down to two significant digits
+        return $elapsedSeconds;
     }
 }
-?>
