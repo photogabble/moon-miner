05896ac13121795cd5e42477ddf78973fa166be0
Simon <carbontwelve@users.noreply.github.com>
2024-09-26 21:35:13 +0100

    chore(#2): celestial property generator boilerplate

--- app/Models/Properties/CelestialProperties.php
+++ app/Models/Properties/CelestialProperties.php
@@ -25,6 +25,9 @@
 
 namespace App\Models\Properties;
 
+use App\Models\Waypoints\Star;
+use App\Models\Waypoints\Planet;
+
 /**
  * Celestial Properties
  *
@@ -65,5 +68,55 @@ final class CelestialProperties extends ModelProperties
         $this->radius = $attributes['radius'] ?? 0.0;
         $this->surfaceGravity = $attributes['surfaceGravity'] ?? 0.0;
         $this->temperature = $attributes['temperature'] ?? 0.0;
+    }
+
+    /**
+     * Generate new celestials properties. If the primary is a Star then this will generate
+     * a planet celestial, otherwise it will generate a moon celestial.
+     *
+     * @see https://www.youtube.com/watch?v=J5xU-8Kb63Y
+     * @param Star|Planet $primary
+     * @param float $distance
+     * @return void
+     */
+    public function generate(Star|Planet $primary, float $distance): void
+    {
+        if ($primary instanceof Star) {
+            $this->generateAsPlanet($primary, $distance);
+            return;
+        }
+
+        $this->generateAsMoon($primary, $distance);
+    }
+
+    /**
+     * Generates Planet properties as a function of the Star and its distance from it.
+     * @param Star $primary
+     * @param float $distance
+     * @return void
+     */
+    private function generateAsPlanet(Star $primary, float $distance): void
+    {
+        // Star Frost line to be used as dividing line between "inner" and "outer"
+        // TODO: need to identify what kind of planet this is going to be based upon where in the
+        //       solar system its orbit lies. The Sol solar system seems to be quite unique, in
+        //       the real world we have hot jupiter systems and failed sun systems to name but
+        //       two in a growing list of odd solar systems. If a solar system has two or fewer
+        //       planets and one of the orbits is inner, we could place a hot jupiter on a dice roll.
+        //
+
+        // Use https://www.1728.org/kepler3a.htm for calculating orbital period of planets
+        // based upon distance from star.
+        // $this->orbitPeriod = ...
+
+        // eccentricity for inner system planets should be kept to 0.0x to 0.00x, can be larger for far out planets
+        // inclination for inner system planets should be kept < 15, can be larger for far out planets
+
+        // Gas giants form near but not on the frost line. 1 - 1.2AU away is a good approximation
+    }
+
+    private function generateAsMoon(Planet $primary, float $distance): void
+    {
+        // TODO
     }
 }
