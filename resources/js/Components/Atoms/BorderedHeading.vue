65f47c37ee2dc0375243a7fc87722c22a1c138bf
Simon <carbontwelve@users.noreply.github.com>
2024-09-30 21:12:29 +0100

    feat: add bordered heading component

---
+++ resources/js/Components/Atoms/BorderedHeading.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+/**
+ * Moon Miner, a Free & Opensource (FOSS), web-based 4X space/strategy game forked
+ * and based upon Black Nova Traders.
+ *
+ * @copyright 2024 Simon Dann
+ * @copyright 2001-2014 Ron Harwood and the BNT development team
+ *
+ * @license GNU AGPL version 3.0 or (at your option) any later version.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+</script>
+
+<template>
+    <div class="uppercase border-y border-ui-orange-500/50 py-2 px-2 mb-4 border-partway-tl" style="--border-part-t-height:25%;">
+        <slot/>
+    </div>
+</template>
