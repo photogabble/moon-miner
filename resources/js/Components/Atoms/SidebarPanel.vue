0013273d5e65ad4df3065fafd306c887a8995d8d
Simon <carbontwelve@users.noreply.github.com>
2024-09-30 21:00:37 +0100

    feat: add sidebar panel component

---
+++ resources/js/Components/Atoms/SidebarPanel.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+/**
+ * Moon Miner, a Free & Opensource (FOSS), web-based 4X space/strategy game forked
+ * and based upon Black Nova Traders.
+ *
+ * @copyright 2024 Simon Dann
+ * @copyright 2001-2014 Ron Harwood and the BNT development team
+ *
+ * @license GNU AGPL version 3.0 or (at your option) any later version.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+</script>
+<template>
+    <section>
+        <header class="flex uppercase border-y border-ui-orange-500/50 p-1 px-2 border-partway-tl text-sm" style="--border-part-t-height:25%;">
+            <slot name="heading" />
+        </header>
+        <div class="mt-1"><slot /></div>
+    </section>
+</template>
