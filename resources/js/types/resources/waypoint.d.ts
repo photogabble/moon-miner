7255c830c896aa5771aa5babb33b409886cb7581
Simon <carbontwelve@users.noreply.github.com>
2024-09-30 21:15:06 +0100

    chore: flesh out waypoint TypeScript types to match PHP

--- resources/js/types/resources/waypoint.d.ts
+++ resources/js/types/resources/waypoint.d.ts
@@ -1,4 +1,5 @@
 import {Owner} from "@/types";
+import {SystemResource} from "@/types/resources/system";
 
 export enum WaypointType {
     Star = 'Star',
@@ -29,18 +30,34 @@ export interface StarProperties {
     colour: string
 }
 
-export interface PlanetProperties {
-    // TODO
+export interface CelestialProperties {
+    density: number
+    eccentricity: number
+    escapeVelocity: number
+    massDust: number
+    massGas: number
+    orbitPeriod: number
+    orbitRadius: number
+    pressure: 'very low' | 'low' | 'medium' | 'high' | 'very high'
+    radius: number
+    surfaceGravity: number
+    temperatureK: number
 }
 
-export interface WaypointResource<T = any> {
+export type TrackPropertiesOf<T extends WaypointType> =
+    T extends "Star" ? StarProperties :
+    T extends "Planet" ? CelestialProperties :
+    never;
+
+export interface WaypointResource<T extends WaypointType> {
     id: number
     name: string
-    type: WaypointType
+    type: T
     owner?: Owner
     primary_id: number
+    system?: SystemResource
 
-    properties: T
+    properties: TrackPropertiesOf<T>
 
     orbit: {
         angle: number
@@ -49,3 +66,7 @@ export interface WaypointResource<T = any> {
         inclination: number
     }
 }
+
+export type WaypointList = Array<
+    WaypointResource<'Star'> | WaypointResource<'Planet'>
+>;
