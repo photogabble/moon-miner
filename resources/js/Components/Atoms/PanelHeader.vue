73367186f3af4c27f81396b8788eebc26959451e
Simon <carbontwelve@users.noreply.github.com>
2024-09-30 22:41:35 +0100

    refactor: blade -> vue component

---
+++ resources/js/Components/Atoms/PanelHeader.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">/**
+ * Moon Miner, a Free & Opensource (FOSS), web-based 4X space/strategy game forked
+ * and based upon Black Nova Traders.
+ *
+ * @copyright 2024 Simon Dann
+ * @copyright 2001-2014 Ron Harwood and the BNT development team
+ *
+ * @license GNU AGPL version 3.0 or (at your option) any later version.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+import {useSlots} from "vue";
+
+const slots = useSlots();
+
+</script>
+
+<template>
+    <div class="flex justify-between border-b border-b-ui-orange-500/50 py-1 px-2 bg-ui-grey-900">
+        <div><slot /></div>
+        <div v-if="slots.actions" class="flex items-center text-sm gap-1">
+            <slot name="actions" />
+        </div>
+    </div>
+</template>
--- resources/views/components/panel-header.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="flex justify-between border-b border-b-ui-orange-500/50 py-1 px-2 bg-ui-grey-900">
-    <div>{{ $slot }}</div>
-    @if (isset($actions) && $actions->hasActualContent())
-    <div class="flex items-center text-sm gap-1">
-        {{ $actions }}
-    </div>
-    @endif
-</div>
