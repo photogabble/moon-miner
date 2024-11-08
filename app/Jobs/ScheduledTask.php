66882a218819416ddd797ee57aa970bd99de96ea
Simon <carbontwelve@users.noreply.github.com>
2024-11-08 23:39:27 +0000

    refactor(scheduler): create ScheduledTask class

--- app/Jobs/ScheduledTask.php
+++ app/Jobs/ScheduledTask.php
@@ -1,191 +1,109 @@
-<?php
-// Blacknova Traders - A web-based massively multiplayer space combat and trading game
-// Copyright (C) 2001-2014 Ron Harwood and the BNT development team
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Affero General Public License as
-//  published by the Free Software Foundation, either version 3 of the
-//  License, or (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Affero General Public License for more details.
-//
-//  You should have received a copy of the GNU Affero General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-// File: scheduler.php
+<?php declare(strict_types=1);
+/**
+ * Moon Miner, a Free & Opensource (FOSS), web-based 4X space/strategy game forked
+ * and based upon Black Nova Traders.
+ *
+ * @copyright 2024 Simon Dann
+ * @copyright 2001-2014 Ron Harwood and the BNT development team
+ *
+ * @license GNU AGPL version 3.0 or (at your option) any later version.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
-// Explanation of the scheduler
-//
-// Here are the scheduler DB fields, and what they are used for :
-// - sched_id : Unique ID. Before calling the file responsible
-//   for the event, the variable $sched_var_id will be set to
-//   this value, so the called file can modify the triggering
-//   scheduler entry if it needs to.
-//
-// - run_once : Set this to 'N' if you want the event to be
-//   repeated endlessly. If this value is set to 'N', the 'spawn'
-//   field is not used.
-//
-// - ticks_left : Used internally by the scheduler. It represents
-//   the number of mins elapsed since the last call. ALWAYS set
-//   this to 0 when scheduling a new event.
-//
-// - ticks_full : This is the interval in minutes between
-//   different runs of your event. Set this to the frenquency
-//   you wish the event to happen. For example, if you want your
-//   event to be run every three minutes, set this to 3.
-//
-// - spawn : If you want your event to be run a certain number of
-//   times only, set this to the number of times. For this to
-//   work, loop must be set to 'N'. When the event has been run
-//   spawn number of times, it is deleted from the scheduler.
-//
-// - sched_file : This is the file that will be called when an
-//   event has been trigerred.
-//
-// - extra_info : This is a text variable that can be used to
-//   store any extra information concerning the event triggered.
-//   It will be made available to the called file through the
-//   variable $sched_var_extrainfo.
-//
-//  If you are including files in your trigger file, it is important
-//  to use include_once instead of include, as your file might
-//  be called multiple times in a single execution. If you need to
-//  define functions, you can put them in your own
-//  include file, with an include statement. THEY CANNOT BE
-//  DEFINED IN YOUR MAIN FILE BODY. This would cause PHP to issue a
-//  multiple function declaration error.
-//
-//  End of scheduler explanation
+namespace App\Jobs;
 
-$index_page = true; // Ensure that we do not set sessions
-require_once './common.php';
-require_once './config/admin_config.php';
+use Carbon\Carbon;
+use App\Models\Scheduler;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 
-$title = $langvars['l_sys_update'];
-Bnt\Header::display($pdo_db, $lang, $template, $title);
-
-// Database driven language entries
-$langvars = Bnt\Translate::load($pdo_db, $lang, array('admin', 'common', 'global_includes', 'global_funcs', 'footer', 'news', 'scheduler'));
-echo "<h1>" . $title . "</h1>\n";
-
-// This isn't the right thing to do, but its better than creating an entire class for a 12 line function.
-function is_query_ok($db, $res)
+abstract class ScheduledTask implements ShouldQueue
 {
-    $test_result = Bnt\Db::logDbErrors($db, $res, __LINE__, __FILE__);
-    if ($test_result)
-    {
-        echo " ok.<br>";
-    }
-    else
-    {
-        die (" Failed.");
-    }
-}
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-// TODO: Add filtering to swordfish
-if (array_key_exists('swordfish', $_GET))
-{
-    $swordfish = $_GET['swordfish'];
-}
-else
-{
-    if (array_key_exists('swordfish', $_POST))
+    protected Scheduler $schedule;
+
+    protected float $multiplier = 1;
+
+    public function handle(): void
     {
-        $swordfish = $_POST['swordfish'];
-    }
-    else
-    {
-        $swordfish = null;
-    }
-}
+        $this->schedule = Scheduler::firstOrCreate(
+            ['class_name' => static::class],
+            ['last_run_at' => Carbon::now()]
+        );
 
-if ($swordfish != ADMIN_PW)
-{
-    echo "<form accept-charset='utf-8' action='ScheduledTask.php' method='post'>";
-    echo "Password: <input type='password' name='swordfish' size='20' maxlength='20'><br><br>";
-    echo "<input type='submit' value='Submit'><input type='reset' value='Reset'>";
-    echo "</form>";
-}
-else
-{
-    $starttime = time();
-    $lastRun = 0;
-    $schedCount = 0;
-    $lastrunList = null;
-    $sched_res = $db->Execute("SELECT * FROM {$db->prefix}scheduler");
-    Bnt\Db::logDbErrors($db, $sched_res, __LINE__, __FILE__);
-    if ($sched_res)
-    {
-        while (!$sched_res->EOF)
-        {
-            $event = $sched_res->fields;
-            $multiplier = ($bntreg->sched_ticks / $event['ticks_full']) + ($event['ticks_left'] / $event['ticks_full']);
-            $multiplier = (int) $multiplier;
-            $ticks_left = ($bntreg->sched_ticks + $event['ticks_left']) % $event['ticks_full'];
-            $lastRun += $event['last_run'];
-            $schedCount += 1;
+        if ($this->schedule->wasRecentlyCreated === false) {
+            $diff = Carbon::now()->floatDiffInSeconds($this->schedule->next_run_after);
 
-            // Store the last time the individual schedule was last run.
-            $lastrunList[$event['sched_file']] = $event['last_run'];
+            if ($this->schedule->next_run_after->isPast()) {
+                // If running very late, alert.
+                if ($diff > $this->periodSeconds() * 2) $this->alert();
 
-            if ($event['run_once'] == 'Y')
-            {
-                if ($multiplier > $event['spawn'])
-                {
-                    $multiplier = $event['spawn'];
-                }
-
-                if ($event['spawn'] - $multiplier == 0)
-                {
-                    $resx = $db->Execute("DELETE FROM {$db->prefix}scheduler WHERE sched_id = ?", array($event['sched_id']));
-                    Bnt\Db::logDbErrors($db, $resx, __LINE__, __FILE__);
-                }
-                else
-                {
-                    $resy = $db->Execute("UPDATE {$db->prefix}scheduler SET ticks_left = ?, spawn = spawn - ? WHERE sched_id = ?", array($ticks_left, $multiplier, $event['sched_id']));
-                    Bnt\Db::logDbErrors($db, $resy, __LINE__, __FILE__);
-                }
+                // Work out how many ticks we have missed and set as the multiplier.
+                $this->multiplier = 1 + $diff / $this->periodSeconds();
             }
-            else
-            {
-                $resz = $db->Execute("UPDATE {$db->prefix}scheduler SET ticks_left = ? WHERE sched_id = ?", array($ticks_left, $event['sched_id']));
-                Bnt\Db::logDbErrors($db, $resz, __LINE__, __FILE__);
-            }
-
-            $sched_var_id = $event['sched_id'];
-            $sched_var_extrainfo = $event['extra_info'];
-
-            $sched_i = 0;
-            while ($sched_i < $multiplier)
-            {
-                include_once './'. $event['sched_file'];
-                $sched_i++;
-            }
-            $sched_res->MoveNext();
         }
-        $lastRun /= $schedCount;
+
+        $runs = ($this->maxCatchup() >= 0)
+            ? $this->maxCatchup()
+            : floor($this->multiplier);
+
+        while ($runs > 0) {
+            $this->run();
+            $runs--;
+        }
+
+        $this->schedule->update([
+            'last_run_at' => Carbon::now(),
+            'next_run_after' => Carbon::now()->addMinutes($this->periodMinutes()),
+            'multiplier' => $this->multiplier,
+        ]);
     }
 
-    // Calculate the difference in time when the last good update happened.
-    $schedDiff = ($lastRun - (time() - ($bntreg->sched_ticks * 60)));
-    if (abs($schedDiff) > ($bntreg->sched_ticks * 60))
+    protected function alert(): void
     {
-        // Hmmm, seems that we have missed at least 1 update, so log it to the admin.
-        Bnt\AdminLog::writeLog($db, 2468, "Detected Scheduler Issue|{$lastRun}|". time() ."|". (time() - ($bntreg->sched_ticks * 60)) ."|{$schedDiff}|". serialize($lastrunList));
+        // TODO: log and alert admin
     }
 
-    $runtime = time() - $starttime;
-    echo "<p>The scheduler took $runtime seconds to execute.<p>";
+    /**
+     * How many minutes should be between runs. This should be identical to what
+     * the Scheduler is configured in App\Console\Kernel.
+     *
+     * @return int
+     */
+    abstract public function periodMinutes(): int;
 
-    $res = $db->Execute("UPDATE {$db->prefix}scheduler SET last_run = ". time());
-    Bnt\Db::logDbErrors($db, $res, __LINE__, __FILE__);
+    public function periodSeconds(): int
+    {
+        return $this->periodMinutes() * 60;
+    }
+
+    /**
+     * If running late, how many times should this task be run to catch up.
+     * Set to -1 to be number of runs that can fit within diff.
+     *
+     * @return int
+     */
+    public function maxCatchup(): int
+    {
+        return -1;
+    }
+
+    abstract protected function run(): void;
 }
-
-echo "<br>";
-Bnt\Text::gotoMain($db, $lang, $langvars);
-Bnt\Footer::display($pdo_db, $lang, $bntreg, $template);
-?>
