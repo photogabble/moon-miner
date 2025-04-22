0d4b4bef5855a4d5cf4a27b420cc9d882a9c813f
Simon <carbontwelve@users.noreply.github.com>
2025-04-22 21:05:48 +0100

    refactor: inertia.js

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,23 +1,4 @@
-{{--
-    Blacknova Traders - A web-based massively multiplayer space combat and trading game
-    Copyright (C) 2001-2024 Ron Harwood and the BNT development team.
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-    File: layout.blade.php
---}}
-    <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <!-- START OF HEADER -->
 <head>
@@ -29,26 +10,19 @@
     <meta name="Rating" content="General">
     <link rel="shortcut icon" href="favicon.ico">
 
-    <!-- Styles -->
-    @vite('resources/css/app.css')
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Ubuntu">
+    <title inertia>{{ config('app.name', 'Moon Miner') }}</title>
 
-    <title>@yield('title')</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    @vite('resources/js/app.js')
+    <!-- Scripts -->
+    @localisation
+    @routes
+    @vite(['resources/js/app.ts', "resources/js/Pages/{$page['component']}.vue"])
+    @inertiaHead
 </head>
 <!-- END OF HEADER -->
 
-<body class="{{ $body_class ?? 'bnt' }}">
-<div class="wrapper">
-
-    <!-- START OF BODY -->
-    @yield('content')
-    <!-- END OF BODY -->
-
-    <!-- START OF FOOTER -->
-    <x-footer />
-<!-- END OF FOOTER -->
-
-</body>
+<body class="font-sans antialiased">@inertia</body>
 </html>
